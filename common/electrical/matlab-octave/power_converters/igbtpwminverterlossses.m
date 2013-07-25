c0f79f0c0258d5f436d7b6ff88a7a0bb7eb5c7dc
crobarcro <3159692+crobarcro@users.noreply.github.com>
2013-07-25 21:30:05 +0000

    igbtpwminverterlossses.m: added loss calculation for alternative modulation type, pluss other improvements Test_igbtpwminverterlossses.m: corrected some test inputs to match paper comparisons

--- common/electrical/matlab-octave/power_converters/igbtpwminverterlossses.m
+++ common/electrical/matlab-octave/power_converters/igbtpwminverterlossses.m
@@ -1,9 +1,11 @@
-function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_CE0,V_FN,V_F0,t_rN,t_fN,t_rrN,Q_rrN,I_CM,V_cc,F_s,M)
+function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= ...
+    igbtpwminverterlossses(I_CN, V_CEN, V_CE0, V_FN, V_F0, t_rN, t_fN, t_rrN, Q_rrN, I_CM, V_cc, F_s, M, pf, mtype)
 % calculates losses in the IGBT switches of PWM inverters
 %
 % Syntax 
 %
-% [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwmlossses()
+% [ Ptot, Pi, Pd, Pon, Poff, Prr ]= ...
+%       igbtpwmlossses(I_CN,V_CEN,V_CE0,V_FN,V_F0,t_rN,t_fN,t_rrN,Q_rrN,I_CM,V_cc,F_s,M)
 %
 % Description
 %
@@ -12,6 +14,9 @@ function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_C
 % system will be able to calculate losses for different modulation methods,
 % provided the current output is sinusoidal. This function implements the
 % methods described in [1].
+%
+% Note that the results here are defined for 2nd generation 1200V IGBTs,
+% and depend on the
 % 
 % Input
 %
@@ -42,6 +47,15 @@ function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_C
 %
 %   F_s - switching frequency
 %
+%   pf - desired power factor
+%
+%   mtype - (optional) scalar specifying modulation type, can be 0 or 1, if
+%     0 sine modulation is used, if 1, sine modulation with the third
+%     harmonic is used. Default is 1 is not supplied. An mtype of 1 can be
+%     used for other modulation systems which also give 100% output
+%     voltage, such as bus clamping and vector modulation with good
+%     accuracy.
+%
 % Output
 %
 %   Ptot - Total power loss in a single switch
@@ -61,37 +75,42 @@ function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_C
 %
 %
 
-% Created by Richard Crozier 2012
+% Created by Richard Crozier 2013
 
-    % The typical voltage/current graph V_CE / I_CE is approximated by
-    % the following linear equation, where:
-    % 
-    % I_C = collector current
-    % I_CN = rated I_C
-    % V_CE = collector-to-emitter voltage
-    % V_CN = rated V_CE
-    % V_CE0 = threshold V_CE
+    if nargin < 15
+        mtype = 1;
+    end
     
-%     V_CE = ( (V_CEN - V_CE0) .* I_C ) / I_CN + V_CE0;
-%     
-%     % The diode forward voltage follows an exponential law. In the working
-%     % range, we may simplify the equation, approximating it to a linear law
-%     % with the origin at V_F0. 
-%     if fdafd
-%         V_F = ( (V_FN - V_F0) * I_C ) / I_CN + V_F0;
-%     else
-%         % The threshold voltage is taken as 0.7 V
-%         V_F = 0.7;
-%     end
+    if mtype == 0
+        % modulation is simple sine wave
+        
+        % calculate the IGBT forward losses
+        Pi = ( 0.125 + M./(3*pi)) .* ( (V_CEN - V_CE0) ./ I_CN ) .* I_CM.^2 ...
+             + (1/(2*pi) + 0.125.*M.*pf) .* V_CE0 .* I_CM;
+
+        % the diode forward losses
+        Pd = ( 0.125 - M./(3*pi)) .* ( (V_FN - V_F0) ./ I_CN) .* I_CM.^2 ...
+             + (1/(2*pi) - 0.125.*M.*pf) .* V_CE0 .* I_CM;        
+
+    elseif mtype == 1
+        % modulation is sine wave with third harmonic
+        
+        % do some subcalculations for speed
+        a = 2 * sqrt(3) ./ (9 * pi);
+        b = sqrt(3) / (45 * pi);
+        c = sqrt(3) / 12;
+        theta = acos(pf);
+        
+        % calculate the IGBT forward losses
+        Pi = ( 0.125 + a.*M.*pf - b.*M.*cos(3.*theta) ) .* ( (V_CEN - V_CE0) ./ I_CN ) .* I_CM.^2 ...
+             + (1/(2*pi) + c.*M.*pf) .* V_CE0 .* I_CM;
+
+        % the diode forward losses
+        Pd = ( 0.125 - a.*M.*pf + b.*M.*cos(3.*theta) ) .* ( (V_FN - V_F0) ./ I_CN) .* I_CM.^2 ...
+             + (1/(2*pi) - c.*M.*pf) .* V_CE0 .* I_CM;
+         
+    end
     
-    % calculate the IGBT forward losses
-    Pi = ( 0.125 + M./(3*pi)) .* ( (V_CEN - V_CE0) ./ I_CN) .* I_CM.^2 ...
-         + (1/(2*pi) + 0.125.*M.*cos(theta)) .* V_CE0 .* I_CM;
-    
-    % the diode forward losses
-    Pd = ( 0.125 - M./(3*pi)) .* ( (V_FN - V_F0) ./ I_CN) .* I_CM.^2 ...
-         + (1/(2*pi) - 0.125.*M.*cos(theta)) .* V_CE0 .* I_CM;
-     
     % IGBT turn-on losses
     % 
     % t_rN = rated rise time
@@ -99,7 +118,7 @@ function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_C
     % F_s = switching frequency
     % I_CM = maximum collector current
     % I_CN = rated collector current
-    Pon = 0.125 .* V_cc .* t_rN .* (I_CM.^2 / I_CN) .* F_s;
+    Pon = 0.125 .* V_cc .* t_rN .* (I_CM.^2 ./ I_CN) .* F_s;
     
     % IGBT turn-off losses
     % 
@@ -108,7 +127,7 @@ function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_C
     % F_s = switching frequency
     % I_CM = maximum collector current
     % I_CN = rated collector current
-    Poff = V_cc .* I_CM .* t_fN .* F_s .* ( (1/(3*pi)) + (1/24).*(I_CM/I_CN));
+    Poff = V_cc .* I_CM .* t_fN .* F_s .* ( (1/(3*pi)) + (1/24).*(I_CM./I_CN));
     
     % Recovery losses
     %
@@ -116,7 +135,7 @@ function [ Ptot, Pi, Pd, Pon, Poff, Prr ]= igbtpwminverterlossses(I_CN,V_CEN,V_C
     % t_rrN = rated recovery time
     %
     Prr = F_s .* V_cc .* ( (0.28 + (0.38.*I_CM)./(pi.*I_CN) + 0.015 .*(I_CM./I_CN).^2) .* Q_rrN ...
-                          + ( 0.8/pi + 0.05.*(I_CM./ICN) ).*I_CM.*t_rrN  );
+                          + ( 0.8/pi + 0.05.*(I_CM./I_CN) ).*I_CM.*t_rrN  );
     
     
 	% get the total power loss
