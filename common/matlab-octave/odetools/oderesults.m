b2c5bb37e5ef8e9fdb7e11ce3baf88fcfc791be9
crobarcro <3159692+crobarcro@users.noreply.github.com>
2013-09-11 14:11:56 +0000

    Added additional ode tools odeinternals.m, prallocresfcn.m and oderesults.m

---
+++ common/matlab-octave/odetools/oderesults.m
@@ -0,0 +1,81 @@
+function results = oderesults(T, Y, odeevfcn, odeargs, skip)
+% oderesults: extracts results from an appropriately coded ode which were
+% not variables of the integration.
+%
+% Syntax
+%
+% results = oderesults(T, Y, odeevfcn)
+% results = oderesults(T, Y, odeevfcn, odeargs)
+% results = oderesults(T, Y, odeevfcn, odeargs, skip)
+%
+% Description
+%
+% oderesults recalls a function designed to be used with the standard
+% matlab ODE solvers (e.g. ode45, ode15s etc.) after a solution is complete
+% in order to extract internally calculated values of interest generated
+% during the simulation, but which are not returned by the solver (which
+% generally only return the variables which are the subject of the
+% integration). To achieve this with oderesults, the solution function must
+% be coded appropriately to return certain information about the desired
+% results when called in a certain way.
+%
+% T, Y - these are the output produced by the ODE solver using the solution
+%   function in 'odeevfcn'
+%
+% odeevfcn is an ode solution function that can be called with multiple
+%   output arguments. Each output argument must return a scalar or vector
+%   which represents a result of interest at each time in T.  It must also
+%   be coded so that when called with no arguments it returns a cell array
+%   of strings which will become the names of fields in a results
+%   structure. The number of results of interest will be determined by the
+%   number of strings in the cell array. If returning a vector, the length
+%   of the vector on each call must remain the same as they will be
+%   concatenated into a matrix.
+%
+% odeargs in an optional cell array of additional arguments to passed to the
+%   ode solution function, cal be an empty cell array if no arguments are
+%   desired, but the 'skip' input is to be used.
+%
+% skip is a scalar integer which determines how much of the solution to
+%   recalculate. If skip is greater than 1, only every 'skip'th value is
+%   recalculated.
+%
+% See also: prallocresfcn.m
+%
+
+    if nargin < 4
+        odeargs = {};
+    end
+    
+    if nargin < 5
+        skip = 1;
+    end
+
+    % preallocate the results structure with fields containing arrays of
+    % zeros
+    [results, fn, nOut] = prallocresfcn(T, Y, odeevfcn, odeargs, skip);
+    
+    if skip ~= 1
+        % store the times the results values occur at if we are skipping
+        % some in the results structure
+        results.Tskip = T(1:skip:length(T));
+    end
+    
+    k = 0;
+    
+    outArgs = cell(1, nOut);
+    
+    % now recalculate the values and put them in the results structure
+    for i = 1:skip:length(T)
+
+        k = k + 1;
+        
+        [outArgs{1:nOut}] = feval(odeevfcn, T(i), Y(i,:)', odeargs{:});
+        
+        for j = 1:nOut
+           results.(fn{j})(k,:) = outArgs{j}; 
+        end
+    
+    end
+    
+end
---
+++ common/matlab-octave/odetools/prallocresfcn.m
@@ -0,0 +1,68 @@
+function [results, fn, nOut] = prallocresfcn(T, Y, odeevfcn, odeargs, skip)
+% prallocresfcn: preallocates a structure with the appropriate fields to
+% extract the results of an ode simulation. It is expected that the ode
+% function will return a cell array of strings with the appropriate field
+% names when called with no arguments.
+%
+% Syntax
+%
+% [results, fn, nOut] = prallocresfcn(T, Y, odeevfcn, odeargs)
+%
+% Input
+%
+% T - vector of time points as produced by the ode solver acting on the
+%     solution function.
+%
+% Y - matlrix of values of the solution variables at each time step in T.
+%     Each column of Y should be a solution variable.
+%
+% odeevfcn - function handle or string containing the name of the ode
+%            evaluation function used to generate the results. This
+%            function must return a cell array of strings containing the
+%            names of the variables returned by the function. These will be
+%            used to populate a structure containing fields with the same
+%            names.
+%
+% odeargs - (optional) a cell array extra arguments which must be passed to 
+%           the ode function.
+%
+% skip - (optional) a variable to determine if we must extract results for
+%        every time in T, or if some can be skipped. The elements
+%        1:skip:numel(T) are to be evaluated, and so an appropriate
+%        allocation of space will be made for this. If not supplied a value
+%        of 1 is used, and space is reserved for every time step. If skip
+%        is to be used, but no extra odeargs are required, odeargs can be
+%        an empty cell array. 
+%
+
+    if nargin < 5
+        skip = 1;
+    end
+    
+    % get the field names for the output structure by calling the ode
+    % function with no inputs. It should be coded to output a cell array of
+    % strings containing these if no input arguments are supplied
+    fn = feval(odeevfcn);
+    
+    nOut = numel(fn); 
+    
+    % test the size of the arrays needed for preallocation by calling the
+    % ode function
+    [testresult{1:nOut}] = feval(odeevfcn, T(1), Y(1,:)', odeargs{:});
+     
+    if skip > numel(T) - 1
+        rows = 1;
+    else
+        rows = ceil(numel(T) / skip); 
+    end
+    
+    results.(fn{1}) = zeros(rows, numel(testresult{1}));
+    
+    % Now preallocate arrays of the correct sizes  
+    for i = 2:numel(fn)
+        
+        results.(fn{i}) = zeros(rows, numel(testresult{i}));
+        
+    end
+
+end
