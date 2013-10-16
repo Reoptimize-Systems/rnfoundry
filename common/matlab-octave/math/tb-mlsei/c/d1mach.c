49921e293febbd2f131844f9a6de7baad810b142
crobarcro <crobarcro@localhost>
2013-10-16 11:28:26 +0000

    added d1mach.c and i1mach.c

---
+++ common/matlab-octave/math/tb-mlsei/c/d1mach.c
@@ -0,0 +1,18 @@
+ /* Standard C source for D1MACH -- remove the * in column 1 */
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
+#include <stdlib.h>
+             
+double d1mach_(long *i)
+{
+	switch(*i){
+	  case 1: return DBL_MIN;
+	  case 2: return DBL_MAX;
+	  case 3: return DBL_EPSILON/FLT_RADIX;
+	  case 4: return DBL_EPSILON;
+	  case 5: return log10((double)FLT_RADIX);
+	  }
+	fprintf(stderr, "invalid argument: d1mach(%ld)\n", *i);
+	exit(1); return 0; /* some compilers demand return values */
+}
