aaf85ba998d173a9ee62fc23e6f15c858c23a2d9
crobarcro <crobarcro@localhost>
2013-10-17 13:36:29 +0000

    finfun_RADIAL.m: removed commented code now in finfun_ROTARY

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/odesim/finfun_RADIAL.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/odesim/finfun_RADIAL.m
@@ -3,12 +3,6 @@ function [design, simoptions] = finfun_RADIAL(design, simoptions)
 % produced by an fea simulation in readiness for a dynamic simulation
 
     % do post-processing common to TORUS type machines
-%     
-%     % Convert air gap closing force to force per unit area
-%     design.gforce = design.gforce ./ (design.taupm * design.hm);
-%     
-%     % fit a polynomial to the air gap force
-%     design.p_gforce = polyfitn(design.gvar, design.gforce, 2);
 
     % set the polewidth to be the distance in radians swept out by a pole
     design.PoleWidth = design.thetap;
