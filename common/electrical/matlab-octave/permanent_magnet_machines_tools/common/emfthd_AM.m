4fb6fa65c9b682bceed2b83c562f161257685e5b
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-02-24 13:58:39 +0000

    emfthd_AM.m: added comments

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/emfthd_AM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/emfthd_AM.m
@@ -12,9 +12,12 @@ function THD = emfthd_AM(slm_fluxlinkage)
 %       waveform
 %
 
+    % get the fundumental frequency and its harmonics from the slm object
+    % fitted to the flux lnkage with displacement
     [fundamental_power, harmonics_power] = decomposeemf(slm_fluxlinkage);
 
-    % Calculate THD as a ratio of square roots of powers
+    % Calculate % THD as a ratio of square roots of the total harmonic
+    % power to the fundumental power
     THD = 100 * sqrt(harmonics_power) / sqrt(fundamental_power);
 
 end
