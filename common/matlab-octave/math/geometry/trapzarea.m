af75f50a0507b529972549c49b142fa577d32a46
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-03-07 12:14:34 +0000

    trapzarea.m: improved help text

--- common/matlab-octave/math/geometry/trapzarea.m
+++ common/matlab-octave/math/geometry/trapzarea.m
@@ -1,6 +1,21 @@
 function area = trapzarea(a, b, h)
 % calculates the area of a trapesium
-
+%
+% Syntax
+%
+% area = trapzarea(a, b, h)
+%
+% Description 
+%
+%        <-- a -->
+%        _________
+%       /          \       ^
+%      /            \      : h
+%     /              \     :
+%    /________________\    v
+%    <------ b ------>
+%
+%
     area = h .* ((a + b) ./ 2);
 
 end
