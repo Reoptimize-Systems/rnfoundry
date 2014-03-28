4580482485e0453bfb3d9513659f3348159e869b
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-03-28 12:25:30 +0000

    completedesign_ROTARY.m: Added comments and now automatically create qc fraction object under certain circumstances

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/optimisation/completedesign_ROTARY.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/optimisation/completedesign_ROTARY.m
@@ -6,7 +6,38 @@ function design = completedesign_ROTARY(design, simoptions)
 % design = completedesign_ROTARY(design, simoptions)
 %
 % 
-    
+% Description
+%
+% 
+
+% Stator slot/coil/winding terminology is based on that presented in:
+%
+% J. J. Germishuizen and M. J. Kamper, "Classification of symmetrical
+% non-overlapping three-phase windings," in The XIX International
+% Conference on Electrical Machines - ICEM 2010, 2010, pp. 1-6.
+% Single layer
+%   q = 2qc
+%   Qs = 2Qc
+% Double layer
+%   q = qc
+%   Qs = Qc
+%
+% yp - Average coil pitch as defined by (Qs/Poles)
+% yd - Actual coil pitch as defined by round(yp) +/- k
+% Qs  -  total number of stator slots in machine
+% Qc  -  total number of winding coils in machine 
+% q  -  number of slots per pole and phase, (1)
+% qn  -  numerator of q
+% qd  -  denominator of q
+% qc - number of coils per pole and phase, (2)
+% qcn  -  numerator of qc
+% qcd  -  denominator of qc
+
+    % check the minimum set of winding specification
+    if all(isfield(design, {'Qc', 'Phases', 'Poles'})) && ~isfield (design, 'qc')
+        % create qc fraction
+        design.qc = fr(design.Qc, design.Poles * design.Phases);
+    end
 
     % the number of Poles should be supplied and the ratio of coils to
     % Poles and Phases
@@ -21,10 +52,11 @@ function design = completedesign_ROTARY(design, simoptions)
     % get the "basic winding" (the smallest repetitive segment of the
     % machine winding in terms of symmetry) based on the qc ratio (the
     % ratio of coils per pole and phase). The smallest repetitive segment
-    % is the smallest part that can be modelled using symmetric boudaries
+    % is the smallest part that can be modelled using symmetric boundaries
     [design.Qcb,design.pb] = rat(design.qc * design.Phases);
     
-    % ensure a magnetically neutral rotor
+    % ensure a magnetically neutral rotor by having at least 2 poles in the
+    % basic winding segment
     if design.pb == 1
         design.pb = 2;
         design.Qcb = 2 * design.Qcb;
@@ -85,6 +117,8 @@ function design = completedesign_ROTARY(design, simoptions)
     end
     
     design.thetap = 2*pi / design.Poles;
+    
+    design.NCoilsPerPhase = design.Qc / design.Phases;
     
 end
 
