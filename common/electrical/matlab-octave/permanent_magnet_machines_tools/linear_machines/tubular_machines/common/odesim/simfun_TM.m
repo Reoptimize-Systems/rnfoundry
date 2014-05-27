30509a06a64ba05e2420b25f5c18298feb30ead8
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-05-27 12:55:57 +0000

    simfun_TM.m: added some help text

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/odesim/simfun_TM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/odesim/simfun_TM.m
@@ -1,4 +1,14 @@
 function [design, simoptions] = simfun_TM(design, simoptions)
+% simulation preprocessing function common to all tubular linear machines
+%
+% Syntax
+%
+% [design, simoptions] = simfun_TM(design, simoptions)
+%
+% Input
+%
+% design - a tubular machine design structure 
+% 
 
     design.MTL = 2 * pi * mean([design.Ro, design.Ri]); 
 
