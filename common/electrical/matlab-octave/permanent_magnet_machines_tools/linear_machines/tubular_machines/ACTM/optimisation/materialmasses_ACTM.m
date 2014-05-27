82238099ae23fc9c3df5250ca65f6a08e8694389
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-05-27 12:54:45 +0000

    separated out mass and cost calculations for tubular machines

---
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/ACTM/optimisation/materialmasses_ACTM.m
@@ -0,0 +1,11 @@
+function [design, simoptions] = materialmasses_ACTM(design, simoptions)
+% calculates material masses of components of the air-cored tubular machine
+%
+% Syntax
+%
+% [design, simoptions] = materialmasses_ACTM(design, simoptions)
+%
+
+    [design, simoptions] = materialmasses_TM(design, simoptions);
+    
+end
--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/optimisation/costestimate_TM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/optimisation/costestimate_TM.m
@@ -1,10 +1,10 @@
-function [cost, design] = costestimate_TM(design, evaloptions, buoymass)
+function [cost, design] = costestimate_TM(design, simoptions, buoymass)
 
     if nargin < 3
         buoymass = 0;
     end
     
-    design.BuoyCost = buoymass * evaloptions.BuoyMassCost;
+    design.BuoyCost = buoymass * simoptions.evaloptions.BuoyMassCost;
     
     [design.PowerConverterCost, design.PowerConverterRating] = ...
                      powerconvertercostest(design.EMFPhasePeak, ...
@@ -12,40 +12,16 @@ function [cost, design] = costestimate_TM(design, evaloptions, buoymass)
                                            design.PowerLoadMean, ...
                                            design.L(1,1), ...
                                            design.vRmax / (2 * design.PoleWidth) );
+
+    design.CopperCost = design.CopperMass * simoptions.evaloptions.CopperCost;
+
+    design.MagnetCost = design.MagnetMass * simoptions.evaloptions.MagnetCost;
+
+    design.ArmatureIronCost = design.ArmatureIronMass * simoptions.evaloptions.ArmatureIronCost;
     
-    %design.wlength = wirelength_TM(design.RoVRm, design.CoilTurns, design.Rm, design.g);
-    design.wlength = design.CoilTurns * pi * 2 * (design.Ri + (design.Hc / 2));
-    
-    % Determine the copper cost based on the length of wire in a coil
-    % multiplied by the number of Phases and armature Poles and the wire
-    % cross-sectional area etc.
-    design.CopperMass = design.Phases * design.wlength * (pi* (design.Dc/2)^2) * ...
-                        design.Poles(2) * evaloptions.CopperDensity;
-    design.CopperCost = design.CopperMass * evaloptions.CopperCost;
-    
-    % Now calculate the total cost of the magnets
-    design.MagnetMass = design.Poles(1) * design.Wm * pi * (design.Rm.^2 - design.Rso^2) * ...
-                        evaloptions.MagnetDensity;
-    design.MagnetCost = design.MagnetMass * evaloptions.MagnetCost;
-    
-    % Calculate the cost of the laminated iron in the armature
-    design.ArmatureIronMass = design.Poles(2) * design.Wp * pi * (design.Ra.^2 - design.Ro.^2) * ...
-                              evaloptions.ArmatureIronDensity;
-    design.ArmatureIronCost = design.ArmatureIronMass * evaloptions.ArmatureIronCost;
-    
-    % Calculate the cost of the back iron on the field, i.e. the steel
-    % discs in the translator
-    if design.mode == 1 || design.mode == 3
-        cavityvol = steelcavityvol_TM(design);
-    else
-        cavityvol = 0;
-    end
-    
-    design.FieldIronMass = (design.Ws * pi * (design.Rm^2 - design.Rso^2) - cavityvol) * ...
-                           design.Poles(1) * evaloptions.FieldIronDensity;
-    design.FieldIronCost = design.FieldIronMass * evaloptions.FieldIronCost;
+    design.FieldIronCost = design.FieldIronMass * simoptions.evaloptions.FieldIronCost;
                     
-    design.ShaftCost = design.ShaftMass * evaloptions.StructMaterialCost;
+    design.ShaftCost = design.ShaftMass * simoptions.evaloptions.StructMaterialCost;
     
     % calculate the estimated cost of the machine, not including converter
     design.MachineCost = design.MagnetCost + design.CopperCost + ...
@@ -53,7 +29,7 @@ function [cost, design] = costestimate_TM(design, evaloptions, buoymass)
                          design.ShaftCost;
     
     % Finally calculate the combined cost
-    cost = (design.MachineCost + design.PowerConverterCost) * evaloptions.nmachines + design.BuoyCost;
+    cost = (design.MachineCost + design.PowerConverterCost) * simoptions.evaloptions.nmachines + design.BuoyCost;
 
     design.CostEstimate = cost;
     
