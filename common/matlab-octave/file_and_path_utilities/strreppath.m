8c48c67d32030d3077fee4bbc8c5e7d1a26fa221
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-06-17 10:35:23 +0000

    strreppath.m: modified how optional options are supplied

--- common/matlab-octave/file_and_path_utilities/strreppath.m
+++ common/matlab-octave/file_and_path_utilities/strreppath.m
@@ -1,26 +1,39 @@
-function strreppath(S1, S2, topdir)
+function strreppath(S1, S2, varargin)
 % replace strings in all files in the matlab path or a sub-path recursively
 %
 % Syntax
 %
 % strreppath(S1, S2)
-% strreppath(S1, S2, topdir)
+% strreppath(S1, S2, 'Parameter', Value)
 %
 % Description
 % 
-% strreppath(S1,S2) replaces all occurrences of the string S1 in all files
-% on the matlab path with the string S2. S1, S2 may also be cell arrays of
-% strings of the same size, in which case the replacement is performed for
-% each pair by performing a STRREP using corresponding elements of the
-% inputs. Alternatively S2 may be a string and S1 a cell array, in this
-% case the single string S2 will replace all the strings in S1.
+% strreppath(S1,S2) replaces all occurrences of the string S1 in all mfiles
+% (with a '.m' extension) on the matlab path with the string S2. S1, S2 may
+% also be cell arrays of strings of the same size, in which case the
+% replacement is performed for each pair by performing a STRREP using
+% corresponding elements of the inputs. Alternatively S2 may be a string
+% and S1 a cell array, in this case the single string S2 will replace all
+% the strings in S1.
 %
-% strreppath(S1,S2,topdir) replaces the strings the directory supplied in
-% 'topdir', and all subdirectories recursively.
+% Additional arguments can also be supplied as parameter-value pairs.
 %
+% strreppath(S1,S2,'topdir',path) replaces the strings the directory
+% supplied in path, and all its subdirectories recursively.
+%
+% strreppath(S1,S2,'ext',ext) replaces the strings the directory supplied
+% in 'topdir', and all subdirectories recursively, but in files with the
+% extension supplied in ext, instead of files with a '.m' extension. The
+% extension should be supplied without the '.', e.g. just 'm', or 'txt', or
+% 'cpp'.
 %
 
-    if nargin < 3 || isempty(topdir)
+    Inputs.ext = 'm';
+    Inputs.topdir = '';
+    
+    Inputs = parseoptions (Inputs, varargin);
+    
+    if isempty(Inputs.topdir)
         thepath = path2cell(path);
     else
         if ~ischar(topdir)
@@ -29,17 +42,15 @@ function strreppath(S1, S2, topdir)
         if exist(topdir, 'file') ~= 7
             error('supplied directory name does not exist.')
         end
-        thepath = path2cell(genpath(topdir));
+        thepath = path2cell(genpath(Inputs.topdir));
     end
     
     for indi = 1:numel(thepath)
 
-        mfiles = dir([ thepath{indi}, '\*.m' ]);
+        mfiles = dir([ thepath{indi}, '\*.', Inputs.ext ]);
 
         for indii = 1:numel(mfiles)
-            % replace string in file using regexprepfile, with a regex
-            % looking for word boundaries to avoid functions with similar
-            % substrings
+            % replace string(s) in file using strrepfile
             strrepfile(fullfile(thepath{indi}, mfiles(indii).name), S1, S2);
         end
 
