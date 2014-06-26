e5d5197dbc5278d2e7e3ab12bc9aa5c02ae0f45c
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-06-26 16:22:31 +0000

    pm_machines_tools_rootdir.m: corrected h1 line

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/pm_machines_tools_rootdir.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/pm_machines_tools_rootdir.m
@@ -1,5 +1,11 @@
 function path = pm_machines_tools_rootdir()
-% returns the root directory for hammerfest code
+% returns the root directory for the permanent magnet machine toolbox
+%
+% Syntax
+%
+% path = pm_machines_tools_rootdir()
+%
+% 
 
     path = getmfilepath(mfilename());
 
