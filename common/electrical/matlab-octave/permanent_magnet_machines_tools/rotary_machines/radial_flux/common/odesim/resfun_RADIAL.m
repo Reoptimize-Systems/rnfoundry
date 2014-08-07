a4819897e51ef931ac3c2275189a4cdd9077a535
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-08-07 20:55:07 +0000

    Added stub resfun functions for radial and slotted radial

---
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/odesim/resfun_RADIAL.m
@@ -0,0 +1,20 @@
+function [results, design] = resfun_RADIAL(T, Y, design, simoptions)
+% post processes results from an ode simulation of a radial flux electrical
+% machine
+%
+% Syntax
+%
+% [results, design] = resfun_ROTARY(T, Y, design, simoptions)
+%
+%
+% Input
+%
+% 
+
+% Copyright Richard Crozier 2014
+
+
+    [results, design] = resfun_ROTARY(T, Y, design, simoptions);
+
+
+end
