b01f8a23171fa1824fbf8d6d22366124445d9597
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-08-15 15:30:46 +0000

    Updates to new core loss data codes

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/iron_losses/corelosscoeffs.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/iron_losses/corelosscoeffs.m
@@ -1,6 +1,21 @@
 function [kh, kc, ke, beta] = corelosscoeffs (grade, gage, varargin)
 
-    [fq, Bq, Pq] = corelossdata(grade, gage, varargin{:});
+    Inputs.LimitFreqs = [-inf, inf];
+    Inputs.AddZeros = false;
+    % following are inputs for corelossdata.m
+    Inputs.Processing = 'AsSheared';
+    Inputs.InterpolateMissing = true;
+    
+    Inputs = parse_pv_pairs (Inputs, varargin);
+
+    [fq, Bq, Pq] = corelossdata(grade, gage, ...
+                      'Processing', Inputs.Processing, ...
+                      'InterpolateMissing', Inputs.InterpolateMissing );
+
+    % strip frequencies of no interest
+    Bq = Bq(fq > Inputs.LimitFreqs(1) & fq < Inputs.LimitFreqs(2));
+    Pq = Pq(fq > Inputs.LimitFreqs(1) & fq < Inputs.LimitFreqs(2));
+    fq = fq(fq > Inputs.LimitFreqs(1) & fq < Inputs.LimitFreqs(2));
 
     %     kh      kc     ke   beta
     Xo = [0.02, 0.0001, 0.001, 1.5];
