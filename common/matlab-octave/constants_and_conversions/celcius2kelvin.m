15258f668544d802f9461090c5e3591523fc9c7d
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-08-19 12:09:31 +0000

    Added conversions for kelvin and celcius

---
+++ common/matlab-octave/constants_and_conversions/celcius2kelvin.m
@@ -0,0 +1,12 @@
+function K = celcius2kelvin (C)
+% convert a temperature on the celcius scale to the Kelvin scale
+%
+% Syntax
+%
+% K = celcius2kelvin (C)
+%
+%
+
+    K = C + 273.15;
+
+end
---
+++ common/matlab-octave/constants_and_conversions/kelvin2celcius.m
@@ -0,0 +1,12 @@
+function C = kelvin2celcius (K)
+% convert a temperature on the celcius scale to the Kelvin scale
+%
+% Syntax
+%
+% C = kelvin2celcius (K)
+%
+%
+
+    C = K - 273.15;
+
+end
