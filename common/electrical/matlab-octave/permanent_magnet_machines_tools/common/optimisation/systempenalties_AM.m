367440f512ce38f5e09b5d824aae3a9992b699a0
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-08-21 08:30:57 +0000

    Move common penalty functions to new function

---
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/optimisation/systempenalties_AM.m
@@ -0,0 +1,46 @@
+function [score, design, simoptions] = systempenalties_AM(design, simoptions, score)
+% adds penalties related to the operation of a rotary generator system not
+% covered by the electrical penalties
+%
+% Syntax
+%
+% [score, design, simoptions] = systempenalties_AM(design, simoptions, score)
+%
+% 
+    
+    % support legacy code
+    if isfield (simoptions, 'EfficiencyPenFactor') ...
+            && ~isfield (simoptions, 'min_Efficiency_penfactor')
+        simoptions.min_Efficiency_penfactor = simoptions.EfficiencyPenFactor;
+    end
+
+    % efficiency penalty
+    design.EfficiencyPenalty = 0;
+
+    if isfield(simoptions, 'addEfficiencyPenalty') 
+        
+        if ~isempty(simoptions.addEfficiencyPenalty) && simoptions.addEfficiencyPenalty
+            
+            optimiumEfficiency = design.RlVRp / (design.RlVRp + 1);
+
+            simoptions = setfieldifabsent(simoptions, 'min_Efficiency_penfactor', [1, 0]);
+
+            if isscalar(simoptions.min_Efficiency_penfactor)
+                simoptions.min_Efficiency_penfactor = [simoptions.min_Efficiency_penfactor, 0];
+            end
+
+            design.EfficiencyPenalty = ...
+                simoptions.min_Efficiency_penfactor(1) * (optimiumEfficiency-abs(design.Efficiency)) ...
+                  + (simoptions.min_Efficiency_penfactor(2) * (optimiumEfficiency-abs(design.Efficiency)))^2;
+
+            score = score + design.EfficiencyPenalty;
+            
+        end
+        
+    end
+    
+    % apply excessive temperature penalty
+    [score, design, simoptions] = ...
+        addpenalty_AM(design, simoptions, 'upper', 'TemperaturePeak', design.BaseScore, score);
+
+end
--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/optimisation/systempenalties_ROTARY.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/optimisation/systempenalties_ROTARY.m
@@ -8,36 +8,7 @@ function [score, design, simoptions] = systempenalties_ROTARY(design, simoptions
 %
 % 
 
-    % support legacy code
-    if isfield (simoptions, 'EfficiencyPenFactor') ...
-            && ~isfield (simoptions, 'min_Efficiency_penfactor')
-        simoptions.min_Efficiency_penfactor = simoptions.EfficiencyPenFactor;
-    end
-
-    % efficiency penalty
-    design.EfficiencyPenalty = 0;
-
-    if isfield(simoptions, 'addEfficiencyPenalty') 
-        
-        if ~isempty(simoptions.addEfficiencyPenalty) && simoptions.addEfficiencyPenalty
-            
-            optimiumEfficiency = design.RlVRp / (design.RlVRp + 1);
-
-            simoptions = setfieldifabsent(simoptions, 'min_Efficiency_penfactor', [1, 0]);
-
-            if isscalar(simoptions.min_Efficiency_penfactor)
-                simoptions.min_Efficiency_penfactor = [simoptions.min_Efficiency_penfactor, 0];
-            end
-
-            design.EfficiencyPenalty = ...
-                simoptions.min_Efficiency_penfactor(1) * (optimiumEfficiency-abs(design.Efficiency)) ...
-                  + (simoptions.min_Efficiency_penfactor(2) * (optimiumEfficiency-abs(design.Efficiency)))^2;
-
-            score = score + design.EfficiencyPenalty;
-            
-        end
-        
-    end
+    [score, design, simoptions] = systempenalties_AM (design, simoptions, score);
     
     % torque ripple penalty
     [score, design, simoptions] = ...
