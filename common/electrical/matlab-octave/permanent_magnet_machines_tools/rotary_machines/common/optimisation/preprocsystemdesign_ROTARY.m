86bcb7e94e80b2d02a408f2fe6489e51f4534b5c
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-08-26 14:51:03 +0000

    preprocsystemdesign_ROTARY.m: made variable name more descriptive

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/optimisation/preprocsystemdesign_ROTARY.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/optimisation/preprocsystemdesign_ROTARY.m
@@ -1,8 +1,8 @@
-function design = preprocsystemdesign_ROTARY(design, simoptions, powerpoles)
+function design = preprocsystemdesign_ROTARY(design, simoptions, activecoilsperphase)
 % preprocsystemdesign_ROTARY: processes some common aspects of linear
 % machine designs in preparation for evaluation by a GA
     
-    design = preprocsystemdesign_AM(design, simoptions, powerpoles);
+    design = preprocsystemdesign_AM(design, simoptions, activecoilsperphase);
     
 end
 
