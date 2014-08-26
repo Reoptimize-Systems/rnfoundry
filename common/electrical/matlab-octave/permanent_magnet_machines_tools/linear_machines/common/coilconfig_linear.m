b2463a3b64c781759a34d6be1dc2c99b3cea7d08
crobarcro <crobarcro@localhost>
2014-08-26 14:14:01 +0000

    Added new coilconfiguration function and use it in place of coilconfig_linear.m

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/common/coilconfig_linear.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/common/coilconfig_linear.m
@@ -1,38 +1,29 @@
-function [branches, coilsperbranch] = coilconfig_linear(coilsperphase, branchfac)
+function [branches, coilsperbranch] = coilconfig_linear(activecoilsperphase, branchfac)
 % finds a suitible coil configuration given the desired number of machine
 % active coils per phase and a desired fraction of the coils per phase into
 % which coil blocks are to be split
 %
 % Syntax
 %
-% [branches, coilsperbranch] = coilconfig_linear(coilsperphase, branchfac)
+% [branches, coilsperbranch] = coilconfig_linear(activecoilsperphase, branchfac)
 %
 % Input
 %
-%   coilsperphase - number active coils per phase
+%   activecoilsperphase - number active coils per phase
 %
-%   branchfrac - desired fraction of coils per phase to make up a desired
-%   block of coils
+%   branchfrac - This should be a number between 0 and 1. Higher
+%     numbers will result in more parallel branches, and less series coils
+%     per branch. Lower numbers yield less branches and more series coils.
 %
 % Output
 %
 %   branches - closest possible number of parallel branches to desired
-%   fraction of coils per phase
+%     fraction of coils per phase
 %
 %   coilsperbranch - number of series coils in each parallel branch
 %
 
-    % now determine the number of parallel coil branches to use
-    branchcomp = branchfac * coilsperphase;
-    
-    factors = factor2(coilsperphase)';
-    
-    NearestFacStruct = ipdm(branchcomp, factors, ...
-                            'Subset', 'NearestNeighbor', ...
-                            'Result', 'Structure');
-                        
-    branches = factors(NearestFacStruct.columnindex, NearestFacStruct.rowindex);
-    
-    coilsperbranch = coilsperphase / branches;
+    % call common function to do the conversion
+    [branches, coilsperbranch] = branchfac2coilconfig_AM(activecoilsperphase, branchfac);
     
 end
