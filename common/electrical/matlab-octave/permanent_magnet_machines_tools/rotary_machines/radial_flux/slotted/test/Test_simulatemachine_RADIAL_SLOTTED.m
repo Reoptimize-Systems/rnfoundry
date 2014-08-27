495fc31b5e6e612413612c8266cf9e4c87ff3577
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-08-27 19:39:14 +0000

    Test_simulatemachine_RADIAL_SLOTTED.m: various changes, rampoles tes

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/test/Test_simulatemachine_RADIAL_SLOTTED.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/test/Test_simulatemachine_RADIAL_SLOTTED.m
@@ -8,20 +8,29 @@ design = test_design_RADIAL_SLOTTED ('external');
 %%
 design.RlVRp = 10;
 
+[design.CoreLoss.kh, ...
+ design.CoreLoss.kc, ...
+ design.CoreLoss.ke, ...
+ design.CoreLoss.beta ] = corelosscoeffs ('M-19', '29', 'InterpolateMissing', false);
+ 
+ 
 simoptions = struct();
 simoptions.GetVariableGapForce = false;
 
 design = completedesign_RADIAL_SLOTTED (design, simoptions);
 
 % setup simulation options
-simoptions = simsetup_ROTARY(design, 'simfun_RADIAL_SLOTTED', 'finfun_RADIAL_SLOTTED', ...
-                                'Rpm', 30, ...
-                                'TSpan', [0,1]);
+%simoptions = simsetup_ROTARY(design, 'simfun_RADIAL_SLOTTED', 'finfun_RADIAL_SLOTTED', ...
+%                                'Rpm', 30, ...
+%                                'TSpan', [0,1]);
 
+simoptions = simsetup_ROTARY(design, 'simfun_RADIAL_SLOTTED', 'finfun_RADIAL_SLOTTED', ...
+                                'PoleCount', 1000, ...
+                                'RampPoles', 20 );
                             
-simoptions.reltol = 1e-4;
-simoptions.PhaseCurrentTols = repmat(0.001, 1, design.Phases);
-simoptions.maxstep = (simoptions.tspan(2) - simoptions.tspan(1)) / 10000;
+simoptions.reltol = 1e-6;
+%simoptions.PhaseCurrentTols = repmat(0.001, 1, design.Phases);
+%simoptions.maxstep = (simoptions.tspan(2) - simoptions.tspan(1)) / 10000;
 
 simoptions.evaloptions = designandevaloptions_RADIAL_SLOTTED ();
 
@@ -39,22 +48,22 @@ simoptions.finfun = [];
                                                          simoptions.resfun);
                                                          
 %%
-
-fsimoptions = simoptions;
-
-bp = 0.1;
-tmax = 1;
-
-fsimoptions.tspan = [ 0:0.01/100:bp, ...
-                      (bp+0.01/100):(bp+tmax)/1000:(bp+tmax)  ];
-                      
-fsimoptions.odesolver = 'odef1';
-
-[fT, fY, fresults, fdesign, fsimoptions] = simulatemachine_AM(design, ...
-                                                         fsimoptions, ...
-                                                         fsimoptions.simfun, ...
-                                                         fsimoptions.finfun, ...
-                                                         fsimoptions.odeevfun, ...
-                                                         fsimoptions.resfun);
+%
+%fsimoptions = simoptions;
+%
+%bp = 0.1;
+%tmax = 1;
+%
+%fsimoptions.tspan = [ 0:0.01/100:bp, ...
+%                      (bp+0.01/100):(bp+tmax)/1000:(bp+tmax)  ];
+%                      
+%fsimoptions.odesolver = 'odef1';
+%
+%[fT, fY, fresults, fdesign, fsimoptions] = simulatemachine_AM(design, ...
+%                                                         fsimoptions, ...
+%                                                         fsimoptions.simfun, ...
+%                                                         fsimoptions.finfun, ...
+%                                                         fsimoptions.odeevfun, ...
+%                                                         fsimoptions.resfun);
 
                                                      
