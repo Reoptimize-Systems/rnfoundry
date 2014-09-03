abf1b417e4bc56a6d7606778e7f70c196abdf951
crobarcro <crobarcro@localhost>
2014-09-03 11:59:08 +0000

    Fixed messed up characters in two function mfiles

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/dqo.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/dqo.m
@@ -1,5 +1,5 @@
 function idqo = dqo(iabc, theta)
-% performs the direct–quadrature–zero transformation on a set of
+% performs the direct quadrature zero transformation on a set of
 % three-phase currents
 %
 % Syntax
@@ -8,8 +8,8 @@ function idqo = dqo(iabc, theta)
 %
 % Description
 %
-% The direct–quadrature–zero (or dq0 or dqo) transformation or
-% zero–direct–quadrature (or 0dq or odq) transformation is a mathematical
+% The direct quadrature zero (or dq0 or dqo) transformation or
+% zero direct quadrature (or 0dq or odq) transformation is a mathematical
 % transformation used to simplify the analysis of three-phase circuits. In
 % the case of balanced three-phase circuits, application of the dqo
 % transform reduces the three AC quantities to two DC quantities.
@@ -19,20 +19,20 @@ function idqo = dqo(iabc, theta)
 % analysis of three-phase synchronous machines or to simplify calculations
 % for the control of three-phase inverters. The dqo transform is similar to
 % the transform first proposed in 1929 by R.H. Park. In fact, the dqo
-% transform is often referred to as Park’s transformation, although there
+% transform is often referred to as Park s transformation, although there
 % are in fact important differences.
 %
 % Input
 %
 % iabc - vector of three values, the currents in a balanced three phase
-%   system which are to be projected onto the direct–quadrature–zero axes
+%   system which are to be projected onto the direct quadrature zero axes
 %
 % theta - angle between the d axis and first current in iabc
 %
 % Output
 %
 % idqo - vector of three values containing the transformed currents on the
-%   direct–quadrature–zero axes. 
+%   direct quadrature zero axes. 
 
     if ~(isvector(iabc) && numel(iabc) == 3 && all(isreal(iabc)))
         error('DQO:badcurrentinput', 'i must be a vector of three real values.')
