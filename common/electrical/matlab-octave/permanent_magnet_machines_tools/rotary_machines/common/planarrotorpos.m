966377d3e26d6887be029585e8860a7125dfbd74
crobarcro <3159692+crobarcro@users.noreply.github.com>
2014-09-03 12:14:32 +0000

    planarrotorpos.m: added help

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/planarrotorpos.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/planarrotorpos.m
@@ -1,6 +1,35 @@
 function position = planarrotorpos(ypole, position, fracpolepos, rotoranglepos)
+% calculates the relative rotor position in units of fractions of a pole
+% width
+%
+% Syntax
+%
+% position = planarrotorpos(ypole, position, fracpolepos, rotoranglepos)
+%
+% Input
+%
+%  ypole - pole width in the same units as 'position'
+%
+%  position - scalar relative position of field component to armature. Can
+%    be empty, in which case fracpolepos is checked next. If not empty,
+%    position is simply passed through to the output.
+%
+%  fracpolepos - scalar relative position of field component to armature as
+%    a ratio of pole widths. Can be empty, in which case rotoranglepos is
+%    checked next. Position is calculated as fracpolepos * ypole.
+%
+%  rotoranglepos - two element vector. The first number should be the
+%    number of poles in the machine, the second the absolute position of a
+%    rotor in radians.
+%
+% Output
+%
+%  position - the relative rotor position in units of fractions of a pole
+%    width
+%
 
-    
+% Copyright 2012 - 2014 Richard Crozier 
+
 
     if ~isempty(fracpolepos) && ~isempty(rotoranglepos) 
         
