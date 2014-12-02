7f799f6cdc85d3d28ec6694bd27c26c2a09ce194
crobarcro <crobarcro@localhost>
2014-12-02 22:22:06 +0000

    Added functions for exporting design info to pythin dict

---
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/reporting/design2pydict_RADIAL.m
@@ -0,0 +1,19 @@
+function cellstrs = design2pydict_RADIAL (design)
+
+    cellstrs = design2pydict_ROTARY (design);
+    
+    % create a function for copy-paste convenience
+    % create a function for copy-paste convenience
+    spm = @(var, val) sprintf ('structdims["Field"]["%s"] = %10.8g * 1000', var, val);
+    spa = @(var, val) sprintf ('structdims["Field"]["%s"] = %10.8g', var, val);
+    
+    cellstrs = [cellstrs;
+py.trynesteddict('structdims', {'Field', 'Rbi'}, sprintf('%10.8g * 1000', design.Rbi)); 
+{ ...
+ spm('Rbo', design.Rbo);
+[spm('Rmi', design.Rmi), ' * 1.001'];
+ spm('Rmo', design.Rmo);
+}];
+
+
+end
