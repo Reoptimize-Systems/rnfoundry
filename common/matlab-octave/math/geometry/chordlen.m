fb8730fbf7d85ef5f94afd28316dc10b63a0079d
crobarcro <crobarcro@localhost>
2015-01-12 16:46:57 +0000

    chordlen.m: added

---
+++ common/matlab-octave/math/geometry/chordlen.m
@@ -0,0 +1,22 @@
+function len = chordlen (r, theta)
+% returns the length of the chord with given radius and segment angle
+%
+% Input
+%
+%   r - matrix of radii at which the chord length is to be calculated
+%
+%   theta - matrix of span angles
+%
+%
+% Output
+%
+%   len - the chord length
+%
+
+    if any (theta(:) <= 0 || theta(:) > 180)
+        error ('theta must be between 0 and 180')
+    end
+    
+    len = 2 .* r .* sin ( theta ./ 2 );
+
+end
