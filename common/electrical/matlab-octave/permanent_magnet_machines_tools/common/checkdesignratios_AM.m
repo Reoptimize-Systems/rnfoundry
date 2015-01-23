29b19b3095203b4a6c2839a93f1e308bec28abc4
crobarcro <3159692+crobarcro@users.noreply.github.com>
2015-01-23 17:28:41 +0000

    Added option to dump the design struct on getting an impossible ratio, and updated completedesign_RADIAL_SLOTTED.m to use it

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/checkdesignratios_AM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/checkdesignratios_AM.m
@@ -1,10 +1,14 @@
-function design = checkdesignratios_AM (design, ratiospec)
+function design = checkdesignratios_AM (design, ratiospec, dumpstruct)
 % checks design ratios are within specified limits
+
+    if nargin < 3
+        dumpstruct = false;
+    end
     
     emax = @(fn) fprintf (1, 'Ratio field %s exceeded the specified maximum possible allowed\n', fn);
     emin = @(fn) fprintf (1, 'Ratio field %s is below the specified minimum possible allowed\n', fn);
     
-    results = checkstructratios (design, ratiospec);
+    results = checkstructratios (design, ratiospec, dumpstruct);
     
     for ind = 1:size (ratiospec, 1)
         
@@ -20,6 +24,11 @@ function design = checkdesignratios_AM (design, ratiospec)
     end
     
     if any (results ~= 0)
+        
+        if dumpstruct
+            dispstruct (design);
+        end
+        
         error ('RENEWNET:pmmachines:designspec', ...
                'Impossible design spec');
     end
@@ -39,11 +48,17 @@ function result = checkstructratios (S, ratiospec)
                 
                 result(ind) = result(ind) + 1;
                 
+                fprintf (1, 'Struct Ratio %s was %f, which was less than the specified min possible allowed (%f)\n', ...
+                            ratiospec{ind,1}, ratiospec{ind,2}, S.(ratiospec{ind,1}));
+                
             end
             
             if  S.(ratiospec{ind,1}) > ratiospec{ind,3}
                 
                 result(ind) = result(ind) + 2;
+                
+                fprintf (1, 'Struct Ratio %s was %f, which was less than the specified min possible allowed (%f)\n', ...
+                            ratiospec{ind,1}, ratiospec{ind,3}, S.(ratiospec{ind,1}));
                 
             end
             
