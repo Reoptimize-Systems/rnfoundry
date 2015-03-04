d07c142295405ba7547d0176c5b7dccea168db20
crobarcro <3159692+crobarcro@users.noreply.github.com>
2015-03-04 11:10:04 +0000

    made coil turn and conductor diamter calculations vectorised

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/CoilTurns.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/CoilTurns.m
@@ -31,19 +31,20 @@ function [N, dc] = CoilTurns(area, CoilFillFactor, dc)
 
     % Calculate the full conductor cross-sectional area, including the
     % insulation
-    Ac = pi * (fulldc/2)^2;
+    Ac = pi .* (fulldc./2).^2;
+    
+    N = ones (size (dc)) * nan;
     
     % determine if we can fit any turns in
-    if Ac > (area * CoilFillFactor)
-        % If we cannot, we must modify the conductor diameter until at
-        % least one does. 
-        N = 1;
-        % Allow 1% for insulation, this calculation of dc will return a
-        % value in m, so no need to convert back
-        dc = 0.99 * sqrt(4 * area * CoilFillFactor / pi);
-    else
-        % If we can fit turns in, calculate how many
-        N = round((area * CoilFillFactor) / Ac);
-    end
+    
+    % If we cannot, we must modify the conductor diameter so that at
+    % least one does.
+    N(Ac > (area * CoilFillFactor)) = 1;
+    % Allow 1% for insulation, this calculation of dc will return a
+    % value in m, so no need to convert back
+    dc(Ac > (area * CoilFillFactor)) = 0.99 * sqrt(4 * area * CoilFillFactor / pi);
+
+    % If we can fit turns in, calculate how many
+    N(Ac < (area * CoilFillFactor)) = round((area * CoilFillFactor) ./ Ac(Ac < (area * CoilFillFactor)));
     
 end
--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/conductord2wired.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/conductord2wired.m
@@ -20,16 +20,23 @@ function fulldc = conductord2wired(conductordc)
     % Convert dc to mm for polynomials (poly input variables are in mm)
     dc = conductordc * 1000;
     
-    if dc < 1.6
-        % First polynomial covers conductor diameter from 0.1 mm to 1.6 mm.
-        fulldc = dc * (1 + (-0.1135*dc^5 + 1.6055*dc^4 - 8.5416*dc^3 + 21.481*dc^2 - 27.039*dc + 18.561)/100);
-    else
-        % For greater than 1.6 mm, a power fit was used so it can be
-        % extrapolated to larger diameter wire 
-        fulldc = dc * (1 + (5.9131 * dc^(-0.6559))/100); 
-    end
+    fulldc = ones (size (dc)) * nan;
     
+    % First polynomial covers conductor diameter from 0.1 mm to 1.6 mm.
+    fulldc(dc < 1.6) = dc(dc < 1.6) ...
+        .* ( 1 + (-0.1135.*dc(dc < 1.6).^5 ...
+                 + 1.6055.*dc(dc < 1.6).^4 ...
+                 - 8.5416.*dc(dc < 1.6).^3 ...
+                 + 21.481.*dc(dc < 1.6).^2 ...
+                 - 27.039.*dc(dc < 1.6) ...
+                 + 18.561 ) ...
+               ./ 100);
+    
+    % For greater than 1.6 mm, a power fit was used so it can be
+    % extrapolated to larger diameter wire 
+    fulldc(dc >= 1.6) = dc(dc >= 1.6) .* (1 + (5.9131 * dc(dc >= 1.6).^(-0.6559))./100); 
+
     % convert the conductor diameter from mm to m
-    fulldc = fulldc / 1000;
+    fulldc = fulldc ./ 1000;
     
 end
