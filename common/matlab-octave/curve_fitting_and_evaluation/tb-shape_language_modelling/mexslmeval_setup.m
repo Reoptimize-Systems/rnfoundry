c24194ae976dbd8ddea1a31f775232056e49d795
crobarcro <crobarcro@localhost>
2015-03-04 12:22:16 +0000

    mexslmeval_setup.m: wrapped mex call in try catch

--- common/matlab-octave/curve_fitting_and_evaluation/tb-shape_language_modelling/mexslmeval_setup.m
+++ common/matlab-octave/curve_fitting_and_evaluation/tb-shape_language_modelling/mexslmeval_setup.m
@@ -1,9 +1,15 @@
 function mexslmeval_setup ()
+% creates the mexslmeval mex file
 
     CC = onCleanup (@() cd(pwd));
     
     cd(getmfilepath (mfilename));
 
-    mex mexslmeval.cpp -lgsl -lgslcblas
+    try
+        % note the order of the linking commands seams to matter here
+        mex mexslmeval.cpp -lgsl -lgslcblas
+    catch
+        warning ('mexslmeval compilation failed, you may be missing required libraries, gsl and gslcblas');
+    end
 
 end
