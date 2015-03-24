0038a033281269029c7086e31cf4cd0e3745e76b
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2015-03-24 14:38:46 +0000

    resfun_RADIAL.m: added air gap shear stress calculation

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/odesim/resfun_RADIAL.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/odesim/resfun_RADIAL.m
@@ -11,10 +11,14 @@ function [results, design] = resfun_RADIAL(T, Y, design, simoptions)
 %
 % 
 
-% Copyright Richard Crozier 2014
+% Copyright Richard Crozier 2014-2015
 
 
     [results, design] = resfun_ROTARY(T, Y, design, simoptions);
+    
+    % calculate the shear stress at the air gap
+    R = (design.Rmo+design.g/2);
 
+    design.ShearStressMean = (abs (design.TorquePtoMean) / R) / (2 * pi * R * design.ls);
 
 end
