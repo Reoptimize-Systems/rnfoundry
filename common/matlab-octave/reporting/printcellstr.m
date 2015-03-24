db122609cca6dadb8842dfab051a12b37f7d6e9e
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2015-03-24 11:39:42 +0000

    printcellstr.m: added option to print to file

--- common/matlab-octave/reporting/printcellstr.m
+++ common/matlab-octave/reporting/printcellstr.m
@@ -1,23 +1,31 @@
-function printcellstr(C)
-% print all the strings in a cell string array to the command line, each on
-% a new line
+function printcellstr(C, fid)
+% print all the strings in a cell string array to the command line, or a
+% file, each on a new line
 %
 % Syntax
 %
 % printcellstr(C)
+% printcellstr(C,fid)
 %
 % Input
 %
 %   C - a cell array of strings to be printed
 %
+%   fid - optional file id to print to, will print to comamnd line if not
+%     supplied.
+%
 
     if ~iscellstr(C)
         error('input must be a cell array of strings');
     end
+    
+    if nargin < 2
+        fid = 1;
+    end
 
     for ind = 1:numel(C)
         
-       fprintf(1, '%s\n', C{ind});
+       fprintf(fid, '%s\n', C{ind});
        
     end
 
