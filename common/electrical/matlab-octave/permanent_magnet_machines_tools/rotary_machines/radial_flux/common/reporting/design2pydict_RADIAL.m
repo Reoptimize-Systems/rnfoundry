654804806ba05884e77e22e4bba538467b2eb85f
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2015-04-17 16:45:19 +0100

    Increased precision with which parameters were written

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/reporting/design2pydict_RADIAL.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/common/reporting/design2pydict_RADIAL.m
@@ -4,11 +4,11 @@ function cellstrs = design2pydict_RADIAL (design)
     
     % create a function for copy-paste convenience
     % create a function for copy-paste convenience
-    spm = @(var, val) sprintf ('structdims["Field"]["%s"] = %10.8g * 1000', var, val);
-    spa = @(var, val) sprintf ('structdims["Field"]["%s"] = %10.8g', var, val);
+    spm = @(var, val) sprintf ('structdims["Field"]["%s"] = %.17e * 1000', var, val);
+    spa = @(var, val) sprintf ('structdims["Field"]["%s"] = %.17e', var, val);
     
     cellstrs = [cellstrs;
-pytools.trynesteddict('structdims', {'Field', 'Rbi'}, sprintf('%10.8g * 1000', design.Rbi)); 
+pytools.trynesteddict('structdims', {'Field', 'Rbi'}, sprintf('%.17e * 1000', design.Rbi)); 
 { ...
  spm('Rbo', design.Rbo);
 [spm('Rmi', design.Rmi), ' * 1.0'];
