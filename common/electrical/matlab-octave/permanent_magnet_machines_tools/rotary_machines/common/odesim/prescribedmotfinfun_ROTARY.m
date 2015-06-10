ef7709dd6061bc41d44cafc74eeecc83d6fcddd6
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2015-06-10 12:05:38 +0100

    prescribedmotfinfun_ROTARY.m: check for right field name

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/odesim/prescribedmotfinfun_ROTARY.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/odesim/prescribedmotfinfun_ROTARY.m
@@ -31,7 +31,7 @@ function [design, simoptions] = prescribedmotfinfun_ROTARY(design, simoptions, f
     % values set in finfun, if any, are used
     simoptions = rmiffield(simoptions, 'abstol');
     
-    if ~all(isfield(design, {'slm_psidot'}))
+    if ~all(isfield(design, {'slm_fluxlinkage'}))
         % In this case we assume we have not already run the finalisation
         % code on this design and must do so
         [design, simoptions] = feval(finfun, design, simoptions);
