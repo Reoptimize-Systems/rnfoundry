9fee73ae89a2ce0d9803d6e0a42ccd662283e5ec
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2015-07-04 18:22:53 +0100

    added litre m3 conversion fcns

---
+++ common/matlab-octave/constants_and_conversions/cubicmetres2litres.m
@@ -0,0 +1,10 @@
+function litres = cubicmetres2litres (m3)
+% converts cubic metres to litres
+%
+% litres = cubicmetres2litres (m3)
+%
+% 
+
+    litres  = m3 * 1e3;
+
+end
---
+++ common/matlab-octave/constants_and_conversions/litres2cubicmetres.m
@@ -0,0 +1,10 @@
+function m3 = litres2cubicmetres (litres)
+% converts litres to cubic metres
+%
+% m3 = litres2cubicmetres (litres)
+%
+% 
+
+    m3 = litres * 1e-3;
+
+end
