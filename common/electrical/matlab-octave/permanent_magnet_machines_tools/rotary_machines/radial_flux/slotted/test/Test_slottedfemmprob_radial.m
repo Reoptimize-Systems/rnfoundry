ff5b53cbcbbf8e27a4503ccd427727076944ece2
Richard Crozier <a@b.c>
2015-10-02 16:48:27 +0100

    Test_slottedfemmprob_radial.m: added test cases and minor modifications

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/test/Test_slottedfemmprob_radial.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/test/Test_slottedfemmprob_radial.m
@@ -16,7 +16,23 @@ design.MagFEASimMaterials.CoilInsulation = 'Air';
 [FemmProblem, coillabellocs] = ...
     slottedfemmprob_radial ( design, ...
                              'ArmatureType', design.ArmatureType, ...
-                             'DrawCoilInsulation', true );
+                             'DrawCoilInsulation', true, ...
+                             'NPolePairs', 2);
+
+plotfemmproblem (FemmProblem);
+% openprobleminfemm_mfemm (FemmProblem);
+
+%% Full number of machine poles (but not 'Full' DrawingType)
+
+design.ShoeCurveControlFrac = 0.1;
+design.CoilInsulationThickness = 2/10000;
+design.MagFEASimMaterials.CoilInsulation = 'Air';
+
+[FemmProblem, coillabellocs] = ...
+    slottedfemmprob_radial ( design, ...
+                             'ArmatureType', design.ArmatureType, ...
+                             'DrawCoilInsulation', true, ...
+                             'NPolePairs', 34);
 
 plotfemmproblem (FemmProblem);
 % openprobleminfemm_mfemm (FemmProblem);
@@ -27,7 +43,8 @@ plotfemmproblem (FemmProblem);
     slottedLfemmprob_radial (design, ...
                              'ArmatureType', design.ArmatureType );
 
-openprobleminfemm_mfemm (FemmProblem);
+plotfemmproblem (FemmProblem);
+% openprobleminfemm_mfemm (FemmProblem);
 
 %%
 [FemmProblem, coillabellocs] = slottedLfemmprob_radial (design, 'ArmatureType', design.ArmatureType);
@@ -66,4 +83,34 @@ design = completedesign_RADIAL_SLOTTED (design);
                              'DrawCoilInsulation', false, ...
                              'DrawOuterRegions', true );
                              
+openprobleminfemm_mfemm (FemmProblem);
+
+
+%%
+
+design.ShoeCurveControlFrac = 0.1;
+design.CoilInsulationThickness = 2/10000;
+design.MagFEASimMaterials.CoilInsulation = 'Air';
+
+[FemmProblem, coillabellocs] = ...
+    slottedfemmprob_radial ( design, ...
+                             'ArmatureType', design.ArmatureType, ...
+                             'DrawCoilInsulation', false, ...
+                             'NPolePairs', 10, ...
+                             'DrawingType', 'BoundarySwap');
+
+plotfemmproblem (FemmProblem);
+% openprobleminfemm_mfemm (FemmProblem);
+
+%%
+
+[FemmProblem, coillabellocs] = ...
+    slottedfemmprob_radial ( design, ...
+                             'ArmatureType', design.ArmatureType, ...
+                             'DrawCoilInsulation', false, ...
+                             'NPolePairs', 1, ...
+                             'DrawingType', 'BoundarySwap', ...
+                             'BoundaryShift', 4);
+
+% plotfemmproblem (FemmProblem);
 openprobleminfemm_mfemm (FemmProblem);
