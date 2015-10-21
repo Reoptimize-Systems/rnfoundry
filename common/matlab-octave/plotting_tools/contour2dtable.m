dd512fe68b989e9c2f3aa5b7570286fe29607604
Richard Crozier <a@b.c>
2015-10-21 13:51:53 +0100

    added contour2dtable.m

---
+++ common/matlab-octave/plotting_tools/contour2dtable.m
@@ -0,0 +1,42 @@
+function [xq,yq,zq] = contour2dtable(x, y, data, varargin)
+% creates a mesh plot of the data in a 2D table
+%
+% Syntax
+%
+% contour2dtable(x, y, data, ...)
+%
+% Inputs
+% 
+%  x - vector of values representing the independent variable variables of
+%   the rows of the table, must be linearly spaced
+%
+%  y - vector of values representing the independent variable variables of
+%   the columns of the table, must be linearly spaced
+%
+%  data - (2 x 2) matrix of data containing the dependent variables
+%    corresponding to each combination of the independent values provided
+%    in the x and y vectors
+%
+% Example
+%
+% x = 1:10
+% y = 0.5:0.5:5
+% data = bsxfun(@times, x, y')
+% contour2dtable(x, y, data)
+%
+% See also, mesh
+%
+
+% Copyright Richard Crozier 2015
+
+    [newx, newy, newz] = makevars2dtable23dplot(x, y, data);
+    
+    % now mesh the data
+    [xq,yq] = meshgrid(x,y);
+    
+    zq = griddata(newx, newy, newz, xq, yq);
+    
+    % and create the mesh plot
+    contour(xq,yq,zq, varargin{:});
+
+end
