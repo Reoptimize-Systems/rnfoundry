a68ac93042e9137feff124bd0fde9e67a9f46a3c
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2015-11-15 11:41:48 +0000

    Test_uncurvedstatorhalf2dfemmprob.m: removed redundant test

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/test/Test_uncurvedstatorhalf2dfemmprob.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/test/Test_uncurvedstatorhalf2dfemmprob.m
@@ -28,33 +28,3 @@ writefemmfile(filename, FemmProblem)
 openfemm;
 
 opendocument(fullfile(pwd, filename))
-
-
-%% test any number of slots
-
-slots = 2 * 21;
-Poles = 28;
-ypole = 1;
-yslot = ypole / (slots / Poles);
-ycoil = yslot * 0.8;
-yshoegap = 0.2 * ycoil;
-xyoke = 0.05;
-xcoil = 0.3;
-xshoebase = 0.05;
-xshoegap = 0.025;
-xoffset = 0;
-side = 'l';
-
-[FemmProblem, outernodes, coillabellocs] = ...
-    uncurvedstatorhalf2dfemmprob(nslots, yslot, ypole, ycoil, yshoegap, ...
-                                  xyoke, xcoil, xshoebase, xshoegap, xoffset, side, ...
-                                  'NWindingLayers', 2, ...
-                                  'NSlots', 4);
-
-filename = 'test.fem';
-
-writefemmfile(filename, FemmProblem)
-
-openfemm;
-
-opendocument(fullfile(pwd, filename))
