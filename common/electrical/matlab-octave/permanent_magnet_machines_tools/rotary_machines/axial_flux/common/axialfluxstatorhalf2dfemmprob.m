879532f384fda4de8330768956d6cd6549ec78d3
Richard Crozier <a@b.c>
2015-11-21 11:00:59 +0000

    axialfluxstatorhalf2dfemmprob.m: completed converson to use of common subfunction

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/axial_flux/common/axialfluxstatorhalf2dfemmprob.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/axial_flux/common/axialfluxstatorhalf2dfemmprob.m
@@ -17,14 +17,22 @@ function [FemmProblem, outernodes, coillabellocs, slotinfo] = axialfluxstatorhal
     Inputs.ToothRegionMeshSize = -1;
     Inputs.ShoeGapMaterial = 1;
     Inputs.ShoeGapRegionMeshSize = -1;
-    Inputs.SlotPositions = [];
-    Inputs.NSlots = [];
     Inputs.Tol = 1e-5;
     Inputs.CoilBaseFraction = 0.05;
     Inputs.ShoeCurveControlFrac = 0.5;
     Inputs.SplitX = false;
     Inputs.DrawCoilInsulation = false;
     Inputs.CoilInsulationThickness = 0;
+    
+    % check an integer number of machine Poles and slots (can't have half a
+    % slot or a pole in a machine
+    slots = round(slots);
+    Poles = round(Poles);
+    
+    slotsperpole = slots / Poles;
+    
+    % make enugh slots for two poles by default
+    Inputs.NSlots = slotsperpole * 2;
     
     Inputs = parse_pv_pairs(Inputs, varargin);
     
@@ -34,40 +42,8 @@ function [FemmProblem, outernodes, coillabellocs, slotinfo] = axialfluxstatorhal
     % remove the odl one to save memory
     Inputs = rmfield(Inputs, 'FemmProblem');
     
-    % check an integer number of machine Poles and slots (can't have half a
-    % slot or a pole in a machine
-    slots = round(slots);
-    Poles = round(Poles);
-
-    slotsperpole = slots / Poles;
-
-    % The user can either supply a set of slot positions (useful for
-    % inductance simulations), or these will be calculated to fill two
-    % Poles of the machine
-    if isempty(Inputs.SlotPositions)
-        
-        if isempty(Inputs.NSlots)
-            
-            toothpos = 0:1/slotsperpole:2;
-
-        else
-            
-            % draw a set number of slots
-            toothpos = linspace(0, Inputs.NSlots*1/slotsperpole, Inputs.NSlots+1);
-
-        end
-        
-        % distribute the slots between the denormalised tooth positions, and
-        % add a full slot separation as the first slot is drawn split along the
-        % line y = 0
-        slotpos = toothpos(1:end-1)*ypole + (ypole / slotsperpole / 2);
-
-    else
-        slotpos = Inputs.SlotPositions;
-    end
-    
     [FemmProblem, outernodes, coillabellocs, slotinfo] = ...
-        uncurvedstatorhalf2dfemmprob ( numel(slotpos), ...
+        uncurvedstatorhalf2dfemmprob ( Inputs.NSlots, ...
                                        ypole / slotsperpole, ...
                                        ycoil, ...
                                        yshoegap, ...
