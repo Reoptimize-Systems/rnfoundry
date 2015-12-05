883df477b61d8898a826eef4149cb3849ec512de
Richard Crozier <a@b.c>
2015-12-05 16:30:32 +0000

    Test_simfun_RADIAL_SLOTTED.m: add internal armature case

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/test/Test_simfun_RADIAL_SLOTTED.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/test/Test_simfun_RADIAL_SLOTTED.m
@@ -23,3 +23,32 @@ simoptions.evaloptions = designandevaloptions_RADIAL_SLOTTED ();
 
 [design, simoptions] = finfun_RADIAL_SLOTTED(design, simoptions);
 
+
+
+%% Internal armature
+%
+%
+
+clear design simoptions 
+
+design = test_design_RADIAL_SLOTTED ('internal');
+
+%%
+design.RlVRp = 10;
+
+simoptions = struct();
+simoptions.GetVariableGapForce = false;
+
+design = completedesign_RADIAL_SLOTTED (design, simoptions);
+
+[design, simoptions] = simfun_RADIAL_SLOTTED(design, simoptions);
+fprintf(1, 'done simfun\n');
+
+%%
+
+simoptions.evaloptions = designandevaloptions_RADIAL_SLOTTED ();
+
+[design, simoptions] = finfun_RADIAL_SLOTTED(design, simoptions);
+
+
+
