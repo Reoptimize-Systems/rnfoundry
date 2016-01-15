49fc61174f6b51beb2b43cc09444fefa57af4ba3
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-01-15 08:36:02 +0000

    Don't store all the phase resistances during ode sim  by default

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/odesim/resfun_AM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/odesim/resfun_AM.m
@@ -8,19 +8,39 @@ function [results, design] = resfun_AM(T, Y, design, simoptions)
 %
 % Description
 %
-% TODO: add description
+% resfun_AM performs a number of post simulation tasks common to all
+% machines including the regeneration and post-processing of results.
 %
+% Input
+%
+%  T - 
+%
+%  Y - 
+%
+%  design - 
+%
+%  simoptions - structure containing 
+%    The following fields must be present in the simoptions structure:
+%
+%    The following optional fields may also be present in the structure:
 %
 % See also: oderesults.m
 %
 
+    % set some results related options to defaults
+    
+    simoptions = setfieldifabsent (simoptions, 'CopyPhaseResistance', false);
+    % SkipOutputFields: cell array of strings containing fields to be
+    % skipped when getting the output values
+    simoptions = setfieldifabsent (simoptions, 'SkipOutputFields', {});
+    
     % extract the internally calculated results from the ode simulation
     % function. It must be coded so that when called with no arguments it
     % returns a cell array of strings which will become the names of fields
     % in a results structure. The order of the strings must be the order of
     % the return arguments of the ode function when called with more than
     % one input
-    results = oderesults(T, Y, simoptions.odeevfun, {design, simoptions}, simoptions.skip);
+    results = oderesults(T, Y, simoptions.odeevfun, {design, simoptions}, simoptions.skip, simoptions.SkipOutputFields);
     
     % store the actual simulation time taken
     design.SimTimeSpan = max(T) - simoptions.tspan(1);
@@ -28,15 +48,18 @@ function [results, design] = resfun_AM(T, Y, design, simoptions)
     % we should use the phase that produced the highest current
     [C,I] = max(max(abs(Y(:,simoptions.ODEPhaseCurrentCol:(simoptions.ODEPhaseCurrentCol-1+design.Phases))), [], 1));
     
-    if isfield(results, 'RPhase')
+    if isfield(results, 'RPhase') &&  simoptions.CopyPhaseResistance
         design.PhaseResistance = results.RPhase;
         results = rmfield(results, 'RPhase');
+    else
+        results.RPhase = design.PhaseResistance;
     end
     
     % Determine some interesting machine electrical outputs
     design = odeelectricalresults(T, ...
                                   Y(:,simoptions.ODEPhaseCurrentCol:simoptions.ODEPhaseCurrentCol-1+design.Phases), ...
                                   results.EMF, ...
+                                  results.RPhase, ...
                                   design, ...
                                   simoptions);
 
