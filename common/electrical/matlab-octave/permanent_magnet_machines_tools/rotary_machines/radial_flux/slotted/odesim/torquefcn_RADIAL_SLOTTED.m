f933c743a4eb9bc67a864eba7d7684b47b350e12
Richard Crozier <a@b.c>
2016-02-03 13:31:40 +0000

    torquefcn_RADIAL_SLOTTED.m: changed style only

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/odesim/torquefcn_RADIAL_SLOTTED.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/radial_flux/slotted/odesim/torquefcn_RADIAL_SLOTTED.m
@@ -1,4 +1,4 @@
-function [Torque, TorqueBD] = torquefcn_RADIAL_SLOTTED(design, simoptions, thetaE, omegaE, EMF, Iphases)
+function [Torque, TorqueBD] = torquefcn_RADIAL_SLOTTED (design, simoptions, thetaE, omegaE, EMF, Iphases)
 % calculates forces for a linearised rotary machine dynamic simulation
 %
 % Syntax
@@ -7,12 +7,12 @@ function [Torque, TorqueBD] = torquefcn_RADIAL_SLOTTED(design, simoptions, theta
 %
 % 
 
-    [Torque, TorqueBD] = torquefcn_RADIAL(design, simoptions, thetaE, omegaE, EMF, Iphases);
+    [Torque, TorqueBD] = torquefcn_RADIAL (design, simoptions, thetaE, omegaE, EMF, Iphases);
     
     thetaR = thetaE ./ design.PoleWidth;
     
     % calculate the cogging torque
-    TorqueBD(end+1) =  periodicslmeval(thetaR,design.slm_coggingtorque,0,false);
+    TorqueBD(end+1) =  periodicslmeval (thetaR, design.slm_coggingtorque, 0, false);
     
     % add it to the total torque
 %     Torque = Torque + TorqueBD(end);
