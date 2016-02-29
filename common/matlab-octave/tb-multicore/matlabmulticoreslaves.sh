c505b11d274a0e1b90f3caa1fc550d8d7e0d14e9
Richard Crozier <a@b.c>
2016-02-29 12:06:13 +0000

    Added automatic slave spawning code

---
+++ common/matlab-octave/tb-multicore/matlabmulticoreslaves.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "The multicore directory:"
+echo $1 # the multicore directory
+echo "The quit date for the slave:"
+echo $2 # the quit date for the slave
+echo "The start directory for the process"
+echo $3 # the start directory for the process
+echo "The delay before starting the process"
+echo $4 # the start directory for the process
+
+echo waiting $4 seconds
+sleep $4
+
+echo "Running Maltab without jvm"
+# Start the matlab process with no gui, no java (until this is fixed),
+# and limited to a single thread
+matlab -nodesktop -nosplash -nojvm -singleCompThread -r "cd('$3');disp(pwd);ls;startup;disp('Successfully Completed Startup');startmulticoreslave2('$1', 1, datenum($2));quit;"
+
