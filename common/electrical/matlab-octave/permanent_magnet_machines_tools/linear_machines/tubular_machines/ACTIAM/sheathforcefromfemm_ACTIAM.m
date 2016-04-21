5e71480083c8944c757e30f5a9743cd0a4caa83c
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-04-21 16:54:50 +0100

    sheathforcefromfemm_ACTIAM.m: update to use xfemm instead of FEMM

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/ACTIAM/sheathforcefromfemm_ACTIAM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/ACTIAM/sheathforcefromfemm_ACTIAM.m
@@ -1,4 +1,4 @@
-function [force, stress] = sheathforcefromfemm_ACTIAM(design)
+function [force, stress] = sheathforcefromfemm_ACTIAM (design, displ)
 % determines the force on the sheath of an ACTIAM using FEMM
 %
 % Syntax
@@ -7,34 +7,48 @@ function [force, stress] = sheathforcefromfemm_ACTIAM(design)
 %
 % 
 
-    RunStructFEMMSimNew_ACTIAM(design);
+    design.FemmProblem = femmprob_ACTIAM(design);
+
+    % analyse
+    [ansfilename, femfilename] = analyse_mfemm (design.FemmProblem, ...
+                                                false, ...
+                                                true);
+    % load solution
+    solution = fpproc(ansfilename);
     
-    mi_analyse(1); mi_loadsolution;
+    delete (femfilename);
     
     %[sheathForce, Area, NormSheathForce] = FEMMGetSheathForce(design.WpVRm, design.RoVRm, design.Rm, 'm');
     
     % Extract the total energy stored in the magnetic field
-    fEnergy = mo_TotalFieldEnergy;
-    
-    mo_close; mi_close;
+    fEnergy = [ solution.totalfieldenergy(), solution.totalfieldcoenergy()];
     
     % Reduce the air gap and perform the sim again
-    displ = design.Ro * 0.01;
+    if nargin < 2
+        displ = design.Ro * 0.01;
+    end
     
     design.Ro = design.Ro - displ;
     design.Ra = design.Ra - displ;
     
     design = dimensions2ratios_ACTIAM(design);
     
-    RunStructFEMMSimNew_ACTIAM(design);
+    design.FemmProblem = femmprob_ACTIAM(design);
+
+    % analyse
+    [ansfilename, femfilename] = analyse_mfemm (design.FemmProblem, ...
+                                                false, ...
+                                                true);
+    % load solution
+    solution = fpproc(ansfilename);
     
-    mi_analyse(1); mi_loadsolution;
+    delete (femfilename);
     
-    fEnergy(1,3:4) = mo_TotalFieldEnergy;
-    
-    mo_close; mi_close;
+    fEnergy(1,3:4) = [ solution.totalfieldenergy(), solution.totalfieldcoenergy()];
     
     force = (fEnergy(1,4)-fEnergy(1,2)) ./ displ;
+    
+    Area = 2 * pi() * design.Ro * design.Wp;
     
     stress = force / Area;
 
