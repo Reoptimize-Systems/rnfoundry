3e23a6239f9ed5113de4ef27a9168b1bd51628c9
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-04-27 08:42:56 +0100

    preprocsystemdesign_AM.m: base min and max wire sizes on strand size not overall equivalenet conductor area

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/optimisation/preprocsystemdesign_AM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/optimisation/preprocsystemdesign_AM.m
@@ -7,26 +7,30 @@ function design = preprocsystemdesign_AM(design, simoptions, activecoilsperphase
     design = setfieldifabsent(design, 'LgVLc', 0);
     
     % make minimum possible wire diameter 0.5 mm if not already set
-    simoptions = setfieldifabsent(simoptions, 'MinWireDiameter',  0.5/1000);
+    simoptions = setfieldifabsent(simoptions, 'MinStrandDiameter',  0.5/1000);
     % set infinite max wire diameter if not yet set
-    simoptions = setfieldifabsent(simoptions, 'MaxWireDiameter',  inf);
+    simoptions = setfieldifabsent(simoptions, 'MaxStrandDiameter',  inf);
     
-    if simoptions.MaxWireDiameter < simoptions.MinWireDiameter
+    if simoptions.MaxStrandDiameter < simoptions.MinWireDiameter
         error ('In simoptions, maximum allowed wire size is less than minimum allowed wire size.')
     end
     
     design.Dc = sqrt(4 * (design.Hc * design.Wc * design.CoilFillFactor * design.DcAreaFac) / pi);
     
-    if design.Dc < simoptions.MinWireDiameter
-        design.Dc = simoptions.MinWireDiameter;
+    design.WireStrandDiameter = stranddiameter (design.Dc, design.NStrands);
+    
+    if design.WireStrandDiameter < simoptions.MinStrandDiameter
+        design.WireStrandDiameter = simoptions.MinStrandDiameter;
+        design.Dc = equivDcfromstranded (design.WireStrandDiameter, design.NStrands);
     end
     
-    if design.Dc > simoptions.MaxWireDiameter
-        design.Dc = simoptions.MaxWireDiameter;
+    if design.WireStrandDiameter > simoptions.MaxStrandDiameter
+        design.WireStrandDiameter = simoptions.MaxStrandDiameter;
+        design.Dc = equivDcfromstranded (design.WireStrandDiameter, design.NStrands);
     end
     
     % determine an appropriate series/parallel coil configuration
-    [design.Branches, design.CoilsPerBranch] = branchfac2coilconfig_AM(activecoilsperphase, design.BranchFac);
+    [design.Branches, design.CoilsPerBranch] = branchfac2coilconfig_AM (activecoilsperphase, design.BranchFac);
 
 end
 
