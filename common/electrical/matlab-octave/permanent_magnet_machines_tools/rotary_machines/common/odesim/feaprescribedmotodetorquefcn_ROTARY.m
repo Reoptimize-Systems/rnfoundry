95f6f5c338244d67a46da757da8b520371037b6f
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-05-17 15:54:22 +0100

    initial addiion of functions to allow full fea based dynamic simulation

---
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/odesim/feaprescribedmotodetorquefcn_ROTARY.m
@@ -0,0 +1,89 @@
+function varargout = feaprescribedmotodetorquefcn_ROTARY(t, x, design, simoptions)
+% solves the right had side of the differential equations for a machine
+% moving with a prescribed motion using FEA to determine main quantities
+%
+% Syntax:
+%
+% Input
+%
+% x is a vector of values of the state of the sytem at time t. The members
+% of x are the following:
+%
+%
+% design is a standard machine design structure popluated with all the
+% necessary information to perform the simulation. 
+%
+% 'simoptions'  is a structure determining the buoy and sea parameters used
+% in the simulation, and also any penalties to be used in the scoring of
+% the design. Essential fields are:
+
+    if nargin == 0
+        % get the output names
+        varargout{1} = {'ydot', 'dlambdaVdt', 'EMF', 'thetaT', 'omegaT', 'Tqpto', 'TqaddE', 'TqaddEBD', 'RPhase'};
+        return;
+    end
+
+    % Change the x members into more useful variables names, MATLAB will
+    % optimise away any memory penalty associated with this I think    
+    Iphases = x(simparams.ODESim.CurrentInds);
+    
+    Icoils = Iphases ./ design.Branches;
+
+    % Initialize dx with zeros
+    dx = zeros(size(x));
+    
+    % get the velocity and position at the current time
+    [thetaE, omegaE] = prescribedmotomegatheta(t, simoptions);
+
+    % determine the machine outputs Get the flux linkage and forces using
+    % the core machine simulation function
+    [flux_linkage, Tqeff, Tqreac, lossinfo, design] = machinefeaodesim_AM(design, simoptions, thetaE, 0, Icoils);
+    
+    % clculate the numerical derivative of the flux linkage
+    dlambdaVdt = simparams.ODESim.NumericalDerivatives (t, flux_linkage);
+    
+    % the emf is the negative of the flux linkage derivative
+    EMF = -dlambdaVdt;
+    
+    % do the integration
+    dx(simparams.ODESim.FluxLinkageInds,1) = dlambdaVdt;
+    
+    % find the derivative of the coil current (solving the differential
+    % equation describing the simple output circuit)
+    dx(simparams.ODESim.CurrentInds,1) = circuitode_linear(Iphases, EMF, design);
+     
+    % call the supplied additional force function
+    [TqaddE, TorqueBD] = ...
+        feval(simoptions.torquefcn, design, simoptions, thetaE, omegaE, EMF, Iphases, simoptions.forcefcnargs{:});
+    
+    % ************************************************************************
+
+    % Now assign the outputs
+    varargout{1} = dx;
+
+    % To record the forces
+    if nargout > 1
+        
+        % rate of change of flux linkage with displacement 
+        varargout{2} = dlambdaVdt;
+        % per-coil induced voltage 
+        varargout{3} = EMF;
+        % vertical displacement of the translator  
+        varargout{4} = thetaE;
+        % vertical velocity of the translator
+        varargout{5} = omegaE;
+        % total electromagnetic forces acting on effector
+        varargout{6} = Tqeff;
+        % output the additional forces transmitted to the primemover
+        varargout{7} = TqaddE;
+        % output a breakdown of the additional forces transmitted to the
+        % prime mover
+        varargout{8} = TorqueBD;
+        % output the phase resistance at each time step
+        varargout{9} = diag(design.RPhase)';
+        
+        
+    end
+
+end
+
