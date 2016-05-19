193bd08f9a4f732877b7b31fec372618fbe40897
Richard Crozier <a@b.c>
2016-05-19 17:35:35 +0100

    prescribedmotodetorquefcn_ROTARY.m: use torquefcnargs not forcefcnargs

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/odesim/prescribedmotodetorquefcn_ROTARY.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/rotary_machines/common/odesim/prescribedmotodetorquefcn_ROTARY.m
@@ -46,7 +46,7 @@ function varargout = prescribedmotodetorquefcn_ROTARY(t, x, design, simoptions)
      
     % call the supplied additional force function
     [TqaddE, TorqueBD] = ...
-        feval(simoptions.torquefcn, design, simoptions, thetaE, omegaE, EMF, Iphases, simoptions.forcefcnargs{:});
+        feval(simoptions.torquefcn, design, simoptions, thetaE, omegaE, EMF, Iphases, simoptions.torquefcnargs{:});
     
     % ************************************************************************
 
