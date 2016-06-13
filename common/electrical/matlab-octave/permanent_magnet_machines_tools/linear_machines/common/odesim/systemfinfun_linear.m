2144a3b508c39e82647652655f8e400eec776ac9
Richard Crozier <a@b.c>
2016-06-13 17:32:02 +0100

    systemfinfun_linear.m: remove redundant ODEPhaseCurrentCol

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/common/odesim/systemfinfun_linear.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/common/odesim/systemfinfun_linear.m
@@ -2,7 +2,7 @@ function [design, simoptions] = systemfinfun_linear(design, simoptions, finfun)
 % systemfinfun_linear: finalises the machine design and buoy simulation
 % setup for a linear machine and heaving buoy simulation
 
-    if ~all(isfield(design, {'slm_psidot'})) || ~all(isfield(simoptions, {'mx_initial_conditions'}))
+    if ~all(isfield(design, {'slm_fluxlinkage'}))
         % In this case we assume we have not already run the finalisation
         % code on this design and must do so
         [design, simoptions] = feval(finfun, design, simoptions);
@@ -14,10 +14,6 @@ function [design, simoptions] = systemfinfun_linear(design, simoptions, finfun)
     simoptions = setfieldifabsent(simoptions, 'maxAllowedxT', inf);
     
     simoptions = setfieldifabsent(simoptions, 'buoy', []);
-    
-    % set the starting column of phase current vals in the results vector
-    % to 5
-    simoptions = setfieldifabsent(simoptions, 'ODEPhaseCurrentCol', 5);
     
     % Set up buoy simulation
     simoptions = buoysimsetup(simoptions.buoy, simoptions);
