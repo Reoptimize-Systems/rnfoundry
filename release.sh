fdd292a92daf058e4e52c16f6d2e1f556759133a
Richard Crozier <a@b.c>
2016-06-22 09:02:06 +0100

    make release.sh work

--- release.sh
+++ release.sh
@@ -13,10 +13,10 @@ VERSION="0_1"
 WORKING_COPY_DIR=$(pwd)
 #echo $WORKING_COPY_DIR
 
-$RELEASE_NAME=RenewNet_$VERSION
+RELEASE_NAME="RenewNet_$VERSION"
 
 # create release directory
-$RELEASE_DIR=$WORKING_COPY_DIR/RenewNet_$VERSION
+RELEASE_DIR="$WORKING_COPY_DIR/RenewNet_$VERSION"
 mkdir $RELEASE_DIR
 
 echo "Creating release $RELEASE_NAME in directory $RELEASE_DIR"
@@ -25,6 +25,8 @@ echo "Creating release $RELEASE_NAME in directory $RELEASE_DIR"
 hg archive $RELEASE_DIR
 # remove file created by mercurial
 rm $RELEASE_DIR/.hg_archival.txt
+#remove hgignore file
+rm $RELEASE_DIR/.hgignore
 # remove the release scripts
 rm $RELEASE_DIR/release.sh
 rm $RELEASE_DIR/test_release.sh
