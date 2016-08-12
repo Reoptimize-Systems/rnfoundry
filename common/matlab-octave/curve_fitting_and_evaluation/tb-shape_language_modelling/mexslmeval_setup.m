e951ddb255ab411b4fc09f0fe09777bb686cf2cb
Richard Crozier <a@b.c>
2016-08-12 10:25:31 +0100

    Added help file for mexslmeval and improve mexslmeval help

--- common/matlab-octave/curve_fitting_and_evaluation/tb-shape_language_modelling/mexslmeval_setup.m
+++ common/matlab-octave/curve_fitting_and_evaluation/tb-shape_language_modelling/mexslmeval_setup.m
@@ -1,5 +1,20 @@
 function mexslmeval_setup ()
-% creates the mexslmeval mex file
+% builds the mexslmeval mex function from source using mex
+%
+% Syntax
+% 
+% mexslmeval_setup ()
+%
+% Description
+%
+% Builds the mexslm mex funcion from the C++ source. Requires a working mex
+% compiler to be set up in Matlab. mexslmeval uses the GNU scientific
+% library and you must therefore have this on your system and on your
+% compiler's path. To be specific mexslmeval links to libgsl and
+% libgslcblas for its histogram functions.
+%
+%
+% See also: slmeval, slmengine
 
     CC = onCleanup (@() cd(pwd));
     
