d1d8ec111bd78d3f2040449c970351b0f801b613
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-11-15 11:57:46 +0000

    simfun_TM: use coil 2D centroid to calculate MTL or Rcm if not present and calculate some defualt mesh sizes

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/odesim/simfun_TM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/odesim/simfun_TM.m
@@ -1,4 +1,4 @@
-function [design, simoptions] = simfun_TM(design, simoptions)
+function [design, simoptions] = simfun_TM (design, simoptions)
 % simulation preprocessing function common to all tubular linear machines
 %
 % Syntax
@@ -10,8 +10,36 @@ function [design, simoptions] = simfun_TM(design, simoptions)
 % design - a tubular machine design structure 
 % 
 
-    design.MTL = 2 * pi * mean([design.Ro, design.Ri]); 
-
+    % for a tubular machine the MTL is the radial distance to the coil
+    % 2D shape centroid, use it if present, othersie use middle of coil
+    if isfield (design, 'CoilCentroid')
+        design.MTL = design.CoilCentroid(1);
+    else
+        % if not avaialble use the 
+        design.MTL = 2 * pi * design.Rcm;
+    end
+                          
+	% do common linear machine setup tasks
     [design, simoptions] = simfun_linear(design, simoptions);
-
+    
+    % set some default mesh size options
+    simoptions.MagFEASim = setfieldifabsent (simoptions.MagFEASim, ...
+        'MagnetRegionMeshSize', choosemesharea_mfemm(design.rm, design.zm, 1/10));
+    
+    simoptions.MagFEASim = setfieldifabsent (simoptions.MagFEASim, ...
+        'MagnetSpacerRegionMeshSize', choosemesharea_mfemm(design.rm, design.zsd, 1/10));
+    
+    simoptions.MagFEASim = setfieldifabsent (simoptions.MagFEASim, ...
+        'AirGapMeshSize', choosemesharea_mfemm(design.g, design.zp, 1/10));
+    
+    simoptions.MagFEASim = setfieldifabsent (simoptions.MagFEASim, ...
+        'OuterRegionsMeshSize', [choosemesharea_mfemm(design.rm, design.zp, 1/5), -1]);
+    
+    simoptions.MagFEASim = setfieldifabsent (simoptions.MagFEASim, 'YokeRegionMeshSize', ...
+                                       mean( [choosemesharea_mfemm(design.ry, 2*design.zp, 1/10), ...
+                                        choosemesharea_mfemm(design.rc(1), design.zs-max(design.zc), 1/10)] ) );
+                                    
+    simoptions.MagFEASim = setfieldifabsent (simoptions.MagFEASim, 'CoilRegionMeshSize', ...
+                                    choosemesharea_mfemm(design.rc(1), mean(design.zc)) );
+    
 end
