2e467f0ba1f104392345cb37b56368954c6c13d0
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2016-11-15 11:59:32 +0000

    added completedesign_TM.m

---
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/common/optimisation/completedesign_TM.m
@@ -0,0 +1,105 @@
+function design = completedesign_TM (design, simoptions)
+% performs design creation operations common to all radial flux type rotary
+% pm machines
+%
+% Syntax
+%
+% design = completedesign_TM(design)
+% design = completedesign_TM(design, simoptions)
+% 
+% Description
+%
+% completedesign_TM performs some design completion tasks common to all
+% tubular linear machines. Most notably it completes the winding specification
+% based on a minimum spec provided in the design structure. The winding is
+% described using the following variables:
+%
+%  yp - Average coil pitch as defined by (Qs/Poles)
+%  yd - Actual coil pitch as defined by round(yp) +/- k
+%  Qs  -  total number of stator slots in machine
+%  Qc  -  total number of winding coils in machine 
+%  q  -  number of slots per pole and phase
+%  qn  -  numerator of q
+%  qd  -  denominator of q
+%  qc - number of coils per pole and phase
+%  qcn  -  numerator of qc
+%  qcd  -  denominator of qc
+%  Qcb - basic winding (the minimum number of coils required to make up a 
+%    repetitive segment of the machine that can be modelled using symmetry)
+%  pb - the number of poles corresponding to the basic winding in Qcb
+%  CoilLayers - the number of layers in the coil slot
+%
+% This pole/slot/coil/winding terminology is based on that presented in
+% [1].
+%
+% Machine windings can be single or double layered, in which case:
+%
+% Single layer
+%   q = 2qc
+%   Qs = 2Qc
+% Double layer
+%   q = qc
+%   Qs = Qc
+%
+% To specify a winding, the 'minimum spec' that must be provided is based on
+% a combination of some or all of the following variables:
+%
+%  Phases - The number of phases in the machine
+%  Poles - The number of magnetic poles in the machine
+%  NBasicWindings - the number of basic winding segments in the machine
+%  qc - number of coils per pole and phase (as a fraction object)
+%  Qc - total number of coils (in all phases) in the machine
+%
+% Any of the following combinations may be supplied to specify the winding:
+%
+%   Poles, Phases, Qc, CoilLayers
+%   Poles, Phases, qc, CoilLayers
+%   qc, Phases, NBasicWindings, CoilLayers
+%
+% These variables must be provided as fields in the design structure. If
+% 'qc' is supplied, it must be an object of the class 'fr'. This is a class
+% designed for handling fractions. See the help for the ''fr'' class for
+% further information. If CoilLayers is not supplied, it defaults to 1.
+% 
+% The machine slot pitch in radians (design.thetas) is also then calculated.
+%
+% completedesign_LINEAR also adds further default design options common to all pm
+% machines if they are not already present in the design structure. The
+% following fields will be added to 'design' if they are not already present:
+%
+%  CoilLayers - default is 1
+%
+%  MagnetSkew - determines the amount of magnet skewwing as a ratio of a pole
+%   width (i.e. it is expected to be between 0 and 1). Defaults to zero if not
+%   supplied.
+%
+%  NStrands - number of strands making up the wire in the coils. Defaults to 1
+%   if not supplied.
+%
+%  NStages - number of stages making up the machine. Defaults to 1 if not
+%   supplied
+%
+% Not all machine types may make use of all the default options set here.
+%
+%
+% [1] J. J. Germishuizen and M. J. Kamper, "Classification of symmetrical
+% non-overlapping three-phase windings," in The XIX International
+% Conference on Electrical Machines - ICEM 2010, 2010, pp. 1-6.
+%
+%
+% See also: fr.m, completedesign_AM.m, completedesign_ROTARY.m
+%
+
+    % perform processing common to all rotary machines
+    design = completedesign_linear (design, simoptions);
+    
+    % ignore the winding layout because it's different for tubular machines
+    % for now
+    warning ('Winding spec ignored for tubular machine');
+    
+%     design.WindingLayout.Phases = repmat ([1; 2; 3], design.Poles(1), 1);
+%     design.WindingLayout.Phases = repmat ((1:design.Phases)', design.Poles(1), 1);
+    
+    design.WindingLayout.Phases = repmat ([1, -2, 3, -1, 2, -3]', design.Poles(1)/2, 1);
+    
+end
