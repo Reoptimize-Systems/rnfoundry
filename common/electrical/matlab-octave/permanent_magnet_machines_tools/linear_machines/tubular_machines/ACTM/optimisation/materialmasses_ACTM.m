4506482e84aea27e638b91856bfaa5d82dee904d
Richard Crozier <a@b.c>
2016-11-15 12:39:09 +0000

    materialmasses_ACTM.m: split out armature and field and use certain quantities in design structure if present

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/ACTM/optimisation/materialmasses_ACTM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/tubular_machines/ACTM/optimisation/materialmasses_ACTM.m
@@ -6,6 +6,70 @@ function [design, simoptions] = materialmasses_ACTM(design, simoptions)
 % [design, simoptions] = materialmasses_ACTM(design, simoptions)
 %
 
-    [design, simoptions] = materialmasses_TM(design, simoptions);
+    [design, simoptions] = materialmasses_field_TM (design, simoptions);
+ 
+    [design, simoptions] = materialmasses_arm_ACTM (design, simoptions);
     
 end
+
+function [design, simoptions] = materialmasses_arm_ACTM (design, simoptions)
+% calculates the masses of common components of tubular linear machines
+%
+% Syntax
+%
+% [design, simoptions] = materialmasses_TM(design, simoptions)
+%
+% 
+
+    if isfield (design, 'MTL')
+        design.wlength = design.CoilTurns * design.MTL;
+    else
+        design.wlength = design.CoilTurns * pi * 2 * (design.Ri + (design.Hc / 2));
+    end
+    
+    % Determine the copper cost based on the length of wire in a coil
+    % multiplied by the number of Phases and armature Poles and the wire
+    % cross-sectional area etc.
+    design.CopperMass = design.Phases ...
+                        * design.wlength ...
+                        * (pi * (design.Dc/2)^2) ...
+                        * design.Poles(2) ...
+                        * simoptions.evaloptions.CopperDensity;
+    
+    % Calculate the cost of the laminated iron in the armature
+    design.ArmatureIronMass = design.Poles(2) ...
+                              * design.Wp ...
+                              * pi ...
+                              * (design.Ra.^2 - design.Ro.^2) ...
+                              * simoptions.evaloptions.ArmatureIronDensity;
+    
+end
+
+function [design, simoptions] = materialmasses_TM_FIELD (design, simoptions)
+% calculates the masses of the field component of tubular linear machines
+%
+% Syntax
+%
+% [design, simoptions] = materialmasses_TM_FIELD(design, simoptions)
+%
+% 
+    
+    % Now calculate the total mass of the magnets
+    design.MagnetMass = design.Poles(1) ...
+                        * design.Wm ...
+                        * pi * (design.Rm.^2 - design.Rso^2)  ...
+                        * simoptions.evaloptions.MagnetDensity;
+
+    % Calculate the cost of the back iron on the field, i.e. the steel
+    % discs in the translator
+    if design.mode == 1 || design.mode == 3
+        cavityvol = steelcavityvol_TM (design);
+    else
+        cavityvol = 0;
+    end
+    
+    design.FieldIronMass = (design.Ws * pi * (design.Rm^2 - design.Rso^2) - cavityvol) ...
+                            * design.Poles(1) * simoptions.evaloptions.FieldIronDensity;
+    
+end
+
