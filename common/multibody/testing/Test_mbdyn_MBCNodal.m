a73bc21129e68ed030c16ddd7d3f91b30ea9bf2a
Richard Crozier <a@b.c>
2016-11-25 16:42:43 +0000

    Test_mbdyn_MBCNodal.m: start mbdyn using 'system' command

--- common/multibody/testing/Test_mbdyn_MBCNodal.m
+++ common/multibody/testing/Test_mbdyn_MBCNodal.m
@@ -3,7 +3,18 @@
 
 % start mbdyn 
 delete ('output.*');
-[status, cmdout] = system (sprintf ('mbdyn -f "%s" -o output > output.txt 2>&1 &', fullfile (getmfilepath ('Test_mbdyn_MBCNodal'), 'socket.mbd')))
+
+mbdpath = fullfile (getmfilepath ('Test_mbdyn_MBCNodal'), 'socket.mbd');
+% start mbdyn 
+
+[status, cmdout] = system ( sprintf ('export LD_LIBRARY_PATH="" ; mbdyn -f "%s" -o "%s" > "%s" 2>&1 &', ...
+                    mbdpath, ...
+                    mbdpath(1:end-4), ...
+                    [mbdpath(1:end-4), '.txt'] ...
+                                     ) ...
+                           );
+                       
+[status, cmdout] = system (sprintf ('mbdyn -f "%s" -o output > output.txt 2>&1 &', mbdpath))
 
 % wait a few seconds for mbdyn to initialise
 pause (3);
@@ -14,7 +25,8 @@ mb = mbdyn.MBCNodal ();
 
 mb.Initialize ( 'local', '/tmp/mbdyn.sock', ...
                 'NNodes', 2, ...
-                'UseLabels', true);
+                'UseLabels', true, ...
+                'Verbose', true );
 
 nnodes = mb.GetNodes ()
 
@@ -29,8 +41,9 @@ while status == 0
     
     for iter = (maxiter-1):-1:1
 
-        disp(iter)
+%         disp(iter)
         status = mb.GetMotion ();
+        fprintf (1, 'iter: %d, iterstatus: %d\n', iter, status);
 
 %         pos1 = mb.X(1)
         
@@ -41,7 +54,8 @@ while status == 0
 
     end
     
-    status = mb.GetMotion ()
+    status = mb.GetMotion ();
+    fprintf (1, 'final status: %d\n', status);
 
     pos1 = mb.X(1)
     
@@ -59,6 +73,8 @@ while status == 0
     mb.applyForcesAndMoments (true)
 
 end
+
+clear mb;
 
 % 
 % mb.GetMotion ();
@@ -79,8 +95,15 @@ end
 % 
 % mb.applyForcesAndMoments (1)
 
+%%
 
-clear mb;
+
+mbout = mbdyn ();
+mbout.loadResults ( mbdpath );
+
+mbout.plotNodeTrajectories ()
+
+
 
 
 
