2bb54e18a9a38cdbdc2cd3c5b5e65ac2382105c2
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2017-02-01 15:58:36 +0000

    +check/multicheck.m: added help text

--- common/matlab-octave/+check/multicheck.m
+++ common/matlab-octave/+check/multicheck.m
@@ -1,5 +1,22 @@
 function multicheck (checkfcn, errormsg, errid, varargin)
 % performs check on multiple inputs and raises error on failure
+%
+% Syntax
+%
+% multicheck (checkfcn, errormsg, errid, arg1, arg2, ..., argn )
+%
+% Input
+%
+%  checkfcn - function to apply to arguents to check their validity
+%
+%  errormsg - error message to display in the event of any failure to pass
+%    the check.
+%
+%  errid - error id to use for failure error message
+%
+%  arg1, arg2, ..., argn - any number of arguments for which to perform the
+%    test.
+%
 
     result = cellfun (checkfcn, varargin, 'UniformOutput', true);
     
