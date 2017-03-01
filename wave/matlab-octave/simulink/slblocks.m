78fb18f7c7fbb004d671a768feb15a66910a419c
Richard Crozier <a@b.c>
2017-03-01 15:30:09 +0000

    Added simulink block for bailey wave model

---
+++ wave/matlab-octave/simulink/slblocks.m
@@ -0,0 +1,9 @@
+function blkStruct = slblocks
+% SLBLOCKS defines a Wave Simulations block library
+
+% Library name, appears in library browser
+
+blkStruct.Name = 'Wave Simulations Library';
+
+blkStruct.OpenFcn = 'wave_simulations_library';
+
