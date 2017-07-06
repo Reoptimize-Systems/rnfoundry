a307b311b760f0e118a81c2636c7fe2c3d8076c5
Richard Crozier <a@b.c>
2017-07-06 13:02:01 +0100

    Test_mbdyn_pre.m: a lot of updates to reflect changes in code base

--- common/multibody/testing/Test_mbdyn_pre.m
+++ common/multibody/testing/Test_mbdyn_pre.m
@@ -1,119 +1,119 @@
-
-
-%% orientation matrix
-
-om = mbdyn.pre.orientmat ('2vectors', struct ('ia', 1, 'vecA', [1;0;0], 'ib', 2, 'vecB', [0;1;0])); 
-
-om.orientationMatrix
-
-
-%% orientation matrix
-
-pos = [1,0,0]
-
-om = mbdyn.pre.orientmat ('2vectors', struct ('ia', 1, 'vecA', [cos(pi/6);sin(pi/6);0], 'ib', 3, 'vecB', [0;0;1])); 
-
-om.orientationMatrix
-
-pos * om.orientationMatrix
-
-om = mbdyn.pre.orientmat ('euler', [0,0,pi/6]);
-
-om.orientationMatrix
-
-pos * om.orientationMatrix
-
-%% references
-
-gref = mbdyn.pre.globalref
-
-theta1 = 0.1;
-theta2 = 0.1;
-L = 1;
-
-Ref_Link1 = mbdyn.pre.reference ([], mbdyn.pre.orientmat ('euler', [0, pi/2 - theta1, 0]), [], [], 'Parent', gref)
-
-Ref_Link1.pos
-
-Ref_Link2 = mbdyn.pre.reference ([L; 0; 0], mbdyn.pre.orientmat ('euler', [0, -theta2, 0]), [], [], 'Parent', Ref_Link1)
-
-Ref_Link2.pos
-
-Node_Link2 = mbdyn.pre.reference ([0.5*L; 0; 0], mbdyn.pre.orientmat ('orientation', eye(3)), [], [], 'Parent', Ref_Link2)
-
-fcalc = [ L*sin(theta1)+(L/2)*sin(theta1+theta2); 
-          0; 
-          -L*cos(theta1)-(L/2)*cos(theta1+theta2) ];
-
-      
-[ fcalc, Node_Link2.pos ]
-
-
-% om = mbdyn.pre.orientmat ('euler', [0, pi-(theta1+theta2), 0])
-
-%% structuralNode6dof
-
-
-sn6dof = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-
-str = sn6dof.generateOutputString ()
-
-%%
-sn3dof = mbdyn.pre.structuralNode3dof ('dynamic displacement', 'Accel', true);
-
-str = sn3dof.generateOutputString ()
-
-%% Body
-
-sn6dof = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-mass = 1;
-cog = [0;0;0];
-inertiamat = eye (3);
-
-bd = mbdyn.pre.body (mass, cog, inertiamat, sn6dof);
-
-str = bd.generateOutputString ()
-
-%% Body
-
-sn6dof = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-mass = 1;
-cog = [0;0;0];
-inertiamat = eye (3);
-
-bd = mbdyn.pre.body (mass, cog, inertiamat, sn6dof, 'InertialOrientation', eye (3));
-
-str = bd.generateOutputString ()
-
-
-%% Total Joint
-
-sn1 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-sn2 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-
-posstatus = 'active';
-orientstatus = true;
-
-jnt = mbdyn.pre.totalJoint (sn1, sn2, posstatus, orientstatus);
-jnt.generateOutputString ()
-
-
-jnt = mbdyn.pre.totalJoint (sn1, sn2, posstatus, orientstatus, ...
-    'RelativeOffset1', [1; 2; 3]);
-jnt.generateOutputString ()
-
-jnt = mbdyn.pre.totalJoint (sn1, sn2, posstatus, orientstatus, ...
-    'RelativeOffset1', [1; 2; 3], ...
-    'RelativeOffset1Reference', 'other node');
-jnt.generateOutputString ()
-
-%% Revolute Rotation
-
-sn1 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-sn2 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
-
-jnt = mbdyn.pre.revoluteRotation (sn1, sn2);
-jnt.generateOutputString ()
+% 
+% 
+% %% orientation matrix
+% 
+% om = mbdyn.pre.orientmat ('2vectors', struct ('ia', 1, 'vecA', [1;0;0], 'ib', 2, 'vecB', [0;1;0])); 
+% 
+% om.orientationMatrix
+% 
+% 
+% %% orientation matrix
+% 
+% pos = [1,0,0]
+% 
+% om = mbdyn.pre.orientmat ('2vectors', struct ('ia', 1, 'vecA', [cos(pi/6);sin(pi/6);0], 'ib', 3, 'vecB', [0;0;1])); 
+% 
+% om.orientationMatrix
+% 
+% pos * om.orientationMatrix
+% 
+% om = mbdyn.pre.orientmat ('euler', [0,0,pi/6]);
+% 
+% om.orientationMatrix
+% 
+% pos * om.orientationMatrix
+% 
+% %% references
+% 
+% gref = mbdyn.pre.globalref
+% 
+% theta1 = 0.0;
+% theta2 = pi/4;
+% L = 1;
+% 
+% Ref_Link1 = mbdyn.pre.reference ([], mbdyn.pre.orientmat ('euler', [0, pi/2 - theta1, 0]), [], [], 'Parent', gref)
+% 
+% Ref_Link1.pos
+% 
+% Ref_Link2 = mbdyn.pre.reference ([L; 0; 0], mbdyn.pre.orientmat ('euler', [0, -theta2, 0]), [], [], 'Parent', Ref_Link1)
+% 
+% Ref_Link2.pos
+% 
+% Node_Link2 = mbdyn.pre.reference ([0.5*L; 0; 0], mbdyn.pre.orientmat ('orientation', eye(3)), [], [], 'Parent', Ref_Link2)
+% 
+% fcalc = [ L*sin(theta1)+(L/2)*sin(theta1+theta2); 
+%           0; 
+%           -L*cos(theta1)-(L/2)*cos(theta1+theta2) ];
+% 
+%       
+% [ fcalc, Node_Link2.pos ]
+% 
+% 
+% % om = mbdyn.pre.orientmat ('euler', [0, pi-(theta1+theta2), 0])
+% 
+% %% structuralNode6dof
+% 
+% 
+% sn6dof = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% 
+% str = sn6dof.generateOutputString ()
+% 
+% %%
+% sn3dof = mbdyn.pre.structuralNode3dof ('dynamic displacement', 'Accel', true);
+% 
+% str = sn3dof.generateOutputString ()
+% 
+% %% Body
+% 
+% sn6dof = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% mass = 1;
+% cog = [0;0;0];
+% inertiamat = eye (3);
+% 
+% bd = mbdyn.pre.body (mass, cog, inertiamat, sn6dof);
+% 
+% str = bd.generateOutputString ()
+% 
+% %% Body
+% 
+% sn6dof = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% mass = 1;
+% cog = [0;0;0];
+% inertiamat = eye (3);
+% 
+% bd = mbdyn.pre.body (mass, cog, inertiamat, sn6dof, 'InertialOrientation', eye (3));
+% 
+% str = bd.generateOutputString ()
+% 
+% 
+% %% Total Joint
+% 
+% sn1 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% sn2 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% 
+% posstatus = 'active';
+% orientstatus = true;
+% 
+% jnt = mbdyn.pre.totalJoint (sn1, sn2, posstatus, orientstatus);
+% jnt.generateOutputString ()
+% 
+% 
+% jnt = mbdyn.pre.totalJoint (sn1, sn2, posstatus, orientstatus, ...
+%     'RelativeOffset1', [1; 2; 3]);
+% jnt.generateOutputString ()
+% 
+% jnt = mbdyn.pre.totalJoint (sn1, sn2, posstatus, orientstatus, ...
+%     'RelativeOffset1', [1; 2; 3], ...
+%     'RelativeOffset1Reference', 'other node');
+% jnt.generateOutputString ()
+% 
+% %% Revolute Rotation
+% 
+% sn1 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% sn2 = mbdyn.pre.structuralNode6dof ('dynamic', 'Accel', true);
+% 
+% jnt = mbdyn.pre.revoluteRotation (sn1, sn2);
+% jnt.generateOutputString ()
 
 %% system
 
@@ -123,38 +123,54 @@ jnt.generateOutputString ()
 
 gref = mbdyn.pre.globalref;
 
-theta1 = pi/2 + 0.2;
-theta2 = 0.1;
+theta1 = -pi/5;
+theta2 = pi/5;
 L = 1;
 M = 1;
 inertiamat = diag ([0., M*L^2./12., M*L^2./12.]);
 
 Ref_Link1 = mbdyn.pre.reference ( [], ...
-                                  mbdyn.pre.orientmat ('euler', [0, pi/2 - theta1, 0]), ...
+                                  mbdyn.pre.orientmat ('euler', [0, theta1, 0]), ...
                                   [], ...
                                   [], ...
-                                  'Parent', gref);
+                                  'Parent', gref, ...
+                                  'Name', 'Ref_Link1');
+                              
+% Ref_Link1 = mbdyn.pre.reference ( [], ...
+%                                   mbdyn.pre.orientmat ('euler', [0, pi/2 - theta1, 0]), ...
+%                                   [], ...
+%                                   [], ...
+%                                   'Parent', gref);
 
 Ref_Node_Link1 = mbdyn.pre.reference ( [0.5*L; 0; 0], ...
                                        [], ...
                                        [], ...
                                        [], ...
-                                       'Parent', Ref_Link1 );
+                                       'Parent', Ref_Link1, ...
+                                       'Name', 'Ref_Node_Link1' );
 
 link1node = mbdyn.pre.structuralNode6dof ('dynamic', ...
                                           'AbsolutePosition', Ref_Node_Link1.pos, ...
                                           'AbsoluteOrientation', Ref_Node_Link1.orientm );
 
 Ref_Link2 = mbdyn.pre.reference ( [L; 0; 0], ...
-                                   mbdyn.pre.orientmat ('euler', [0, -theta2, 0]), [], ...
+                                   mbdyn.pre.orientmat ('euler', [0, theta2, 0]), [], ...
                                    [], ...
-                                   'Parent', Ref_Link1 );
+                                   'Parent', Ref_Link1, ...
+                                   'Name', 'Ref_Link2');
+                               
+% Ref_Link2 = mbdyn.pre.reference ( [L; 0; 0], ...
+%                                    mbdyn.pre.orientmat ('euler', [0, -theta2, 0]), [], ...
+%                                    [], ...
+%                                    'Parent', Ref_Link1, ...
+%                                    'Name', 'Ref_Link2');
 
 Ref_Node_Link2 = mbdyn.pre.reference ( [0.5*L; 0; 0], ...
                                        mbdyn.pre.orientmat ('orientation', eye(3)), ...
                                        [], ...
                                        [], ...
-                                       'Parent', Ref_Link2);
+                                       'Parent', Ref_Link2, ...
+                                       'Name', 'Ref_Node_Link2');
 
 link2node = mbdyn.pre.structuralNode6dof ('dynamic', ...
                                           'AbsolutePosition', Ref_Node_Link2.pos, ...
@@ -170,27 +186,32 @@ link1.setColour ('b');
 
 hinges_orientation = mbdyn.pre.orientmat ('2vectors', struct ('ia', 1, 'vecA', [1;0;0], 'ib', 3, 'vecB', [0;1;0]));
 
-Ref_pin = mbdyn.pre.reference ([], hinges_orientation, [], [], 'Parent', Ref_Link1);
-Ref_hinge = mbdyn.pre.reference ([], hinges_orientation, [], [], 'Parent', Ref_Link2);
+Ref_pin = mbdyn.pre.reference ([], hinges_orientation, [], [], 'Parent', Ref_Link1, 'Name', 'Ref_pin');
+Ref_hinge = mbdyn.pre.reference ([], hinges_orientation, [], [], 'Parent', Ref_Link2, 'Name', 'Ref_hinge');
 
 pinjoint = mbdyn.pre.revolutePin (link1node, Ref_Link1.pos, Ref_Link1.pos, ...
-                    'NodeRelativeOrientation', hinges_orientation, ...
-                    'PinOrientation', hinges_orientation);
+                    'NodeRelativeOrientation', Ref_pin.orientm, ...
+                    'PinOrientation', Ref_pin.orientm);
                 
-linkjoint = mbdyn.pre.revoluteHinge (link1node, link2node, Ref_Link2.pos, Ref_Link2.pos, ...
-                    'Offset1Reference', 'global', ...
-                    'Offset2Reference', 'global', ...
-                    'RelativeOrientation1', Ref_hinge.orientm, ...
-                    'Orientation1Reference', 'global', ...
-                    'RelativeOrientation2', Ref_hinge.orientm, ...
-                    'Orientation2Reference', 'global');
+linkjoint = mbdyn.pre.revoluteHinge ( link1node, link2node, ...
+                                      ...Ref_Link2.pos, Ref_Link2.pos, ...
+                                     [L/2;0;0], [L/2;0;0], ...
+                                     'Offset1Reference', 'node', ...
+                                     'Offset2Reference', 'other node', ...
+                                     ...'Offset1Reference', 'global', ...
+                                     ...'Offset2Reference', 'global', ...
+                                     'RelativeOrientation1', Ref_hinge.orientm, ...
+                                     'Orientation1Reference', 'global', ...
+                                     'RelativeOrientation2', Ref_hinge.orientm, ...
+                                     'Orientation2Reference', 'global' ...
+                                     );
                 
 pinjoint.setSize (L/10, L/10, L/10);
 pinjoint.setColour ('k');
 linkjoint.setSize (L/10, L/10, L/10);
 linkjoint.setColour ('g');
 
-prb = mbdyn.pre.initialValueProblem (0, 5, 1e-3);
+prb = mbdyn.pre.initialValueProblem (0, 10, 1e-3, 'Tolerance', 1e-9) %, 'DerivativesTolerance', 100000);
 
 mbsys = mbdyn.pre.system ( {prb}, ...
                            'Nodes', {link1node, link2node}, ...
@@ -199,9 +220,9 @@ mbsys = mbdyn.pre.system ( {prb}, ...
 str = mbsys.generateMBDynInputStr ()
 
 mbsys.setStructuralNodeSize (L/10, L/10, L/10);
-mbsys.draw ('Mode','wireghost')
+mbsys.draw ('Mode','wireghost', 'AxLims', [-2, 2; -2, 2; -2, 2;])
 
-
+%%
 filename = mbsys.generateMBDynInputFile ('Test_mbdyn_pre.mbd');
 
 % start mbdyn 
@@ -216,16 +237,23 @@ filename = mbsys.generateMBDynInputFile ('Test_mbdyn_pre.mbd');
                      
 %% Post-processing
 
-mbdynpost = mbdyn.postproc (mbsys);
-mbdynpost.loadResultsFromFiles ([filename(1:end-4), '_mbd']);
+mbdynpost = mbdyn.postproc ([filename(1:end-4), '_mbd'], mbsys);
 
-mbdynpost.plotNodeTrajectories ()
+%%
+mbdynpost.plotNodeTrajectories ();
 
+%%
+
+mbdynpost.drawStep (500, 'AxLims', [-3.1, 3.1; -1.5, 1.5;  -3, 3]);
+
+%% Animate
 mbdynpost.animate ( 'PlotTrajectories', true, ...
                     'DrawLabels', true, ...
-                    'Skip', 40, ...
+                    'Skip', 20, ...
                     'DrawMode', 'solid', ...
-                    'Light', true );
+                    'Light', true, ...
+                    'AxLims', [-3.1, 3.1; -1.5, 1.5;  -3, 3], ...
+                    'VideoFile', 'double_pendulum.avi');
 
 % [status, cmdout] = system (sprintf ('mbdyn -f "%s" -o output > output.txt 2>&1 &', mbdpath))
 
