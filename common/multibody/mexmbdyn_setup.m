48ae4802b0cace9b10970e95949e2a611391ab52
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2017-07-06 15:13:48 +0100

    mexmbdyn_setup.m: allow specifying mbc lib and include dir

--- common/multibody/mexmbdyn_setup.m
+++ common/multibody/mexmbdyn_setup.m
@@ -21,6 +21,21 @@ function mexmbdyn_setup (varargin)
 
     options.Verbose = false;
     options.Debug = false;
+    if ispc
+        switch computer ('arch')
+            case 'win64'
+                options.MBCLibDir = fullfile (getmfilepath ('mexmbdyn_setup'), 'mbdyn_win64', 'lib');
+                options.MBCIncludeDir = fullfile (getmfilepath ('mexmbdyn_setup'), 'mbdyn_win64', 'include');
+            case 'win32'
+                options.MBCLibDir = fullfile (getmfilepath ('mexmbdyn_setup'), 'mbdyn_win32', 'lib');
+                options.MBCIncludeDir = fullfile (getmfilepath ('mexmbdyn_setup'), 'mbdyn_win32', 'include');
+            otherwise
+                error ('Arch not supported.')
+        end
+    else
+        options.MBCLibDir = '';
+        options.MBCIncludeDir = '';
+    end
     
     options = parse_pv_pairs (options, varargin);
 
@@ -30,7 +45,19 @@ function mexmbdyn_setup (varargin)
     
     cd(fullfile(getmfilepath (mfilename), '+mbdyn', '+mint'));
 
-    mexMBCNodal_mexargs = {'mexMBCNodal.cpp', '-lmbc'};
+    mexMBCNodal_mexargs = {'mexMBCNodal.cpp', '-lmbc', 'CXXFLAGS="$CXXFLAGS -std=c++11"'};
+    
+    if ispc
+        mexMBCNodal_mexargs = [mexMBCNodal_mexargs, {'-lws2_32.lib'}];
+    end
+    
+    if ~isempty (options.MBCIncludeDir)
+        mexMBCNodal_mexargs = [mexMBCNodal_mexargs, {['-I"', options.MBCIncludeDir, '"']}];
+    end
+    
+    if ~isempty (options.MBCLibDir)
+        mexMBCNodal_mexargs = [mexMBCNodal_mexargs, {['-L"', options.MBCLibDir, '"']}];
+    end
     
     if options.Verbose
         mexMBCNodal_mexargs = [mexMBCNodal_mexargs, {'-v'}];
