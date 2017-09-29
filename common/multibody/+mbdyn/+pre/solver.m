94da2548cfc194d16b946acb0c2ba5c7bebb23dd
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2017-09-29 16:01:08 +0100

    Added base class for solvers

---
+++ common/multibody/+mbdyn/+pre/solver.m
@@ -0,0 +1,18 @@
+classdef solver < mbdyn.pre.base
+    
+    
+    properties (GetAccess = public, SetAccess = protected)
+       modified;
+    end
+    
+    methods
+
+        function str = generateOutputString (self)
+            
+            str = '';
+            
+        end
+         
+    end
+    
+end
