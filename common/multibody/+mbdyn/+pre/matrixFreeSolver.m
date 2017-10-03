f245177e8dba0005dcba0b9801701a7fb671593b
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2017-10-03 12:52:28 +0100

    Added basic matrixFreeSolver.m

---
+++ common/multibody/+mbdyn/+pre/matrixFreeSolver.m
@@ -0,0 +1,63 @@
+classdef matrixFreeSolver < mbdyn.pre.nonlinearSolver
+    
+    
+    properties (GetAccess = public, SetAccess = protected)
+       method;
+    end
+    
+    methods
+        
+        function self = matrixFreeSolver (method, varargin)
+%             
+%             options.Method = '';
+%             
+%             options = parse_pv_pairs (options, varargin);
+%             
+%             if ~isempty (options.Modified)
+%                if (~iscell (options.Modified)) ...
+%                        || (numel (options.Modified) > 3)
+%                   error ('Modified must be a cell array of strings, of length less than 3');
+%                end
+%                
+%                self.checkNumericScalar (options.Modified{1}, true, 'iterations');
+%                
+%                allowedstrings = { 'keep jacobian matrix', ...
+%                                   'honor element requests' };
+%                for ind = 2:numel(options.Modified)
+%                    self.checkAllowedStringInputs (options.Modified{ind}, ...
+%                        allowedstrings, true, sprintf('Modified(%d)', ind));
+%                    
+%                    allowedstrings(1) = [];
+%                end
+%             end
+%             
+%             self.modified = options.Modified;
+
+            self.checkAllowedStringInputs ( method, {'bicgstab', 'gmres'}, true, 'method');
+
+            self.method = method;
+            self.type = 'matrix free';
+            
+        end
+
+        function str = generateOutputString (self)
+            
+%             args = {self.type};
+%             
+%             if ~isempty (self.modified)
+%                 args = [args, {sprintf('modified, %d', self.modified{1})}];
+%                 
+%                 if numel (self.modified) > 1
+%                     args = [args, self.modified(2:end)];
+%                 end
+%             end
+%             
+%             str = self.commaSepList (args{:});
+
+            str = self.commaSepList (self.type, self.method);
+            
+        end
+         
+    end
+    
+end
