42df16a8640ed9025211727357138da91cc8d9f5
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2017-10-11 11:36:04 +0100

    cellstr2str.m: improved help and added test for cell string array

--- common/matlab-octave/misc/cellstr2str.m
+++ common/matlab-octave/misc/cellstr2str.m
@@ -1,7 +1,31 @@
 function s = cellstr2str(C)
-% converts a cellarray of strings to a single string with newline
-% characters between each string
+% converts a cell array of strings to a single string with newlines
+%
+% Syntax
+%
+%
+% Description
+%
+% converts a cell array of strings to a single string with newline
+% characters between the string in each cell.
+%
+% Input
+%
+%  C - cell string array
+%
+% Output
+%
+%  s - string composed of the strings in each cell of the input C cell
+%   array concatenated with a newline character inserted between each
+%   substring.
+%
+% See Also: cellstr2txtfile, printcellstr
+%
 
-    s = sprintf('%s\n', C{:});
+    if ~iscellstr (C)
+        error ('C must a cell array of strings');
+    end
+    
+    s = sprintf ('%s\n', C{:});
 
 end
