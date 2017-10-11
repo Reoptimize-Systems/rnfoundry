a4f4639aa8b3858332e44a02cca89aee07a2d4a3
Richard Crozier <a@b.c>
2017-10-11 12:20:44 +0100

    test_nemoh_cylinder.m: add comments, remove InstallDir option (not portable location)

--- wave/matlab-octave/test_nemoh/test_nemoh_cylinder.m
+++ wave/matlab-octave/test_nemoh/test_nemoh_cylinder.m
@@ -1,10 +1,15 @@
-
+% test_nemoh_cylinder.m
+%
+% Test file for nemoh package generating a basic cylinder
+%
+%
 
 dir = getmfilepath ('test_nemoh_cylinder');
 
 inputdir = fullfile (dir, 'Cylinder');
 
-%%
+%% create the Nemoh body
+
 cylinder = nemoh.body (inputdir);
 
 n = 3; % 3 points are required for describing the shape
@@ -15,29 +20,42 @@ z = [0       draft   draft];
 ntheta = 30;
 verticalCentreOfGravity = -2;
 
+% define the body shape using a 3D profile rotated around the z axis
 cylinder.makeAxiSymmetricMesh (r, z, ntheta, verticalCentreOfGravity);
-% cylinder.drawMesh ();
-% axis equal;
 
-%%
+%% draw the course body mesh (will be refined later)
 
+cylinder.drawMesh ();
+axis equal;
+
+%% Create the nemoh simulation, 
+
+% here we insert the cylinder body at creation of the simulation, but could
+% also have done this later by calling addBody
 sim = nemoh.simulation ( inputdir, ...
-                         'InstallDir', '/home/rcrozier/src/nemoh-hg/bin', ...
                          'Bodies', cylinder );
 
-%%
+%% write mesh files
+
+% write mesh file for all bodies (just one in this case)
 sim.writeMeshes ();
 
-%%
+%% process mesh files
+
+% process mesh files for all bodies to make ready for Nemoh
 sim.processMeshes ();
 
-%%
-% sim.drawMesh ();
-% 
-% axis equal;
+%% Draw all meshes in one figure
 
-%%
-         
+sim.drawMesh ();
+
+axis equal;
+
+%% Generate the Nemoh input file
+
+% the following optinal arguments are avaialable, the defaults are also
+% shown.
+%
 % DorIRFCalculation = true;
 % IRFTimeStep = 0.1;
 % IRFDuration = 10;
@@ -49,9 +67,11 @@ sim.processMeshes ();
 % DomainSize = [400, 400];
 % WaterDepth = 0;
 % WaveMeasurementPoint = [0, 0];
-            
+
+% generate file with all default values
 sim.writeNemoh ()
 
-%%
+%% Run Nemoh on the input files
+
 sim.run ()
 
