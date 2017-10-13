eae99852ef2e71c7cd19f866b5dded2c7f76ec9c
Richard Crozier <a@b.c>
2017-10-13 11:34:34 +0100

    Added sphinx youtube extension

--- common/multibody/MBDyn/doc/sphinx/conf.py
+++ common/multibody/MBDyn/doc/sphinx/conf.py
@@ -16,9 +16,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.join (os.path.abspath('.'), 'exts'))
 
 
 # -- General configuration ------------------------------------------------
@@ -33,7 +33,8 @@
 extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinxcontrib.fulltoc' ]
+    'sphinxcontrib.fulltoc', 
+    'youtube' ]
 
 numfig = True
 
