f91701fe43c3a058ba5c06a5dc170b9be658c35f
Richard Crozier <a@b.c>
2017-11-01 10:21:17 +0000

    Updated example nemoh preprocessor script for docs and updated output figures

--- wave/doc/sphinx/examples/example_nemoh_cylinder.m
+++ wave/doc/sphinx/examples/example_nemoh_cylinder.m
@@ -20,7 +20,13 @@ z = [0       draft   draft];
 ntheta = 30;
 verticalCentreOfGravity = -2;
 
-% define the body shape using a 2D profile rotated around the z axis
+% The body shape is defined using a 2D profile rotated around the z axis,
+% created using the makeAxiSymmetricMesh method. The nemoh.body object
+% actually has a makeCylinderMesh method which we could use to create such
+% a mesh, but the makeAxiSymmetricMesh method is used here just for
+% demonstration of the capability. There is also a makeSphereMesh method,
+% other basic shapes may be introduced in the future. Existing NEMOH meshes
+% can be imported, and also STL files.
 cylinder.makeAxiSymmetricMesh (r, z, ntheta, verticalCentreOfGravity);
 
 %% draw the course body mesh (will be refined later)
@@ -38,7 +44,7 @@ sim = nemoh.simulation ( inputdir, ...
 %% write mesh files
 
 % write mesh file for all bodies (just one in this case)
-sim.writeMeshes ();
+sim.writeMesherInputFiles ();
 
 %% process mesh files
 
@@ -60,7 +66,7 @@ sim.writeNemoh ( 'NWaveFreqs', 10, ...
                  'MinMaxWaveFreqs', 2 * pi() *  (1./T) );
 
 % The above code demonstrates the use of optional arguments to writeNemoh
-% to set the desired wave frequencies. If the wave frencies were not
+% to set the desired wave frequencies. If the wave frequencies were not
 % specified a default value would be used. These are not the only possible
 % options for writeNemoh. The following optional arguments are available,
 % and the defaults used if they are not supplied are also shown:
@@ -82,5 +88,42 @@ sim.writeNemoh ( 'NWaveFreqs', 10, ...
 
 %% Run Nemoh on the input files
 
+% The run Method invokes the Nemo programs on the generated input files.
+% Before running it also first generates the ID.dat file and input.txt file
+% for the problem. See the Nemoh documentation for more information about
+% these files. After generating the files, the preprocessor, solver and
+% postprocessor are all run on the input files in sequence.
+%
 sim.run ()
+
+% Other options supported by the run Method, but not used above are:
+%
+% 'Solver' - integer or string. Used to select the solver for
+%   the problem, the following values are possible:
+%
+%   0 or 'direct gauss' or 'directgauss'
+%   1 or 'GMRES'
+%   2 or 'GMRES-FHM' or 'GMRES with FHM'
+%
+%   The string forms are case-insensitive (e.g. 'gmres' works
+%   just as well as 'GMRES'.
+%
+% 'SavePotential' - logical (true/false) flag indicating whther
+%   the potential should be saved for visualisation.
+%
+% 'GMRESRestart' - scalar integer. Restart criteria for the
+%   gmres solver types. Ignored for other solvers. Default is
+%   20.
+%
+% 'GMRESTol' - scalar value. Tolerance for the gmres solver
+%   types. Ignored for other solvers. Default is 5e-7.
+%
+% 'GMRESMaxIterations' - scalar integer. Maximum number of
+%   iterations allowed for the gmres solver types. Ignored for
+%   other solvers. Default is 100.
+%
+% 'Verbose' - logical (true/false) flag determning whether to
+%   display some text on the command line informing on the
+%   progress of the simulation.
+
 
