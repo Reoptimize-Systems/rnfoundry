f43fdfa22b977847b3257157130954aaf8c46090
Richard Crozier <a@b.c>
2018-01-15 08:43:51 +0000

    twoNodeTranslationalForce.m: first working version

--- common/multibody/MBDyn/+mbdyn/+mint/twoNodeTranslationalForce.m
+++ common/multibody/MBDyn/+mbdyn/+mint/twoNodeTranslationalForce.m
@@ -3,12 +3,22 @@ classdef twoNodeTranslationalForce < mbdyn.mint.twoNodeForce
     properties
         
         forceAxis;
+        forceFcn;
+        referenceDisp;
         
     end
     
     methods
         
-        function self = twoNodeTranslationalForce (reference_node, other_node, axisNum)
+        function self = twoNodeTranslationalForce (reference_node, other_node, axisNum, varargin)
+            
+            options.InitialDisplacementZero = true;
+            options.ForceFcn = [];
+            
+            options = parse_pv_pairs (options, varargin);
+            
+            mbdyn.pre.base.checkLogicalScalar ( options.InitialDisplacementZero, ...
+                                                true, 'InitialDisplacementZero' );
             
             self = self@mbdyn.mint.twoNodeForce (reference_node, other_node);
             
@@ -18,14 +28,43 @@ classdef twoNodeTranslationalForce < mbdyn.mint.twoNodeForce
                 self.forceAxis = axisNum;
                 
             else
-                error ('Invalid axis number');
+                error ('axisNum contains invalid axis number, should be an integer 1, 2 or 3');
             end
+            
+            self.referenceDisp = 0;
+            if options.InitialDisplacementZero
+                % make the initial displacement be set to zero so all
+                % future displacements are reported as relative to this
+                % initial position
+                [reldisp, ~] = displacements (self);
+                
+                self.referenceDisp = reldisp;
+                
+            end
+            
+            if ~isempty (options.ForceFcn)
+                assert (isa (options.ForceFcn, 'function_handle'), ...
+                    'ForceFcn is not a function handle' ) ;
+            end
+            
+            self.forceFcn = options.ForceFcn;
             
         end
         
-        function force (self)
+        function [F, ptoforce, reldisp, relvel] = force (self)
             
             [reldisp, relvel] = displacements (self);
+            
+            ptoforce = feval ( self.forceFcn, reldisp, relvel );
+            
+            F_pto_frame = [0, 0, 0];
+            F_pto_frame(self.forceAxis) = ptoforce;
+            
+            F = (F_pto_frame * self.referenceNode.absoluteOrientation.orientationMatrix)' ;
+            F(1:3,2) = -F;
+            
+%             F(1:3,1) = F(1:3,1) + FptoVec(1:3,1);
+%             F(1:3,2,ind) = F(1:3,2) - FptoVec(1:3,1);
             
         end
         
@@ -65,12 +104,12 @@ classdef twoNodeTranslationalForce < mbdyn.mint.twoNodeForce
             % we pre-multiply by the transpose of the reference node
             % orientation in the global frame to get the reverse rotation
             % in the global frame
-            xRforceVec = self.referenceNode.absoluteOrientation.' * xRvec;
-            vRforceVec = self.referenceNode.absoluteOrientation.' * vRvec;
+            xRforceVec = self.referenceNode.absoluteOrientation.orientationMatrix.' * xRvec;
+            vRforceVec = self.referenceNode.absoluteOrientation.orientationMatrix.' * vRvec;
             
             % velocities and displacements are the desired components of
             % the vectors in the translator coordinate system
-            reldisp = xRforceVec(self.forceAxis);
+            reldisp = xRforceVec(self.forceAxis) - self.referenceDisp;
             relvel = vRforceVec(self.forceAxis);
             
         end
