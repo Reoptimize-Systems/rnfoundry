3d818cf39f08e5a6fe9de4207afa041e50f6dd4a
Richard Crozier <a@b.c>
2018-01-18 10:44:36 +0000

    moved joint checkNodeReferenceType method to singleNodeJoint

--- common/multibody/MBDyn/+mbdyn/+pre/joint.m
+++ common/multibody/MBDyn/+mbdyn/+pre/joint.m
@@ -14,27 +14,7 @@ classdef joint < mbdyn.pre.element
     
     methods (Access = protected)
         
-        function ok = checkNodeReferenceType (self, ref, throw)
-            
-            ok = false;
-            if ischar (ref)
-                switch ref
 
-                    case 'node'
-                        ok = true;
-                    case 'local'
-                        ok = true;
-                    case 'global'
-                        ok = true;
-
-                end
-            end
-            
-            if ~ok && throw
-                error ('invalid reference type, should be ''node'' or ''global''');
-            end
-            
-        end
         
     end
     
--- common/multibody/MBDyn/+mbdyn/+pre/singleNodeJoint.m
+++ common/multibody/MBDyn/+mbdyn/+pre/singleNodeJoint.m
@@ -9,6 +9,7 @@ classdef singleNodeJoint < mbdyn.pre.joint
     
     methods
         function self = singleNodeJoint (node)
+            % generic base class for joints which constrain a single node
         
             self.checkIsStructuralNode (node, true);
             
@@ -24,6 +25,37 @@ classdef singleNodeJoint < mbdyn.pre.joint
     
     methods (Access = protected)
         
+        function ok = checkNodeReferenceType (self, ref, throw)
+            % checks that the specified reference frame is valid
+            %
+            % Syntax
+            %
+            % ok = checkNodeReferenceType (jntobj, ref, throw)
+            %
+            % Input
+            %
+            %  jntobj - mbdyn.pre.singleNodeJoint object
+            %
+            %  ref - char array specifying the reference frame
+            %    in which a position is defined realtive to a node in a
+            %    single node joint. Valid strings are: 'node', 'local' and
+            %    'global'.
+            %
+            %  throw - logical flag determining whether an error is thrown
+            %   by checkNodeReferenceType if ref fails check
+            %
+            % Output
+            %
+            %  ok - logical flag indicating if check was passed
+            %
+            % See Also: 
+            %
+            
+            allowedstrs = {'node', 'local', 'global'};
+            
+            ok = self.checkAllowedStringInputs (ref, allowedstrs, throw, 'node reference type');
+            
+        end
         
     end
     
