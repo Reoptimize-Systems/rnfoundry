59d7cc1790f4fab26ac3cf5063543b577f4b3198
Richard Crozier <a@b.c>
2018-01-25 09:06:53 +0000

    contour2dtable.m and contourf2dtable.m: changed syntax so labels etc. can be supplied

--- common/matlab-octave/plotting_tools/contour2dtable.m
+++ common/matlab-octave/plotting_tools/contour2dtable.m
@@ -17,26 +17,56 @@ function [xq,yq,zq] = contour2dtable(x, y, data, varargin)
 %    corresponding to each combination of the independent values provided
 %    in the x and y vectors
 %
-% Example
+% Examples
 %
 % x = 1:10
 % y = 0.5:0.5:5
 % data = bsxfun(@times, x, y')
 % contour2dtable(x, y, data)
 %
-% See also, mesh
+% See also: contourf2dtable.m surfplot2dtable.m
 %
 
 % Copyright Richard Crozier 2015
 
-    [newx, newy, newz] = makevars2dtable23dplot(x, y, data);
+    options.XLabel = '';
+    options.YLabel = '';
+    options.ZLabel = '';
+    options.Title = '';
+    options.ContourArgs = {};
+    options.Axes = [];
+    
+    options = parse_pv_pairs (options, varargin);
+    
+    [newx, newy, newz] = makevars2dtable23dplot (x, y, data);
     
     % now mesh the data
-    [xq,yq] = meshgrid(x,y);
+    [xq,yq] = meshgrid (x,y);
     
-    zq = griddata(newx, newy, newz, xq, yq);
+    zq = griddata (newx, newy, newz, xq, yq);
+    
+    if isempty (options.Axes)
+        figure;
+        axes;
+    end
     
     % and create the mesh plot
-    contour(xq,yq,zq, varargin{:});
+    contour (xq,yq,zq, options.ContourArgs{:});
 
+    if ~isempty (options.XLabel)
+        xlabel (options.XLabel);
+    end
+
+    if ~isempty (options.XLabel)
+        ylabel (options.YLabel);
+    end
+    
+    if ~isempty (options.XLabel)
+        zlabel (options.ZLabel);
+    end
+    
+    if ~isempty (options.Title)
+        title (options.Title);
+    end
+    
 end
