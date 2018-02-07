72f7a921c7b206f15618f745326a17fc1231cee5
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-02-07 23:06:56 +0000

    revolutePin.m: added new drawing code, added methods related to getting positions and changed input options to handle references better

--- common/multibody/MBDyn/+mbdyn/+pre/revolutePin.m
+++ common/multibody/MBDyn/+mbdyn/+pre/revolutePin.m
@@ -4,7 +4,9 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
     properties (GetAccess = public, SetAccess = protected)
         
         relativeOffset;
+        relativeOffsetReference;
         nodeRelativeOrientation;
+        nodeRelativeOrientationReference;
             
         pinPosition;
         absolutePinOrientation;
@@ -58,7 +60,9 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
             %    supplied as an mbdyn.pre.orientmat object or a 3x3
             %    orientation matrix.
             %
-            %  'NodeRelativeOrientation' - 
+            %  'NodeRelativeOrientation' - orientation of the pin relative
+            %    to the attached node. If both this and PinOrientation are
+            %    supplied they must match.
             %
             %  'NodeOffsetReference' - string defining the reference frame
             %    in which the relative_offset (see above) is defined. By
@@ -99,18 +103,21 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
             
             self.type = 'revolute pin';
             
-            self.checkCartesianVector (relative_offset, true);
-            self.checkCartesianVector (absolute_pin_position, true);
-            self.checkOrientationMatrix (options.NodeRelativeOrientation, true);
-            self.checkOrientationMatrix (options.PinOrientation, true);
-            self.checkNodeReferenceType (options.NodeOffsetReference, true);
-            self.checkNodeReferenceType (options.NodeOrientationReference, true);
+            self.checkCartesianVector (relative_offset, true, 'relative_offset');
+            self.checkCartesianVector (absolute_pin_position, true, 'absolute_pin_position');
+            self.checkOrientationMatrix (options.NodeRelativeOrientation, true, 'NodeRelativeOrientation');
+            self.checkOrientationMatrix (options.PinOrientation, true, 'PinOrientation');
+            self.checkNodeReferenceType (options.NodeOffsetReference, true, 'NodeOffsetReference');
+            self.checkNodeReferenceType (options.NodeOrientationReference, true, 'NodeOrientationReference');
 
-            self.relativeOffset = {'reference', options.NodeOffsetReference, relative_offset};
-            self.nodeRelativeOrientation = {'reference', options.NodeOrientationReference, self.getOrientationMatrix(options.NodeRelativeOrientation)};
+            self.relativeOffset = relative_offset;
+            self.relativeOffsetReference = options.NodeOffsetReference;
+            
+            self.nodeRelativeOrientation = options.NodeRelativeOrientation;
+            self.nodeRelativeOrientationReference = options.NodeOrientationReference;
 
             self.pinPosition = absolute_pin_position;
-            self.absolutePinOrientation = self.getOrientationMatrix (options.PinOrientation);
+            self.absolutePinOrientation = options.PinOrientation;
             
             self.initialTheta = options.InitialTheta;
             
@@ -122,17 +129,31 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
             
             str = self.addOutputLine (str, sprintf('%d', self.node.label), 2, true, 'node label');
             
-            out = self.makeCellIfNot (self.relativeOffset);
-            str = self.addOutputLine (str, self.commaSepList ('position', out{:}), 3, true, 'node relative position' );
+%             out = self.makeCellIfNot (self.relativeOffset);
+            str = self.addOutputLine ( str, ...
+                                       self.commaSepList ( 'position', ...
+                                                           'reference', ...
+                                                           self.relativeOffsetReference,...
+                                                           self.relativeOffset ), ...
+                                       3, ...
+                                       true, ...
+                                       'node relative position' );
             
             if ~isempty (self.nodeRelativeOrientation)
-                out = self.makeCellIfNot (self.nodeRelativeOrientation);
-                str = self.addOutputLine (str, self.commaSepList ('orientation', out{:}), 3, true, 'node relative orientation');
+%                 out = self.makeCellIfNot (self.nodeRelativeOrientation);
+                str = self.addOutputLine ( str, ...
+                                           self.commaSepList ( 'orientation', ...
+                                                               'reference', ...
+                                                               self.nodeRelativeOrientationReference, ...
+                                                               self.nodeRelativeOrientation ), ...
+                                           3, ...
+                                           true, ...
+                                           'node relative orientation' );
             end
             
             out = self.makeCellIfNot (self.pinPosition);
             addcomma = ~(isempty (self.absolutePinOrientation) && isempty (self.initialTheta));
-            str = self.addOutputLine (str, self.commaSepList ('position', out{:}), 2, addcomma, 'pin absolute position');
+            str = self.addOutputLine ( str, self.commaSepList ('position', out{:}), 2, addcomma, 'pin absolute position');
             
             if ~isempty (self.absolutePinOrientation)
                 addcomma = ~isempty (self.initialTheta);
@@ -149,6 +170,60 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
             
         end
         
+        function [ref_pos, ref_orient] = reference (self)
+            % returns a reference object for the joint position
+            
+            switch self.relativeOffsetReference
+                
+                case {'node', 'local'}
+                    posref = self.node.reference ();
+                case {'global', ''}
+                    posref = mbdyn.pre.globalref ();
+                otherwise
+                    error ('Unrecognised reference type');
+                    
+            end
+            
+            switch self.nodeRelativeOrientationReference
+                
+                case {'node', 'local'}
+                    orientref = self.node.reference ();
+                case  {'global', ''}
+                    orientref = mbdyn.pre.globalref ();
+                otherwise
+                    error ('Unrecognised reference type');
+                    
+            end
+            
+            if ischar (self.relativeOffset)
+                reloffset = [0;0;0];
+            else
+                reloffset = self.relativeOffset;
+            end
+            
+            if ischar (self.nodeRelativeOrientation) ...
+                || isempty (self.nodeRelativeOrientation)
+                
+                relorient = mbdyn.pre.orientmat ('eye');
+                
+            else
+                relorient = mbdyn.pre.orientmat ('orientation', self.getOrientationMatrix (self.nodeRelativeOrientation));
+            end
+            
+            ref_pos = mbdyn.pre.reference ( reloffset, ...
+                                            relorient, ...
+                                            [], ...
+                                            [], ...
+                                            'Parent', posref );
+                                        
+            ref_orient = mbdyn.pre.reference ( reloffset, ...
+                                               relorient, ...
+                                               [], ...
+                                               [], ...
+                                               'Parent', orientref );
+                                    
+        end
+        
         function draw (self, varargin)
             
             options.AxesHandle = self.drawAxesH;
@@ -157,11 +232,64 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
             
             options = parse_pv_pairs (options, varargin);
             
-            draw@mbdyn.pre.element ( self, ...
-                'AxesHandle', options.AxesHandle, ...
-                'ForceRedraw', options.ForceRedraw, ...
-                'Mode', options.Mode );
+%             draw@mbdyn.pre.element ( self, ...
+%                 'AxesHandle', options.AxesHandle, ...
+%                 'ForceRedraw', options.ForceRedraw, ...
+%                 'Mode', options.Mode );
+% 
+%             self.setTransform ();
 
+            if options.ForceRedraw
+                self.needsRedraw = true;
+            end
+            
+            self.checkAxes (options.AxesHandle);
+            
+            nodepos = self.node.absolutePosition;
+            [jref_pos, ~] = self.reference ();
+            jpos = jref_pos.pos ();
+                
+            if ~self.needsRedraw
+                % always have to redraw line, can't just transform objects
+                delete (self.shapeObjects{1})
+                self.shapeObjects{1} =  line ( self.drawAxesH, ...
+                                               [ nodepos(1), jpos(1) ], ...
+                                               [ nodepos(2), jpos(2) ], ...
+                                               [ nodepos(3), jpos(3) ], ...
+                                               'Color', self.drawColour );
+                                       
+            end
+            
+            if isempty (self.shapeObjects) ...
+                    || self.needsRedraw
+                % a full redraw is needed (and not just a modification of
+                % transform matrices for the objects).
+                
+                % delete the current patch object
+                self.deleteAllDrawnObjects ();
+                
+                self.shapeObjects = { line( self.drawAxesH, ...
+                                            [ nodepos(1), jpos(1) ], ...
+                                            [ nodepos(2), jpos(2) ], ...
+                                            [ nodepos(3), jpos(3) ], ...
+                                            'Color', self.drawColour ), ...
+                                      line( self.drawAxesH, ...
+                                            [ 0, 0 ], ...
+                                            [ 0, 0 ], ...
+                                            [ -self.sz/2, self.sz/2 ], ...
+                                            'Parent', self.transformObject, ...
+                                            'Color', self.drawColour, ...
+                                            'LineStyle', '--' )
+                                    };
+                
+                self.needsRedraw = false;
+
+%                 if options.Light
+%                     light (self.drawAxesH);
+%                 end
+                
+            end
+            
             self.setTransform ();
             
         end
@@ -170,17 +298,11 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
     
     methods (Access = protected)
         
-        function setTransform (self)
+       function setTransform (self)
             
-            ref_pin = mbdyn.pre.reference ( self.pinPosition, ...
-                                            mbdyn.pre.orientmat ('orientation', self.absolutePinOrientation), ...
-                                            [], []);
-                                        
-%             ref_joint = mbdyn.pre.reference (self.relativeOffset, ...
-%                 mbdyn.pre.orientmat ('orientation', self.nodeRelativeOrientation), ...
-%                 [], [], 'Parent', ref_pin);
+            [ref_pos, ref_orient] = self.reference ();
             
-            M = [ ref_pin.orientm.orientationMatrix , ref_pin.pos; ...
+            M = [ ref_orient.orientm.orientationMatrix , ref_pos.pos; ...
                   0, 0, 0, 1 ];
             
             % matlab uses different convention to mbdyn for rotation
@@ -189,7 +311,7 @@ classdef revolutePin < mbdyn.pre.singleNodeJoint
                   
             set ( self.transformObject, 'Matrix', M );
             
-        end
+        end 
         
     end
     
