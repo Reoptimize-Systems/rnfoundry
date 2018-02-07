b809bf9507938e7fb6f16f12e514e064c7732607
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-02-07 23:10:57 +0000

    stringDrive.m: implemented former stub and added help

--- common/multibody/MBDyn/+mbdyn/+pre/stringDrive.m
+++ common/multibody/MBDyn/+mbdyn/+pre/stringDrive.m
@@ -8,16 +8,46 @@ classdef stringDrive < mbdyn.pre.drive
     
     methods
         
-        function self = stringDrive (string, varargin)
-            
-            error ('not yet implemented')
-        
+        function self = stringDrive (string)
+            % drive which returns the value of a mathematical expression
+            %
+            % Syntax
+            %
+            % sd = stringDrive (string)
+            %
+            % Description
+            %
+            % stringDrive is a drive which represents a mathematical
+            % expression which is evaluated at each time step of the
+            % simulation. 
+            %
+            % Input
+            %
+            %  string - expression parsed by the math parser every time the
+            %    drive is invoked. Two special variable may be used in the
+            %    expression 'Time' which is the current simulation time,
+            %    and 'Var'. The value of 'Var' is set by the caller of the
+            %    string drive
+            %
+            % Output
+            %
+            % sd - mbdyn.pre.stringDrive object
+            %
+            % See Also: 
+            %
+
             if ~ischar (string)
                 error ('''string'' must be a char array')
             end
             
             self.string = string;
                 
+        end
+        
+        function str = generateOutputString (self)
+            
+            str = self.commaSepList (self.type, ['"', self.string, '"']);
+            
         end
         
     end
