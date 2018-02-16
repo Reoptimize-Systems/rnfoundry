f50056108b5175bcbf756515248dc39a4a7655e9
Richard Crozier <a@b.c>
2018-02-16 17:04:51 +0000

    directDrive.m: added help

--- common/multibody/MBDyn/+mbdyn/+pre/directDrive.m
+++ common/multibody/MBDyn/+mbdyn/+pre/directDrive.m
@@ -1,16 +1,43 @@
 classdef directDrive < mbdyn.pre.drive
     
-    properties (GetAccess = public, SetAccess = protected)
-        
-        
+    properties (GetAccess = public, SetAccess = private)
+
     end
     
     methods
         
+        function self = directDrive ()
+            % Constructor for direct drive
+            %
+            % Syntax
+            %
+            % dd = mbdyn.pre.directDrive ()
+            %
+            % Description
+            %
+            % Transparently returns the input value; the arglist is empty.
+            % It is useful in conjunction with those drive callers that
+            % require their output to be fed into another drive caller,
+            % like the dof, node and element drive callers, when the output
+            % needs to be used as is.
+            %
+            %
+            % Output
+            %
+            %  dd - mbdyn.pre.rampDrive object
+            %
+            %
+            %
+            % See Also: 
+            %
+            
+            self.type = 'direct';
+            
+        end
         
         function str = generateOutputString (self)
             
-            str = 'direct';
+            str = self.type;
             
         end
         
