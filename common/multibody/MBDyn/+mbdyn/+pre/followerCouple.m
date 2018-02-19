6b4a21e82a9d1a47c23102d8b6699fc19fa0cfd9
Richard Crozier <a@b.c>
2018-02-19 08:56:25 +0000

    followerCouple.m: added skeleto help

--- common/multibody/MBDyn/+mbdyn/+pre/followerCouple.m
+++ common/multibody/MBDyn/+mbdyn/+pre/followerCouple.m
@@ -9,39 +9,44 @@ classdef followerCouple < mbdyn.pre.structuralCouple
     methods
         
         function self = followerCouple (node, couple_value, varargin)
-            % <force_arglist> ::=
-            % <node_label> ,
-            % [ position , (Vec3) <relative_arm> , ]
-            % (TplDriveCaller<Vec3>) <couple_value> 
+            % followerCouple constructor (a type of structural couple)
+            %
+            % Syntax
+            %
+            % fsc = mbdyn.pre.followerCouple (node, couple_value)
+            % fsc = mbdyn.pre.followerCouple (..., 'Parameter', value)
+            %
+            % Description
+            %
+            % 
+            %
+            % Input
+            %
+            %  node - 
+            %
+            %  couple_value - 
+            %
+            % Output
+            %
+            %  fsc - 
+            %
+            %
+            %
+            % See Also: 
+            %
             
             options.Position = [];
             
             options = parse_pv_pairs (options, varargin);
             
-            self = self@mbdyn.pre.structuralCouple (node, ...
+            self = self@mbdyn.pre.structuralCouple (node, 'follower', couple_value, ...
                             'Position', options.Position);
-
-            self.checkTplDriveCaller (couple_value, true, 'couple_value');
-            
-            self.couple = couple_value;
-            
-            self.type = 'follower';
             
         end
         
         function str = generateOutputString (self)
             
-            str = generateOutputString@mbdyn.pre.couple (self);
-            
-            str = self.addOutputLine (str, sprintf('%d', self.node.label), 2, true);
-            
-            if ~isempty (self.position)
-                str = self.addOutputLine (str, self.commaSepList ('position', self.position), 2, true);
-            end
-            
-            str = self.addOutputLine (str, self.couple.generateOutputString(), 2, false);
-
-            str = self.addOutputLine (str, ';', 1, false, 'end follower structural couple');
+            str = generateOutputString@mbdyn.pre.structuralCouple (self);
             
         end
         
