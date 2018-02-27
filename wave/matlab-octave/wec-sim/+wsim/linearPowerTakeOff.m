c8dc0b6a795fcabca790302d1ced941099783976
Richard Crozier <a@b.c>
2018-02-27 16:57:25 +0000

    linearPowerTakeOff.m: added some help

--- wave/matlab-octave/wec-sim/+wsim/linearPowerTakeOff.m
+++ wave/matlab-octave/wec-sim/+wsim/linearPowerTakeOff.m
@@ -15,7 +15,56 @@ classdef linearPowerTakeOff < wsim.powertakeoff
     methods
         
         function self = linearPowerTakeOff (reference_node, other_node, axisNum, varargin)
-            
+            % construct a wsim.linearPowerTakeOff object
+            %
+            %
+            % Syntax
+            %
+            % lpto = wsim.linearPowerTakeOff (reference_node, other_node, axisNum)
+            % lpto = wsim.linearPowerTakeOff (..., 'Parameter', value)
+            %
+            % Description
+            %
+            % wsim.linearPowerTakeOff is a class representing a linear
+            % power-take-off mechanism in a wave energy converter. It
+            % facilitates sending the correct forces to an MBDyn multibody
+            % simulation. wsim.linearPowerTakeOff applies forces between
+            % two MBDyn nodes based on their relative displacement. Forces
+            % are applied based on the relative displacement and velocity
+            % of the two nodes along axis 3 in the reference frame of the
+            % first node. It is assumed that the nodes motion is
+            % constrained appropriately by other MBDyn elements (e.g. 
+            %
+            % Input
+            %
+            %  reference_node - mbdyn.pre.structuralNode6dof object
+            %
+            %  other_node - 
+            %
+            %  axisNum - 
+            %
+            % Additional options my be supplied as parameter-value pairs.
+            % The avaialable options are:
+            %
+            %  'InitialDisplacementZero' - optional true/false flag
+            %    indicating whether the intial relative displacement (along
+            %    axis 3 of the reference node) in the global frame should
+            %    be taken as the reference point for displacement during
+            %    the simulation, i.e. the PTO starts with an initial
+            %    displacement of zero for the purposes of force calulation,
+            %    and future displacement is measured relative to this
+            %    initial position. If false, the raw position is used
+            %    instead. Default is true if not supplied.
+            %
+            % Output
+            %
+            %  lpto - a wsim.linearPowerTakeOff
+            %
+            %
+            %
+            % See Also: 
+            %
+
             options.InitialDisplacementZero = true;
             options.ForceFcn = [];
             
