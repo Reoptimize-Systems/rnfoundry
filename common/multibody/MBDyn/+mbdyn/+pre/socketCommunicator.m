e1d90614ac4fc5b368a186284e61a7db5f9f917e
Richard Crozier <a@b.c>
2018-03-08 16:16:50 +0000

    socketCommunicator.m: added help

--- common/multibody/MBDyn/+mbdyn/+pre/socketCommunicator.m
+++ common/multibody/MBDyn/+mbdyn/+pre/socketCommunicator.m
@@ -10,7 +10,74 @@ classdef socketCommunicator < mbdyn.pre.externalFileCommunicator
     methods
         
         function self = socketCommunicator (varargin)
-            
+            % socketCommunicator constructor
+            %
+            % Syntax
+            %
+            % sc = socketCommunicator ('Parameter', value)
+            %
+            % Description
+            %
+            % External file communicator for use with the
+            % mbdyn.pre.externalStructuralForce element. It specifies the
+            % use of a socket based communicator to transmit forces and
+            % kinematics between MBDyn and the external software.
+            %
+            % Input
+            %
+            % Arguments may be supplied as parameter-value pairs. The
+            % available options are:
+            %
+            %  'SleepTime' - determines how long MBDyn is supposed to sleep
+            %    while waiting for a new input file to appear or for an old
+            %    output file to disappear.
+            %
+            %  'Coupling' - character vector determining the type of
+            %    coupling with the external software. Can be one of
+            %    'staggered', 'tight' or 'loose'. For details on the
+            %    implications of each of these, see the section on External
+            %    Force in the official MBDyn manual.
+            %
+            %  'SendAfterPredict' - character vector which can be 'yes' or
+            %    'no'. This indicates whether MBDyn must send the predicted
+            %    motion or not when playing a tight coupling loop.
+            %
+            %  'Create' - character vector which can be 'yes' or 'no'. If
+            %    'yes', it indicates that MBDyn will create the socket, and
+            %    the peer will have to connect to it. Otherwise, when set
+            %    to 'no', it indicates that MBDyn will try to connect to an
+            %    already existing socket created by the peer. Connecting to
+            %    a peer is attempted while reading the input file. Sockets
+            %    are created when the input file has been read. MBDyn waits
+            %    until all sockets have been connected to by peers before
+            %    the simulation is started.
+            %
+            %  'Host' - MBDyn supports local (unix) sockets, defined using
+            %    the Path parameter, and inet sockets, defined using the
+            %    Port and Host parameter. When using inet sockets, Host
+            %    should be a character vector containing the inet address
+            %    of the interface MBDyn will listen on. The use of Host and
+            %    Port is mutually exclusive to the use of the Path option.
+            %
+            %  'Port' - Scalar integer. The Port option is used in
+            %    conjuction with the Host option to specify the port inet
+            %    sockets will use to communicate on.
+            %
+            %  'Path' - MBDyn supports local (unix) sockets, defined using
+            %    the Path parameter, and inet sockets, defined using the
+            %    Port and Host parameter. When using local sockets, Path
+            %    should be a character vector containing the file path to
+            %    the local socket which MBDyn will listen on.
+            %
+            % Output
+            %
+            %  sc - mbdyn.pre.socketCommunicator
+            %
+            %
+            %
+            % See Also: 
+            %
+
             options.SleepTime = [];
             options.Coupling = [];
             options.SendAfterPredict = 'yes';
