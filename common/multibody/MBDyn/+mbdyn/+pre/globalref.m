dcad2cb07ac2d726311a7274a3af84f9ae443fcb
Richard Crozier <a@b.c>
2018-03-26 13:25:04 +0100

    globalref.m: documented properties

--- common/multibody/MBDyn/+mbdyn/+pre/globalref.m
+++ common/multibody/MBDyn/+mbdyn/+pre/globalref.m
@@ -6,11 +6,11 @@ classdef globalref < handle
    
    properties (GetAccess = public, SetAccess = private )
        
-       name = 'global';
-       pos = [0; 0; 0];
-       orientm = mbdyn.pre.orientmat ('orientation', eye (3));
-       v = [0; 0; 0];
-       omega = [0; 0; 0];
+       name = 'global'; % name for the reference (used in plots)
+       pos = [0; 0; 0]; % position of the reference in the global frame: [0; 0; 0]
+       orientm = mbdyn.pre.orientmat ('orientation', eye (3)); % orientation of the reference in the global frame (3 x3 identity matrix)
+       v = [0; 0; 0]; % velocity of the reference in the global frame: [0; 0; 0]
+       omega = [0; 0; 0]; % angular velocity of the reference in the global frame :[0; 0; 0]
        
    end
 
