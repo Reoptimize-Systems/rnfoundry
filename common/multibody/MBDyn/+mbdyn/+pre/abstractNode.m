2e79f668df301053b7f3280c92c815e873a849f0
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-03-26 12:56:44 +0100

    abstractNode.m: added help

--- common/multibody/MBDyn/+mbdyn/+pre/abstractNode.m
+++ common/multibody/MBDyn/+mbdyn/+pre/abstractNode.m
@@ -1,20 +1,91 @@
 classdef abstractNode < mbdyn.pre.node
-    
+% replace h1 line
+%
+% Syntax
+%
+%
+% Description
+%
+% In MBDyn, abstract nodes are ancestors of all scalar node types. Many
+% genel and electric elements can be connected to abstract nodes as well,
+% since they directly use the ancestor class.
+%
+% mbdyn.pre.abstractNode Methods:
+%
+%   abstractNode - constructor
+%   draw - draw/plot the element. Inactive for this element
+%   generateOutputString - generate an mbdyn input file string for the 
+%    element
+%
+%
+% See Also: mbdyn.pre.node
+%
+
     properties (GetAccess = public, SetAccess = protected)
         
-        algebraicOrDifferential;
-        value;
-        derivative;
+        algebraicOrDifferential; % specifies whether the node is algabraic or differential type
+        value; % value of the node
+        derivative; % derivative of the node
         
     end
 
     methods
         
         function self = abstractNode (varargin)
+            % mbdyn.pre.abstractNode constructor
+            %
+            % Syntax
+            %
+            % an = mbdyn.pre.abstractNode ('Parameter', Value)
+            %
+            % Description
+            %
+            % In MBDyn, abstract nodes are ancestors of all scalar node
+            % types. Many genel and electric elements can be connected to
+            % abstract nodes as well, since they directly use the ancestor
+            % class.
+            %
+            % Input
+            %
+            % Arguments may be supplied as parameter-value pairs. The
+            % available options are:
+            %
+            %  'Value' - The value of the node
+            %
+            %  'Derivative' - The derivative of the node
+            %
+            %  'AlgebraicOrDifferential' - character vector which can be
+            %    either 'algebraic' or 'differential'. By default abstract
+            %    nodes are differential type.
+            %
+            % 'HumanReadableLabel' - a text string intended to provide a
+            %   meaningful label. For some node types this may optionally
+            %   be displayed when they they are drawn.
+            %
+            %  'Scale' - optional. Used to control the scaling of the
+            %    residual for the node equations before performing testing
+            %    whether the required tolerance has been met. For more
+            %    information see the help for mbdyn.pre.initialValueProblem
+            %    (see the 'ScaleResidual' option in the constructor), and
+            %    mbdyn.pre.system (see the 'DefaultScales' option in the
+            %    constructor).
+            %
+            %  'Output' - true/false flag, or a character vector which must
+            %    be 'yes' of 'no'. Determines wheter this node will produce
+            %    output. By default output will be produced.
+            %
+            % Output
+            %
+            %  an - mbdyn.pre.abstractNode object
+            %
+            %
+            %
+            % See Also: mbdyn.pre.node
+            %
+
             
             options.Value = [];
             options.Derivative = [];
-%             options.UniqueTextLabelPrefix = 'node';
             options.AlgebraicOrDifferential = 'differential';
             options.HumanReadableLabel = '';
             options.Scale = [];
@@ -41,7 +112,10 @@ classdef abstractNode < mbdyn.pre.node
                 self.derivative = options.Derivative;
             end
             
-            self.checkAllowedStringInputs  ( options.AlgebraicOrDifferential, {'algebraic', 'differential'}, true, 'AgabraicOrDifferential');
+            self.checkAllowedStringInputs  ( options.AlgebraicOrDifferential, ...
+                                             {'algebraic', 'differential'}, ...
+                                             true, ...
+                                             'AlgebraicOrDifferential' );
             
             self.type = 'abstract';
             self.algebraicOrDifferential = options.AlgebraicOrDifferential;
@@ -49,7 +123,28 @@ classdef abstractNode < mbdyn.pre.node
         end
         
         function str = generateOutputString (self)
-            
+            % generate an mbdyn input file string for the element
+            %
+            % Syntax
+            %
+            % str = generateOutputString (an)
+            %
+            % Description
+            %
+            % generateOutputString is a method shared by all MBDyn
+            % components and is called to generate a character vector used
+            % to construct an MBDyn input file.
+            %
+            % Input
+            %
+            %  an - mbdyn.pre.abstractNode
+            %
+            % Output
+            %
+            %  str - character vector for insertion into an MBDyn input
+            %   file.
+            %
+
             nodestr = generateOutputString@mbdyn.pre.node (self);
             
             str = self.addOutputLine ('', sprintf('abstract : %d, %s', self.label, self.algebraicOrDifferential), 1, false);
@@ -75,7 +170,8 @@ classdef abstractNode < mbdyn.pre.node
         end
         
         function draw (self, varargin)
-            % do nothing by default
+            % draw/plot the element. Inactive for this element
+            
         end
         
     end
