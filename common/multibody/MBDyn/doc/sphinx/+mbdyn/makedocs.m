8b4d8d10144e940b341996c767170458c10ac29d
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-03-26 13:37:44 +0100

    +mbdyn/makedocs.m generate api docs and example output figures

--- common/multibody/MBDyn/doc/sphinx/+mbdyn/makedocs.m
+++ common/multibody/MBDyn/doc/sphinx/+mbdyn/makedocs.m
@@ -1,22 +1,32 @@
 % makedocs.m
 %
-% Script to generate files for mbdyn docs
+% Script to generate files for mbdyn docs and build the docs
 
 % files required for embedding in docs
-
-
-
-
 thisfiledir = getmfilepath ('mbdyn.makedocs');
 
-outputdir = fullfile (thisfiledir, '..', 'api_reference');
+imagedir = fullfile (thisfiledir, '..', 'images');
 
-mkdir (outputdir);
+
+%% Run examples and save figures
+
+% save_example_figs ('example_double_pendulum', imagedir, 1:6);
+save_example_figs ('example_basic_cosimulation', imagedir, 1:3);
+
+
+%% Make API reference
+apidir = fullfile (thisfiledir, '..', 'api_reference');
+
+mkdir (apidir);
+
+pre_apidir = fullfile (apidir, 'pre');
+mkdir (pre_apidir);
 
 % generated docs from class file help
 mbdyn_pre_class_list = dir (fullfile (getmfilepath ('mbdyn.pre.element'), '*.m'));
 
-exclude_list = { 'baseSystem.m' };
+exclude_list = { 'baseSystem.m', ...
+                 'subsystem.m' };
 
 
 for ind = 1:numel(mbdyn_pre_class_list)
@@ -26,7 +36,7 @@ for ind = 1:numel(mbdyn_pre_class_list)
         rst = help2rst (['mbdyn.pre.', mbdyn_pre_class_list(ind).name(1:end-2)], ...
                         'SectionChars', '=-^"+');
 
-        outpufile = fullfile (outputdir, [mbdyn_pre_class_list(ind).name(1:end-2), '.rst']);
+        outpufile = fullfile (pre_apidir, [mbdyn_pre_class_list(ind).name(1:end-2), '.rst']);
 
         cellstr2txtfile (outpufile, rst);
     
@@ -34,6 +44,77 @@ for ind = 1:numel(mbdyn_pre_class_list)
     
 end
 
-cd (fullfile (outputdir, '..'));
+
+mint_apidir = fullfile (apidir, 'mint');
+mkdir (mint_apidir);
+
+% generated docs from class file help
+mbdyn_pre_class_list = dir (fullfile (getmfilepath ('mbdyn.mint.MBCNodal'), '*.m'));
+
+exclude_list = { 'externalForces.m', ...
+                 'torque.m', ...
+                 'force.m' };
+
+for ind = 1:numel(mbdyn_pre_class_list)
+    
+    if ~any (strcmp (mbdyn_pre_class_list(ind).name, exclude_list )) 
+    
+        rst = help2rst (['mbdyn.mint.', mbdyn_pre_class_list(ind).name(1:end-2)], ...
+                        'SectionChars', '=-^"+');
+
+        outpufile = fullfile (mint_apidir, [mbdyn_pre_class_list(ind).name(1:end-2), '.rst']);
+
+        cellstr2txtfile (outpufile, rst);
+    
+    end
+    
+end
+
+post_apidir = fullfile (apidir, 'post');
+mkdir (post_apidir);
+
+rst = help2rst ( 'mbdyn.postproc', ...
+                 'SectionChars', '=-^"+');
+
+outpufile = fullfile (post_apidir, 'mbdyn.postproc.rst');
+
+cellstr2txtfile (outpufile, rst);
+        
+        
+%% build the docs
+cd (fullfile (apidir, '..'));
 
 [status, out] = cleansystem ('make html');
+
+%% functions
+
+function save_example_figs (exname, imdir, fignums_to_save)
+
+    close all
+    run (exname);
+    
+    handles = findall(0,'type','figure');
+    
+    fname = fullfile (imdir, [exname, '_%d.png']);
+    
+    for ind = 1:numel (handles)
+        
+        fignum = get (handles(ind), 'Number');
+        
+%         tightfig (handles(ind));
+        
+        switch fignum
+            
+            case num2cell ( fignums_to_save )
+                
+                saveas (handles(ind), sprintf (fname, fignum));
+                
+            otherwise
+                
+        end
+
+    end
+    
+    close all
+    
+end
