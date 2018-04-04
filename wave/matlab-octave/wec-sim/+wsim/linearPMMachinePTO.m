4ba6ca97bd040026e6ba8a3d406bb66ae29dfcd5
Richard Crozier <a@b.c>
2018-04-04 18:42:37 +0100

    +wsim/linearPMMachinePTO.m: lots of developemnt to make it work

--- wave/matlab-octave/wec-sim/+wsim/linearPMMachinePTO.m
+++ wave/matlab-octave/wec-sim/+wsim/linearPMMachinePTO.m
@@ -1,20 +1,26 @@
 classdef linearPMMachinePTO < wsim.powerTakeOff
     % power take-off from relative linear displacement of two nodes
    
+    properties (GetAccess = public, SetAccess = private)
+        
+        initialInternalTimeStep;
+        maxInternalTimeStep;
+        design;
+        simoptions;
+        
+    end
+    
     properties (GetAccess = private, SetAccess = private)
         
         mbdynForceObj;
-        
-        % internal logging variables
-        lastInternalForce;
-        lastRelativeDisplacement;
-        lastRelativeVelocity;
+        machineODESolver;
+        initialPhaseCurrents; % Initial machine phase currents
         
     end
     
     methods
         
-        function self = linearPMMachinePTO (reference_node, other_node, axisNum, varargin)
+        function self = linearPMMachinePTO (reference_node, other_node, axisNum, design, simoptions, varargin)
             % construct a wsim.linearPMMachinePTO object
             %
             %
@@ -39,9 +45,15 @@ classdef linearPMMachinePTO < wsim.powerTakeOff
             %
             %  reference_node - mbdyn.pre.structuralNode6dof object
             %
-            %  other_node - 
+            %  other_node - mbdyn.pre.structuralNode6dof object
             %
-            %  axisNum - 
+            %  axisNum - axis in the frame of the reference node. Forces
+            %   will be applied to the node in a direction parallel to this
+            %   axis.
+            %
+            %  design - 
+            %
+            %  simoptions - 
             %
             % Additional options my be supplied as parameter-value pairs.
             % The avaialable options are:
@@ -62,26 +74,54 @@ classdef linearPMMachinePTO < wsim.powerTakeOff
             %
             %
             %
-            % See Also: 
+            % See Also: wsim.powerTakeOff, wsim.linearPowerTakeOff
             %
 
             options.InitialDisplacementZero = true;
             options.ForceFcn = [];
+            options.InitialPhaseCurrents = [0,0,0];
+            options.InitialStep = [];
+            options.MaxStep = [];
             
             options = parse_pv_pairs (options, varargin);
             
-            lginfo.AvailableNames = { 'InternalForce', ...
-                                    'RelativeDisplacement', ...
-                                    'RelativeVelocity' ...
-                                  };
-                              
-            lginfo.IndepVars = { 'Time', ...
-                               'Time', ...
-                               'Time' };
-                              
-            lginfo.Sizes = { [1,1], [1,1], [1,1] };
+            if ~isempty (options.InitialStep)
+                check.isNumericScalar (options.InitialStep, true, 'InitialStep', 1);
+                assert (options.InitialStep > 0, 'InitialStep must be greater than zero');
+            end
+            if ~isempty (options.MaxStep)
+                check.isNumericScalar (options.InitialStep, true, 'MaxStep', 1);
+                assert (options.InitialStep > 0, 'MaxStep must be greater than zero');
+            end
             
-            lginfo.Descriptions = {'', '', ''};
+            lginfo.AvailableNames = { 'PTOTime', ...
+                                      'Fpto', ...
+                                      'EMF', ...
+                                      'PhaseCurrents', ...
+                                      'FaddE', ...
+                                      'RelativeDisplacement', ...
+                                      'RelativeVelocity' ...
+                                    };
+                              
+            lginfo.IndepVars = { '', ...
+                                 1, ...
+                                 1, ...
+                                 1, ...
+                                 1, ...
+                                 'Time', ...
+                                 'Time', ...
+                                 };
+                              
+            lginfo.Sizes = { [1,1], [1,1], [1,3], [1,3], [1,1], [1,1], [1,1] };
+            
+            lginfo.Descriptions = { 'Time [s]', ...
+                                    'Force [N]', ...
+                                    'EMF [V]', ...
+                                    'Phase Currents [A]', ...
+                                    'Force [N]', ...
+                                    'Relative Displacement [m]', ...
+                                    'Relative Velocity [ms^{-1}]', ...
+                                    };
             
             lginfo.NAvailable = numel(lginfo.AvailableNames);
             
@@ -92,40 +132,175 @@ classdef linearPMMachinePTO < wsim.powerTakeOff
                                     'InitialDisplacementZero', options.InitialDisplacementZero, ...
                                     'ForceFcn', [] );
             
-        end
-        
-        function [FM, ptoforce, reldisp, relvel] = forceAndMoment (self)
-            
-            [FM, ptoforce, reldisp, relvel] = self.mbdynForceObj.force ();
-            
-            % need to add zero moments to forces
-            FM = [FM; zeros(size (FM))];
-            
-            self.lastInternalForce = ptoforce;
-            self.lastRelativeDisplacement = reldisp;
-            self.lastRelativeVelocity = relvel;
+            self.initialPhaseCurrents = options.InitialPhaseCurrents;
+            self.design = design;
+            self.simoptions = simoptions;
+            self.initialInternalTimeStep = options.InitialStep;
+            self.maxInternalTimeStep = options.MaxStep;
             
         end
         
-        function info = loggingSetup (self, logger)
+        function [FM, ptoforce, reldisp, relvel] = forceAndMoment (self, time)
+            
+            if time == self.simulationInfo.TStart
+                
+                FM = zeros (6,2);
+                
+            else
+                
+                [reldisp, relvel] = displacements (self.mbdynForceObj);
 
-            if nargin < 2
-                logger = [];
+                solve (self.machineODESolver, time, [reldisp; relvel]);
+
+                results = self.machineODESolver.getOutputs ();
+
+                F = self.mbdynForceObj.force (results.Fpto);
+
+                % need to add zero moments to forces
+                FM = [ F; 
+                       zeros(size (F)) ];
+                
             end
             
-
+        end
+        
+        function start (self, siminfo)
+            % initialise the pto simulation
             
-            self.initLogging (info, logger);
-                              
+            start@wsim.powerTakeOff (self, siminfo);
+            
+            [reldisp, relvel] = self.mbdynForceObj.displacements ();
+            
+            innerodeevfcn = @(t, y, mc, flag) nestedodeforcefcn_linear (t, y, mc, flag, self.design, self.simoptions);
+
+            % initial displacement and velocity of the generator
+            interpdat = [reldisp; relvel];
+            
+            if isempty (self.initialInternalTimeStep)
+                self.initialInternalTimeStep = self.simulationInfo.TStep/3; 
+            end
+                
+            odeoptions = odeset ('InitialStep', self.initialInternalTimeStep);
+            
+            if ~isempty (self.maxInternalTimeStep)
+                odeoptions = odeset (odeoptions, 'MaxStep', self.maxInternalTimeStep);
+            end
+
+            self.machineODESolver = ode.odesolver ( ...
+                            innerodeevfcn, ...
+                            self.simulationInfo.TStart, ...
+                            self.initialPhaseCurrents, ...
+                            interpdat, ...
+                            odeoptions, ...
+                            'Solver', 'ode15s', ...
+                            'SaveSolutions', false ...
+                                ... 'SplitFcn', @(flag, results, sol, mc, evalfcn) nestedsysresults_linear (flag, results, sol, mc, evalfcn, design, simoptions) ...
+                                          );
+            
+        end
+        
+        function advanceStep (self, time, varargin)
+            % advance to the next simulation time step
+            %
+            % Syntax
+            %
+            % advanceStep (pto)
+            %
+            % Description
+            %
+            % wsim.linearPMMachinePTO.advanceStep is intended to be called
+            % when the simulation is ready to advance to the next time
+            % step. It is always called when the linearPMMachinePTO is used
+            % with the wsim.wecSim class to manage a simulation.
+            % wsim.linearPMMachinePTO.advanceStep log data to the
+            % wsim.logger object (internally it calls
+            % wsim.powerTakeOff.logData). It also recalculates the last
+            % values of the mahine simualtion at the current time step and
+            % advances the ODE simulation of the PM machine to the next
+            % time step, accepting the last values in to the simulation
+            % history.
+            %
+            % Input
+            %
+            %  pto - wsim.powerTakeOff object
+            %
+            %
+            % See Also: wsim.powerTakeOff.logData
+            %
+            
+            [reldisp, relvel] = displacements (self.mbdynForceObj);
+            
+            if time == self.simulationInfo.TStart
+                
+%                 self.machineODESolver.acceptState ([reldisp; relvel]);
+                
+                % get the full output from the last generator sim
+                self.internalVariables = self.machineODESolver.getOutputs ({}, false);
+                
+                self.internalVariables.PTOTime = time;
+                
+                self.internalVariables.PhaseCurrents = self.machineODESolver.sol.y';
+                
+            else
+                % get the full output from the last generator sim
+                self.internalVariables = self.machineODESolver.getOutputs ({}, true);
+                
+                % strip the first entry as after the first step it is a
+                % duplicate of the final step of the previous simulation block
+                fnames = fieldnames (self.internalVariables);
+                for ind = 1:numel (fnames)
+                    self.internalVariables.(fnames{ind})(1,:) = [];
+                end
+                
+                self.internalVariables.PTOTime = self.machineODESolver.sol.x(2:end)';
+            
+                self.internalVariables.PhaseCurrents = self.machineODESolver.sol.y(:,2:end)';
+                
+                self.machineODESolver.acceptState ([reldisp; relvel]);
+            
+            end
+            
+            self.internalVariables.RelativeDisplacement = reldisp;
+            
+            self.internalVariables.RelativeVelocity = relvel;
+            
+            
+            
+            self.logData ();
+            
         end
         
         function logData (self)
             % appends the internal variable data to the log
+            %
+            % Syntax
+            %
+            % logData (pto)
+            %
+            % Description
+            %
+            % logData appends the last recorded values of the internal
+            % variables to a logger object.
+            %
+            % Input
+            %
+            %  pto - wsim.linearPMMachinePTO object
+            %
+            % See Also: wsim.powerTakeOff.loggingSetup,
+            %           wsim.linearPMMachinePTO.advanceStep
+            %
             
             if self.loggerReady
-                self.logger.logVal (self.uniqueLoggingNames{1}, self.lastInternalForce);
-                self.logger.logVal (self.uniqueLoggingNames{2}, self.lastRelativeDisplacement);
-                self.logger.logVal (self.uniqueLoggingNames{3}, self.lastRelativeVelocity);
+                for ind = 1:numel(self.loggingInfo.LoggedVarInds)
+                    
+                    fieldname = self.loggingInfo.AvailableNames{self.loggingInfo.LoggedVarInds(ind)};
+                    
+                    for indii = 1:size ( self.internalVariables.(fieldname), 1)
+                        self.logger.logVal ( self.uniqueLoggingNames{self.loggingInfo.LoggedVarInds(ind)}, ...
+                                             self.internalVariables.(fieldname)(indii,:) ...
+                                           );
+                    end
+                end
             else
                 error ('You have called logData, but logging has not been set up, have you called loggingSetup yet?');
             end
