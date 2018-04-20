8929cd9d542a88745c1a0b82f8f38a048a1c5e3f
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-04-20 11:12:18 +0100

    split out call_resets_recurse function to won file, and add option to allow skipping of electrical results calcs

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/odesim/resfun_AM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/odesim/resfun_AM.m
@@ -65,32 +65,17 @@ function [results, design] = resfun_AM(T, Y, design, simoptions)
         results.RPhase = design.PhaseResistance;
     end
     
-    % Determine some interesting machine electrical outputs
-    design = odeelectricalresults(T, ...
-                                  Y(:,currentInds), ...
-                                  results.EMF, ...
-                                  results.RPhase, ...
-                                  design, ...
-                                  simoptions);
-
-end
-
-
-function call_resets_recurse (ODESim)
-% recursively call the reset fuction for the solution components and any
-% nested simulation components if present
-
-    if isfield (ODESim, 'NestedSim')
-        nested_sim_recurse (ODESim);
-    end
-    
-    if isfield (ODESim, 'SolutionComponents')
-        fnames = fieldnames (ODESim.SolutionComponents);
-        for ind = 1:numel(fnames)
-            if isfield (ODESim.SolutionComponents.(fnames{ind}), 'ResetFcn')
-                feval (ODESim.SolutionComponents.(fnames{ind}).ResetFcn);
-            end
-        end
+    if ~simoptions.SkipODEElectricalResults
+        % Determine some interesting machine electrical outputs
+        design = odeelectricalresults(T, ...
+                                      Y(:,currentInds), ...
+                                      results.EMF, ...
+                                      results.RPhase, ...
+                                      design, ...
+                                      simoptions);
     end
 
 end
+
+
+
