dcc64fd4df1554b438749dd34612fae45570f957
Richard Crozier <a@b.c>
2018-05-16 15:24:19 +0100

    Added code to test operation in Octave

---
+++ wave/matlab-octave/wec-sim/test/+test_wsim_RM3/test_wecsim_graphics_in_octave.m
@@ -0,0 +1,123 @@
+% +test_wsim_RM3/run.m.m
+
+clear waves simu hsys mbsys float_hbody spar_hbody hydro_mbnodes hydro_mbbodies hydro_mbelements lssett wsobj
+
+%% Hydro Simulation Data
+simu = wsim.simSettings (fullfile ( wecsim_rootdir(), 'test', '+test_wsim_RM3'));  % Create the Simulation Variable
+% simu.mode = 'normal';                 %Specify Simulation Mode ('normal','accelerator','rapid-accelerator')
+% simu.explorer='on';                   %Turn SimMechanics Explorer (on/off)
+simu.startTime = 0;                   %Simulation Start Time [s]
+simu.endTime=400;                       %Simulation End bdcloseTime [s]
+simu.solver = 'ode4';                   %simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
+simu.dt = 0.1; 							%Simulation time-step [s]
+simu.rampT = 100;                       %Wave Ramp Time Length [s]
+simu.multibodySolver = 'MBDyn';
+simu.b2b = true;
+
+%% Wave Information 
+
+% noWaveCIC, no waves with radiation CIC  
+% waves = waveClass('noWaveCIC');       %Create the Wave Variable and Specify Type      
+%%%%%%%%%%%%%%%%%%%
+
+% Regular Waves
+waves = wsim.waveSettings ('regularCIC');        %Create the Wave Variable and Specify Type                               
+waves.H = 2.5;                          %Wave Height [m]
+waves.T = 8;                            %Wave Period [s]
+%%%%%%%%%%%%%%%%%%
+
+% Regular Waves
+% waves = wsim.waveSettings ('regular');        %Create the Wave Variable and Specify Type                               
+% waves.H = 2.5;                          %Wave Height [m]
+% waves.T = 8;                            %Wave Period [s]
+% simu.ssCalc = 1;
+%%%%%%%%%%%%%%%%%%%
+
+% Irregular Waves using PM Spectrum with Convolution Integral Calculation
+% waves = wsim.waveSettings ('irregular');       %Create the Wave Variable and Specify Type
+% waves.H = 2.5;                        %Significant Wave Height [m]
+% waves.T = 8;                          %Peak Period [s]
+% waves.spectrumType = 'PM';
+%%%%%%%%%%%%%%%%%%%
+
+% % Irregular Waves using BS Spectrum with Convolution Integral Calculation
+% waves = wsim.waveSettings ('irregular');       %Create the Wave Variable and Specify Type
+% waves.H = 2.5;                        %Significant Wave Height [m]
+% waves.T = 8;                          %Peak Period [s]
+% waves.spectrumType = 'BS';
+% %%%%%%%%%%%%%%%%%%%
+
+% Irregular Waves using BS Spectrum with State Space Calculation
+% waves = wsim.waveSettings ('irregular');       %Create the Wave Variable and Specify Type
+% waves.H = 2.5;                        %Significant Wave Height [m]
+% waves.T = 8;                          %Peak Period [s]
+% waves.spectrumType = 'BS';
+% simu.ssCalc = 1;	
+%Control option to use state space model 
+%%%%%%%%%%%%%%%%%%%
+
+% Irregular Waves using User-Defined Spectrum
+% waves = wsim.waveSettings ('irregularImport');  %Create the Wave Variable and Specify Type
+% waves.spectrumDataFile = 'ndbcBuoyData.txt';  %Name of User-Defined Spectrum File [2,:] = [omega, Sf]
+%%%%%%%%%%%%%%%%%%%
+
+% User-Defined Time-Series
+% waves = wsim.waveSettings ('userDefined');     %Create the Wave Variable and Specify Type
+% waves.etaDataFile = 'umpqua46229_6_2008.mat'; % Name of User-Defined Time-Series File [:,2] = [time, wave_elev]
+%%%%%%%%%%%%%%%%%%%
+
+%% Hydrodynamic body system
+
+% hydro data fiels assumed to be in assumed to be in folder 
+% <case_directory>/hydroData
+
+% Float
+float_hbody = wsim.hydroBody('float.mat', 'CaseDirectory', simu.caseDir);      
+    %Create the wsim.hydroBody(1) Variable, Set Location of Hydrodynamic Data File 
+    %and Body Number Within this File.   
+float_hbody.mass = 'equilibrium';                   
+    %Body Mass. The 'equilibrium' Option Sets it to the Displaced Water 
+    %Weight.
+float_hbody.momOfInertia = [20907301, 21306090.66, 37085481.11];  %Moment of Inertia [kg*m^2]     
+float_hbody.geometryFile = 'float.stl'; % Geomtry File Name (assumed to be in <case_directory>/geometry)
+
+% Spar/Plate
+spar_hbody = wsim.hydroBody('spar.mat', 'CaseDirectory', simu.caseDir); 
+spar_hbody.mass = 'equilibrium';                   
+spar_hbody.momOfInertia = [94419614.57, 94407091.24, 28542224.82];
+spar_hbody.geometryFile = 'plate.stl'; % Geomtry File Name (assumed to be in <case_directory>/geometry)
+
+tmp = float_hbody;
+tmp(2) = spar_hbody;
+% make a hydrosys object for simulation
+hsys = wsim.hydroSystem (waves, simu, tmp);
+
+% set up transient simulation
+hsys.initialiseHydrobodies ();
+hsys.odeSimSetup ();
+
+% generate the nodes and elements for simulation of the hydrodynamic system
+% in MBDyn. One node and one body element are created for each hydrodynamic
+% body interacting with the waves.
+[hydro_mbnodes, hydro_mbbodies, hydro_mbelements] = hsys.makeMBDynComponents ();
+
+%% Multibody dynamics system specification (mbdyn)
+
+problem_options.ResidualTol = 1e-5;
+problem_options.MaxIterations = 200;
+problem_options.Output = {}; % 'iterations', 'solution', 'jacobian matrix', 'matrix condition number', 'solver condition number'
+% problem_options.NonLinearSolver = mbdyn.pre.newtonRaphsonSolver ();
+problem_options.NonLinearSolver = [];
+% problem_options.LinearSolver = mbdyn.pre.linearSolver ('naive');
+problem_options.LinearSolver = [];
+
+[mbsys, initptodpos] = test_wsim_RM3.make_multibody_system (waves, simu, hydro_mbnodes, hydro_mbbodies, hydro_mbelements, problem_options);
+                     
+% draw it
+% mbsys.draw ('Mode', 'wireghost', 'Light', false);
+
+mbsys.draw ( 'Mode', 'solid', ...
+             'Light', true, ...
+             'AxLims', [-30, 30; -30, 30; -35, 35], ...
+             'Joints', false, ...
+             'StructuralNodes', false)
