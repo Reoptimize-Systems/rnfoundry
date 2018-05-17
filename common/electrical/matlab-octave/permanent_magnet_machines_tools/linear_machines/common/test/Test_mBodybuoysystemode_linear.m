72ebab46faae8d59ab1f22692c314767272157fc
Richard Crozier <a@b.c>
2018-05-17 14:45:48 +0100

    committing some old style multibosdy stuff without checking too much as it has been superceded by mbdyn

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/common/test/Test_mBodybuoysystemode_linear.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/linear_machines/common/test/Test_mBodybuoysystemode_linear.m
@@ -2,33 +2,11 @@
 
 clear design simoptions
 
-design.Phases = 3;         % Number of phases in machine
-design.Rm = 0.1;
-design.g = 5/1000;
-design.Ri = design.Rm + design.g;
-design.WmVWp = 0.75;
-design.WpVRm = 0.5;
-design.RiVRm = design.Ri / design.Rm;
-design.RoVRm = 1.2;
-design.RaVRo = 1.025;
-design.RsoVRm = 0.1;
-design.RsiVRso = 0;
-design.WcVWp = 1/3;
-design.CoilFillFactor = 0.65;
-%design.Dc = 1/1000;  % 1 mm diameter wire 
-design.CoilTurns = 50;
-design.mode = 2; 
-design.LgVLc = 0;
-design.Poles = [10 30];
-design.CoilsPerBranch = 10;
-design.Branches = 1;
-% design.FieldDirection = 1;
-% design.PowerPoles = poles(1);
+design = test_design_ACTM ();
 
 design = ratios2dimensions_ACTM(design);
 
-
-%% Set up Common Parameters
+% Set up Common Parameters
 
 design.RlVRp = 10;
 
@@ -36,18 +14,22 @@ simoptions.Lmode = 0;
 simoptions.NoOfMachines = 1;
 simoptions.maxAllowedxT = 0.5;
 
-%% Test with buoy in sinusoidal sea
+% Test with buoy in sinusoidal sea
 
 % Set up the buoy and sea data files, these are for the 2m buoy
-simoptions.buoy = 'cyl_2dia_1dr';
+simoptions.BuoySim.buoy = 'cyl_2dia_1dr';
 
-simoptions.ODESim.TimeSpan = [0, 0.5];
+simoptions.ODESim.TimeSpan = [0, 15];
 % params.amp = 1;
 % params.sigma = 2 * pi * 0.35;
 % params.phase = pi/2;
+simoptions.ODESim.MaxStep = 0.2;
+simoptions.ODESim.InitialStep = 0.1;
 
 % use a default sea
-simoptions.BuoySim.SeaParameters = seasetup ('Sigmas', 2 * pi * 0.35);
+% simoptions.BuoySim.SeaParameters = seasetup ('Sigmas', 2 * pi * 0.35);
+simoptions.BuoySim.SeaParameters = seasetup ('Sigmas', 2 * pi * (1/5));
+simoptions.BuoySim.NRadiationCoefs = 10;
 
 % set the end stop position (the maximum allowed translator displacement)
 simoptions.maxAllowedxT = inf;
@@ -55,14 +37,192 @@ simoptions.maxAllowedxT = inf;
 % simoptions.BuoySim.tether_length = 4;
 
 simoptions.ODESim.PreProcFcn = 'simfun_ACTM';
-simoptions.ODESim.EvalFcn = 'mBodybuoysystemode_linear'; 
+simoptions.ODESim.EvalFcn = 'mBodybuoysystemode_linear_nested'; 
 simoptions.ODESim.PostPreProcFcn = 'mBodybuoysystemfinfun_ACTM';
-simoptions.ODESim.PostSimFcn = 'systemresfun_linear'; 
+simoptions.ODESim.PostAssemblyFcn = 'mBodybuoysyspostassembly_linear';
+simoptions.ODESim.PostSimFcn = 'nestedsysresfun_AM'; 
 % simoptions.events = 'systemevents_linear'; 
 
-[T, Y, results, outdesign, outsimoptions] = simulatemachine_linear(design, simoptions); 
+simoptions.ODESim.Solver = 'ode45';
 
+%%
+tic;
+[T, Y, results, outdesign, outsimoptions] = simulatemachine_linear(design, simoptions); 
+toc;
+
+%%
 plotresultsbuoysys_linear(T, Y, results, design, outsimoptions, 1)
+
+%%
+mb = outdesign.MultiBodySystem;
+
+axlims = [ min(results.xBs) - outsimoptions.BuoySim.BuoyParameters.a, ...
+           max(results.xBs) + outsimoptions.BuoySim.BuoyParameters.a, ...
+           -2*outsimoptions.BuoySim.BuoyParameters.a, ...
+           2*outsimoptions.BuoySim.BuoyParameters.a, ...
+           -10, ...
+           max(results.xBh) + outsimoptions.ODESim.SolutionComponents.MultiBodySystem.Buoy.sz ];
+           
+
+mb.animate ('t', T, 'x', Y(:,21:end)', 'alpha', 0.1, 'PlotFcn', ...
+                @(tind, t, x, mbod) plotvectors ( tind, t, x, mbod, ...
+                                                  results.vRvec, ...
+                                                  results.vRgenVec, ...
+                                                  results.FptoVec, ...
+                                                  results.FBDs, ...
+                                                  results.FBDh, ...
+                                                  axlims ));
+
+%%
+
+axlims = [ min(results.xBs) - outsimoptions.BuoySim.BuoyParameters.a, ...
+           max(results.xBs) + outsimoptions.BuoySim.BuoyParameters.a, ...
+           -2*outsimoptions.BuoySim.BuoyParameters.a, ...
+           2*outsimoptions.BuoySim.BuoyParameters.a, ...
+           -10, ...
+           max(results.xBh) + outsimoptions.ODESim.SolutionComponents.MultiBodySystem.Buoy.sz ];
+           
+
+mb.animate ('t', T, ...
+            'x', Y(:,21:end)', ...
+            'alpha', 0.1, ...
+            'filename', 'test_buoy_vid', ...
+            'speed', 1, ...
+            'PlotFcn', @(tind, t, x, mbod) plotvectors ( tind, t, x, mbod, ...
+                                                         results.vRvec, ...
+                                                         results.vRgenVec, ...
+                                                         results.FptoVec, ...
+                                                         results.FBDs, ...
+                                                         results.FBDh, ...
+                                                         axlims ) ...
+           );
+                                              
+                                              
+%%
+
+figure;
+maxT = 8; 
+[ax, h1, h2] = plotyy (T(T<=maxT), [results.FBs(T<=maxT), results.FBh(T<=maxT)], T(T<=maxT), [results.vBs(T<=maxT), results.vBh(T<=maxT)]);
+hFBs = h1(1); 
+hFBh = h1(2); 
+hvBs = h2(1); 
+hvBh = h2(2);
+legend ('FBs', 'FBh', 'vBs', 'vBh');
+
+hvBh.Color = hFBh.Color;
+hvBs.Color = hFBh.Color;
+hvBs.LineStyle = '--';
+
+hFBh.Color = hFBs.Color;
+hFBs.LineStyle = '--';
+
+%%
+
+figure;
+maxT = 8; 
+[ax, h1, h2] = plotyy ( T(T<=maxT), ...
+                        [ ...
+                          results.buoyancy_force(T<=maxT), ...
+                          results.excitation_force_heave(T<=maxT), ...
+                          results.radiation_force_heave(T<=maxT), ...
+                          results.FBDh(T<=maxT) ...
+                          results.FBh(T<=maxT) ...
+                          results.FptoVec(T<=maxT,3) ...
+                          results.excitation_force_surge(T<=maxT), ...
+                          results.radiation_force_surge(T<=maxT) ...
+                          results.FBDs(T<=maxT) ...
+                          results.FBs(T<=maxT) ...
+                          results.FptoVec(T<=maxT,1) ...
+                        ], ...
+                        T(T<=maxT), ...
+                        [ ...
+                          results.xBs(T<=maxT), ...
+                          results.xBh(T<=maxT) ...
+                          results.vBs(T<=maxT), ...
+                          results.vBh(T<=maxT) ...
+                        ] ...
+                      );
+                  
+h_buoyancy_force = h1(1); 
+h_excitation_force_heave = h1(2); 
+h_radiation_force_heave = h1(3); 
+h_FBDh = h1(4);
+h_FBh = h1(5);
+h_Fptos = h1(6);
+h_excitation_force_surge = h1(7);
+h_radiation_force_surge = h1(8);
+h_FBDs = h1(9);
+h_FBs = h1(10);
+h_Fptoh = h1(11);
+
+
+h_buoyancy_force.Color = 'k';
+
+h_excitation_force_surge.Color = h_excitation_force_heave.Color;
+h_excitation_force_surge.LineStyle = '--';
+
+h_radiation_force_surge.Color = h_radiation_force_heave.Color;
+h_radiation_force_surge.LineStyle = '--';
+
+h_FBDs.Color = h_FBDh.Color;
+h_FBDs.LineStyle = '--';
+
+h_FBs.Color = h_FBh.Color;
+h_FBs.LineStyle = '--';
+
+h_Fptos.Color = h_Fptoh.Color;
+h_Fptos.LineStyle = '--';
+
+legend ('buoyancy force', ...
+        'excitation force heave', ...
+        'radiation force heave', ...
+        'FBDh', ...
+        'FBh', ...
+        'Fptoh', ...
+        'excitation force surge', ...
+        'radiation force surge', ...
+        'FBDs', ...
+        'FBh', ...
+        'Fptos', ...
+        'xBs', ...
+        'xBh', ...
+        'vBs', ...
+        'vBh' );
+    
+xlabel ('Time, s')
+ax1 = ax(1);
+ax1.YLabel('Forces, N');
+ax2 = ax(2);
+ax2.YLabel('Velocity/Displacement');
+
+%%
+
+figure;
+maxT = 8; 
+[ax, h1, h2] = plotyy ( T(T<=maxT), ...
+                        [ ...
+                          results.buoyancy_force(T<=maxT), ...
+                          ... results.excitation_force_heave(T<=maxT), ...
+                          ... results.radiation_force_heave(T<=maxT), ...
+                          ... results.excitation_force_surge(T<=maxT), ...
+                          ... results.radiation_force_surge(T<=maxT) ...
+                        ], ...
+                        T(T<=maxT), ...
+                        [ ...
+                          ... results.vBs(T<=maxT), ...
+                          results.xBh(T<=maxT) ...
+                        ] ...
+                      );
+
+legend ('buoyancy force', 'xBh');
+
+% h_buoyancy_force = h1(1); 
+% h_excitation_force_heave = h1(2); 
+% h_radiation_force_heave = h1(3); 
+% h_excitation_force_surge = h1(4);
+% h_radiation_force_surge = h1(5);
+% 
+% legend ('buoyancy force', 'excitation force heave', 'radiation force heave', 'excitation force surge', 'radiation force surge', 'vBs', 'vBh');
 
 
 %% SImulate with buoy in random sea
@@ -97,7 +257,27 @@ simoptions.BuoySim.SeaParameters = seasetup ('PMPeakFreq', 1/9);
 skip = 5;
 plotresultsbuoysys_linear(T, Y, results, outdesign, outsimoptions, skip)
 
+%% plot
+ 
+fnames = dir ([simoptions.ODESim.Nested.GeneratorSolver.solutionFileTemplate, '*.mat']);
 
- 
- 
+S = load (fnames(1).name);
+innerT = S.T';
+innerY = S.Y;
+
+for ind = 2:numel(fnames)
+   
+    S = load (fnames(ind).name);
+    
+    innerT = [innerT; S.T(2:end)'];
+    
+    innerY = [innerY; S.Y(2:end,:)];
+    
+end
+              
+figure; plotyy (T, Y(:,[simoptions.ODESim.SolutionComponents.MultiBodySystem.SolutionIndices(simoptions.ODESim.SolutionComponents.MultiBodySystem.BuoyPositionSolutionInd), ...
+                        simoptions.ODESim.SolutionComponents.MultiBodySystem.SolutionIndices(simoptions.ODESim.SolutionComponents.MultiBodySystem.BuoyVelocitySolutionInd) ...
+                       ] ...
+                     ), ...
+                innerT, innerY);
  
