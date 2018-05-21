b2aeb5c7ab91ce8aa8e5ad92b2d296352871c3ff
Richard Crozier <a@b.c>
2018-05-21 12:51:28 +0100

    txtfile2cell.m: added an input check

--- common/matlab-octave/misc/txtfile2cell.m
+++ common/matlab-octave/misc/txtfile2cell.m
@@ -5,8 +5,30 @@ function [strs, nlines] = txtfile2cell(fname)
 %
 % [strs, nlines] = txtfile2cell(fname)
 %
-% 
+% Description
+%
+% txtfile2cell ead an entire text file into a cell array of strings. The
+% text file is split on the newline character, so each line in the file is
+% in a cell.
+%
+% Input
+%
+%  fname - character vector containing th name of the file to read in
+%
+% Output
+%
+%  strs - cell array of character vectors containing each line of text from
+%   the file
+%
+%  nlines - the number of lines read in 
+%
+%
+%
+% See Also: cellstr2txtfile, cellstr2str
+%
 
+    assert (ischar (fname), 'fname must be a character vector');
+    
     % try to open the file
     [fid, message] = fopen(fname, 'r');
     
