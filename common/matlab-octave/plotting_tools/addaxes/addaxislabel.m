d892d41fe35875c798bab7a5b339166d0daa09d4
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-05-25 23:47:38 +0100

    addaxislabel.m: make it possible to accept number or axes handle as input

--- common/matlab-octave/plotting_tools/addaxes/addaxislabel.m
+++ common/matlab-octave/plotting_tools/addaxes/addaxislabel.m
@@ -1,4 +1,4 @@
-function varargout = addaxislabel(axnum, varargin)
+function varargout = addaxislabel(input, varargin)
 %ADDAXISLABEL adds axis labels to axes made with ADDAXIS.m
 %
 % Syntax
@@ -7,34 +7,51 @@ function varargout = addaxislabel(axnum, varargin)
 %
 %  See also
 %  ADDAXISPLOT, ADDAXIS, SPLOT 
+
+    assert (isscalar (input), 'input must be a scalar integer, or single axes handle');
   
-    % get current axes handle
-    cah = gca;
+    ret = what_is_input (input);
     
-    % get the handles to all the axes in the figure
-    axh = getaddaxisdata(cah,'axisdata');
+    switch ret
+        
+        case 'label'
+            
+            % get current axes handle
+            current_ax = gca;
+            axnum = input;
+            
+            % get the handles to all the axes in the figure
+            axh = getaddaxisdata (current_ax, 'axisdata');
 
-    %  get axis handles to all the main axes and subaxes in the order they
-    %  were added
-    axhand = cah;
-    postot(1,:) = get(cah,'position');
-    for I = 1:length(axh)
-        axhand(I+1) = axh{I}(1);
-        postot(I+1,:) = get(axhand(I+1),'position');
+            %  get axis handles to all the main axes and subaxes in the order they
+            %  were added
+            axhand = current_ax;
+
+            postot(1,:) = get (current_ax, 'position');
+
+            for I = 1:length(axh)
+                axhand(I+1) = axh{I}(1);
+                postot(I+1,:) = get (axhand(I+1), 'position');
+            end
+            
+            change_ax = axhand(axnum);
+
+        case 'axes_handle'
+            
+            change_ax = input;
+            
+        otherwise
+            
+            error ('Invalid input, input must be the number of the axes, or axes handle for the axes for which the ylabel is to be set.');
+            
     end
-
-    %  set current axis to the axis to be labeled
-    axes(axhand(axnum));
     
     % Set the label of the y axis and get the handle to the label object so
     % we can change it's colour
-    htxt = ylabel(varargin{:});
+    htxt = ylabel (change_ax, varargin{:});
     
     % Set the label text colour to the same as the axis colour
-    set(htxt,'color',get(axhand(axnum),'ycolor'));
-
-    %  set current axis back to the main axis
-    axes(cah);
+    set ( htxt, 'color', get (change_ax, 'ycolor') );
 
     if nargout == 1
         % return the handle to the label object if requested
@@ -42,3 +59,16 @@ function varargout = addaxislabel(axnum, varargin)
     end
 
 end
+
+function ret = what_is_input (thing)
+
+    if check.isAxes (thing, false)
+        ret = 'axes_handle';
+    elseif check.isint2eps (thing)
+        ret = 'label';
+    else
+        ret = '';
+    end
+
+end
+
