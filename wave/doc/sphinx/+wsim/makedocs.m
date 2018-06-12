81d51e6f5c3d24b00e0b0bdb2a24f49622339144
Richard Crozier <a@b.c>
2018-06-12 14:43:47 +0100

    Added content and images to wave docs

---
+++ wave/doc/sphinx/+wsim/makedocs.m
@@ -0,0 +1,84 @@
+% makedocs.m
+%
+% Script to generate files for mbdyn docs and build the docs
+
+% files required for embedding in docs
+thisfiledir = getmfilepath ('wsim.makedocs');
+
+imagedir = fullfile (thisfiledir, '..', 'images');
+
+
+%% Run examples and save figures
+
+% save_example_figs ('example_double_pendulum', imagedir, 1:6);
+% save_example_figs ('example_basic_cosimulation', imagedir, 1:3);
+
+
+%% Make API reference
+apidir = fullfile (thisfiledir, '..', 'api_reference');
+
+mkdir (apidir);
+
+wsim_apidir = fullfile (apidir, 'pre');
+mkdir (wsim_apidir);
+
+% generated docs from class file help
+wsim_class_list = dir (fullfile (getmfilepath ('mbdyn.pre.element'), '*.m'));
+
+% exclude_list = { 'baseSystem.m', ...
+%                  'subsystem.m' };
+
+
+for ind = 1:numel(wsim_class_list)
+    
+    if ~any (strcmp (wsim_class_list(ind).name, exclude_list )) 
+    
+        rst = help2rst (['wsim.', wsim_class_list(ind).name(1:end-2)], ...
+                        'SectionChars', '=-^"+');
+
+        outputfile = fullfile (wsim_apidir, [wsim_class_list(ind).name(1:end-2), '.rst']);
+
+        cellstr2txtfile (outputfile, rst);
+    
+    end
+    
+end
+        
+        
+%% build the docs
+cd (fullfile (apidir, '..'));
+
+[status, out] = cleansystem ('make html');
+
+%% functions
+% 
+% function save_example_figs (exname, imdir, fignums_to_save)
+% 
+%     close all
+%     run (exname);
+%     
+%     handles = findall(0,'type','figure');
+%     
+%     fname = fullfile (imdir, [exname, '_%d.png']);
+%     
+%     for ind = 1:numel (handles)
+%         
+%         fignum = get (handles(ind), 'Number');
+%         
+% %         tightfig (handles(ind));
+%         
+%         switch fignum
+%             
+%             case num2cell ( fignums_to_save )
+%                 
+%                 saveas (handles(ind), sprintf (fname, fignum));
+%                 
+%             otherwise
+%                 
+%         end
+% 
+%     end
+%     
+%     close all
+%     
+% end
