7c0efe6c9054d465bd0fe88e6db66134cead29cd
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-07-03 16:10:01 +0100

    added +test_wsim_RM3/generate_hydrodata_scaled.m

---
+++ wave/matlab-octave/wec-sim/test/+test_wsim_RM3/generate_hydrodata_scaled.m
@@ -0,0 +1,142 @@
+function hydro = generate_hydrodata_scaled (varargin)
+% Run example: hydro = test_wsim_RM3.generate_hydrodata_scaled ('ScaleFac', 2)
+
+    options.PlotBEM = true;
+    options.WriteH5 = true;
+    options.SkipIfExisting = false;
+    options.ScaleFac = 1;
+    options.PlotMesh = false;
+    options.NemohOutputDir = '';
+    
+    options = parse_pv_pairs (options, varargin);
+    
+    dir = getmfilepath ('test_wsim_RM3.generate_hydrodata_scaled');
+    
+    if isempty (options.NemohOutputDir)
+        inputdir = fullfile (dir, 'RM3_NEMOH_output');
+    else
+        inputdir = options.NemohOutputDir;
+    end
+
+
+    %% create the float
+
+    % create a body object which will be the cylinder
+    float = nemoh.body (inputdir, 'Name', 'float');
+
+    % define the body shape using a 2D profile rotated around the z axis
+    float.loadNemohMesherInputFile ( fullfile (dir, 'geometry', 'float.nmi'), ...
+                                     fullfile (dir, 'geometry', 'float_Mesh.cal') ...
+                                     ... 'CentreOfGravity', [0,0,-0.72] 
+                                    );
+                                
+	float.scaleMesh (options.ScaleFac);
+%     float.scaleMesh (options.ScaleFac, 'CentreOfGravity', [0,0,-10]);
+
+    %% draw body mesh 
+
+%     float.drawMesh ();
+%     axis equal;
+
+    %% create the Nemoh spar
+
+    spar = nemoh.body (inputdir, 'Name', 'spar');
+
+    % define the body shape using a 2D profile rotated around the z axis
+    spar.loadNemohMesherInputFile ( fullfile (dir, 'geometry', 'spar.nmi'), ...
+                                    fullfile (dir, 'geometry', 'spar_Mesh.cal') ...
+                                    ... 'Draft', 28.8999996, ... % got from reading example mesh file in WEC-Sim 
+                                    ... 'CentreOfGravity', [ 0, 0, -21.29 + 28.8999996 - 8.71 ] 
+                                    );
+                                
+    spar.scaleMesh (options.ScaleFac);
+
+
+    %% draw the course body mesh (will be refined later)
+
+%     spar.drawMesh ();
+%     axis equal;
+
+    %% Create the nemoh simulation
+
+    % here we insert the cylinder body at creation of the simulation, but could
+    % also have done this later by calling addBody
+    sim = nemoh.simulation ( inputdir, ...
+                             'Bodies', [ float, spar ] );
+
+
+    sim.drawMesh ();
+    axis equal
+
+    %% write mesh files
+
+    % write mesh file for all bodies 
+    sim.writeMesherInputFiles ();
+
+    %% process mesh files
+
+    % process mesh files for all bodies to make ready for Nemoh
+    sim.processMeshes ();
+
+    %% Draw all meshes in one figure
+
+    sim.drawMesh ();
+    axis equal;
+
+    %% Generate the Nemoh input file
+
+    % generate the file 
+    omega = [0.02, 5.20];
+    sim.writeNemoh ( 'NWaveFreqs', 2, ... %260
+                     'MinMaxWaveFreqs', omega, ...
+                     'DoIRFCalculation', false, ...
+                     'NDirectionAngles', 1 );
+
+    % The above code demonstrates the use of optional arguments to writeNemoh
+    % to set the desired wave frequencies. If the wave frencies were not
+    % specified a default value would be used. These are not the only possible
+    % options for writeNemoh. The following optional arguments are available,
+    % and the defaults used if they are not supplied are also shown:
+    %
+    % DoIRFCalculation = true;
+    % IRFTimeStep = 0.1;
+    % IRFDuration = 10;
+    % NWaveFreqs = 1;
+    % MinMaxWaveFreqs = [0.8, 0.8];
+    % NDirectionAngles = 0;
+    % MinMaxDirectionAngles = [0, 0];
+    % FreeSurfacePoints = [0, 50];
+    % DomainSize = [400, 400];
+    % WaterDepth = 0;
+    % WaveMeasurementPoint = [0, 0];
+    %
+    % For more information on these arguments, see the help for the writeNemoh
+    % method.
+
+    %% Run Nemoh on the input files
+
+    sim.run ()
+    
+    %% Create hydro structure for 
+    
+    hydro = struct();
+
+    hydro = wsim.bemio.processnemoh (inputdir);
+    
+    % hydro = Read_WAMIT(hydro,'..\..\WAMIT\RM3\rm3.out',[]);
+    % hydro = Combine_BEM(hydro); % Compare WAMIT
+    hydro = Radiation_IRF (hydro, 60, [], [], [], 1.9);
+    
+    hydro = Radiation_IRF_SS (hydro, [], []);
+
+    hydro = Excitation_IRF (hydro,157, [], [], [], 1.9);
+    
+    if options.WriteH5
+        Write_H5 (hydro, fullfile (dir, 'hydroData'))
+    end
+    
+    if options.PlotBEM
+        Plot_BEMIO (hydro)
+    end
+
+end
