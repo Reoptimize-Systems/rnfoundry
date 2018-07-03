f63d8fcf8278e89fa0f640fbcb316f86b10050ee
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-07-03 16:14:01 +0100

    release.sh: make no zip file creation the default

--- release.sh
+++ release.sh
@@ -18,7 +18,8 @@ working_copy_dir=$(pwd)
 run_tests=false
 copy_win_libs=true
 skip_mex=false
-make_zip=true
+make_zip=false
+make_docs=true
 
 usage="$(basename "$0") [-h] [-v <version>] [-t] [-w] [-m] [-z]  -- creates Renewnet Foundry release
 
@@ -28,7 +29,7 @@ where:
     -t  run tests
     -w  copy windows libraries etc
     -m  skip building mex files (requires matlab)
-    -z  don't create zip file"
+    -z  create zip file"
 
 while getopts "h?v:twmz" opt; do
     case "$opt" in
@@ -48,93 +49,104 @@ while getopts "h?v:twmz" opt; do
     m)  skip_mex=true
         echo "skip_mex: $skip_mex"
         ;;
-    z)  make_zip=false
+    z)  make_zip=true
+        echo "make_zip: $make_zip"
+        ;;
+    d)  make_docs=false
         echo "make_zip: $make_zip"
         ;;
     esac
 done
 
-#echo $working_copy_dir
+#echo ${working_copy_dir}
 
-echo "Releasing with version string: $version"
+echo "Releasing with version string: ${version}"
 
 release_name="RenewNet_${version}"
 
 # create release directory
 release_dir="${working_copy_dir}/${release_name}"
-mkdir $release_dir
+mkdir ${release_dir}
 
-echo "Creating release $release_name in directory $release_dir"
+echo "Creating release ${release_name} in directory ${release_dir}"
 
 # export from the working directory to the release directory
-hg archive $release_dir
+hg archive ${release_dir}
 # remove file created by mercurial
-rm $release_dir/.hg_archival.txt
+rm ${release_dir}/.hg_archival.txt
 #remove hgignore file
-rm $release_dir/.hgignore
+rm ${release_dir}/.hgignore
 # remove the release scripts
-rm $release_dir/release.sh
-rm $release_dir/test_release.sh
+rm ${release_dir}/release.sh
+rm ${release_dir}/test_release.sh
 # remove test scripts
-rm $release_dir/test_hg.sh
+rm ${release_dir}/test_hg.sh
 
 # create version.txt
-echo ${version} > $release_dir/version.txt
+echo ${version} > ${release_dir}/version.txt
 
 if [ "$copy_win_libs" = true ]; then
 
+    mxedir=/opt/mxe
+
     # we need to copy a bunch of files cross-compiled using MXE to the
     # release so it can be built on windows machines
-    mkdir $release_dir/x86_64-w64-mingw32
-    mkdir $release_dir/x86_64-w64-mingw32/include
-    mkdir $release_dir/x86_64-w64-mingw32/lib
+    mkdir ${release_dir}/x86_64-w64-mingw32
+    mkdir ${release_dir}/x86_64-w64-mingw32/include
+    mkdir ${release_dir}/x86_64-w64-mingw32/lib
 
     # gsl
     # octave requires standard unix lib names (.a)
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libgsl.a  $release_dir/x86_64-w64-mingw32/lib/
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libgslcblas.a $release_dir/x86_64-w64-mingw32/lib/
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libgsl.a  ${release_dir}/x86_64-w64-mingw32/lib/
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libgslcblas.a ${release_dir}/x86_64-w64-mingw32/lib/
     # matlab needs libraries to have a different name (.lib)
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libgsl.a  $release_dir/x86_64-w64-mingw32/lib/gsl.lib
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libgslcblas.a $release_dir/x86_64-w64-mingw32/lib/gslcblas.lib
-    cp -r /opt/mxe/usr/x86_64-w64-mingw32.static/include/gsl/ $release_dir/x86_64-w64-mingw32/include/
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libgsl.a  ${release_dir}/x86_64-w64-mingw32/lib/gsl.lib
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libgslcblas.a ${release_dir}/x86_64-w64-mingw32/lib/gslcblas.lib
+    cp -r ${mxedir}/usr/x86_64-w64-mingw32.static/include/gsl/ ${release_dir}/x86_64-w64-mingw32/include/
 
     # f2c
     # octave requires standard unix lib names (.a)
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libf2c.a  $release_dir/x86_64-w64-mingw32/lib/
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libf2c.a  ${release_dir}/x86_64-w64-mingw32/lib/
     # matlab needs libraries to have a different name (.lib)
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libf2c.a  $release_dir/x86_64-w64-mingw32/lib/f2c.lib
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libf2c.a  ${release_dir}/x86_64-w64-mingw32/lib/f2c.lib
 
     # mbdyn
-    cp -r ~/build/mbdyn/windows/* $release_dir/x86_64-w64-mingw32/
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libws2_32.a $release_dir/x86_64-w64-mingw32/lib/
+    cp -r ~/build/mbdyn/x86_64-w64-mingw32_static/* ${release_dir}/x86_64-w64-mingw32/
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libws2_32.a ${release_dir}/x86_64-w64-mingw32/lib/
     # matlab needs libraries to have a different name (.lib)
-    cp ~/build/mbdyn/windows/lib/libmbc.a  $release_dir/x86_64-w64-mingw32/lib/mbc.lib
-    cp /opt/mxe/usr/x86_64-w64-mingw32.static/lib/libws2_32.a $release_dir/x86_64-w64-mingw32/lib/ws2_32.lib
+    cp ~/build/mbdyn/x86_64-w64-mingw32_shared/lib/libmbc.a  ${release_dir}/x86_64-w64-mingw32/lib/mbc.lib
+    cp ${mxedir}/usr/x86_64-w64-mingw32.static/lib/libws2_32.a ${release_dir}/x86_64-w64-mingw32/lib/ws2_32.lib
 
 else
-  echo "Not copying gsl gslcblas and f2c libraries"
+  echo "Not copying gsl gslcblas and f2c libraries, or mbdyn program"
 fi
 
 if [ "$skip_mex" = false ]; then
   if ! [ -x "$(command -v matlab)" ]; then
     echo 'matlab is not installed, not building mex files using Matlab.' >&2
   else
-    # buld the mex files using matlab
-    matlab -nodesktop -r "restoredefaultpath; cd('$release_dir'); rnfoundry_setup('Runtests', false, 'PreventXFemmCheck', true); quit"
-    #matlab -nodesktop -r "restoredefaultpath; cd('$release_dir'); rnfoundry_setup('Runtests', false, 'PreventXFemmCheck', true, 'CrossBuildW64', true); quit"
+    # buld the mex files using (oldish) version of matlab
+    /usr/local/MATLAB/R2016b/bin/matlab -nodesktop -r "restoredefaultpath; cd('${release_dir}'); rnfoundry_setup('Runtests', false, 'PreventXFemmCheck', true); quit"
+    #matlab -nodesktop -r "restoredefaultpath; cd('${release_dir}'); rnfoundry_setup('Runtests', false, 'PreventXFemmCheck', true, 'CrossBuildW64', true); quit"
   fi
 fi
 
 if [ "$make_zip" = true ]; then
   # zip up the result
-  cd $working_copy_dir
-  zip -qr ${release_name}.zip $release_name/
+  cd ${working_copy_dir}
+  zip -qr ${release_name}.zip ${release_name}/
 fi
+
+#if [ "$make_docs" = true ]; then
+#  # copy the mbdyn sphinx docs directory to /tmp
+#  cp ${working_copy_dir}
+#  zip -qr ${release_name}.zip $release_name/
+#fi
 
 if [ "$run_tests" = true ]; then
   # test
-  cd $working_copy_dir
-  ./test_release.sh $release_dir
+  cd ${working_copy_dir}
+  ./test_release.sh ${release_dir}
 else
   echo "Skipping Tests"
 fi
