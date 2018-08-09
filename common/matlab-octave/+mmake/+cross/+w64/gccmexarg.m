5a9a9f95f1f48c686ca4c844ac6c89030a861550
Richard Crozier <a@b.c>
2018-08-09 16:52:02 +0100

    +mmake/+cross/+w64/gccmexarg.m: use static build by default

--- common/matlab-octave/+mmake/+cross/+w64/gccmexarg.m
+++ common/matlab-octave/+mmake/+cross/+w64/gccmexarg.m
@@ -1,7 +1,7 @@
 function gcc_arg = gccmexarg (varargin)
 % get the full path to cross-compiler gcc
 
-    options.CrossPrefix = 'x86_64-w64-mingw32.shared';
+    options.CrossPrefix = 'x86_64-w64-mingw32.static';
     
     options = parse_pv_pairs (options, varargin);
 
