47a17afe5485b273944768b020a556e1bbf31b4c
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-10-12 16:53:04 +0100

    circuitprops_AM.m: added help and made it possible to set PID coeffs directly

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/circuitprops_AM.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/circuitprops_AM.m
@@ -1,6 +1,5 @@
 function [design, simoptions] = circuitprops_AM(design, simoptions)
-% Calculates and completes various circuit parameters for a machine for
-% evaluation in a ode simulation
+% Calculates and completes circuit parameters for a machine ode simulation
 %
 % Syntax
 % 
@@ -8,52 +7,146 @@ function [design, simoptions] = circuitprops_AM(design, simoptions)
 %
 % Description
 %
+% sets up the circuit parameters for a machine simulation, i.e. the branch
+% and series configuration of the coils and the load model used for
+% simulation. Possible load models include a machine side active rectifier,
+% in which case circuitprops_AM can also set up the field oriented control
+% parameters.
+%
 % Input
 %
 %  design - a structure containing various design parameters of the
-%   electrical machine necessary for calculating some circuit properties. The
-%   design structure must contain the following fields:
+%   electrical machine necessary for calculating some circuit properties.
+%   The design structure may contain the following fields:
 % 
 %   CoilResistance : resistance of a single coil winding
 %
-%   RlVRp : ratio of coil resistance to grid/load resistance
-%
 %   design can also optionally contain the following fields:
 %
+%   RlVRp : ratio of coil resistance to grid/load resistance, see the
+%    simoptions LoadModel field description below for more information.
+%
+%   LoadResistance : load resistance value, see the simoptions LoadModel
+%    field description below for more information.
+%
 %   CoilInductance - Inductance of a single coil, if not present the
-%     phase inductance will be set to 1e-4. If CoilInductance is a
-%     two-element vector, the first value should be the self-inductance of
-%     the coils, while the second is the mutual inductance between coils. 
+%    phase inductance will be set to 1e-4. If CoilInductance is a
+%    two-element vector, the first value should be the self-inductance of
+%    the coils, while the second is the mutual inductance between coils. 
 %
-%   LgVLc - ratio of grid inductance to col inductance, ignored if
-%     CoilInductance not present
+%   LgVLc : ratio of grid inductance to col inductance, ignored if
+%    CoilInductance not present
 %
-%   Branches - Scalar number of parallel branches of series coils per phase
+%   Branches : Scalar number of parallel branches of series coils per phase
 %
-%   CoilsPerBranch - Scalar number of series coils in each parallel branch
+%   CoilsPerBranch : Scalar number of series coils in each parallel branch
 %
-%   NCoilsPerPhase - Scalar total number of coils per phase. 
+%   NCoilsPerPhase : Scalar total number of coils per phase. 
 %
 %   Any two of 'Branches', 'CoilsPerBranch' and 'NCoilsPerPhase' can be
 %   supplied and the other will be calculated if not present. You can also
 %   supply all three, a check will be made that the combination works. If
 %   only 'NCoilsPerPhase' is present all coils will be put in series.
 %
+%   MachineSidePowerConverter : This optional field is used when the load
+%    model is set to 'Machine Side Power Converter'. See the help for the
+%    simoption LoadModel field below for the expected contents.
 %
-% simoptions - 
+%   FOControl : This optional field is used when the load model is set to
+%    'Machine Side Power Converter'. See the help for the simoption
+%    LoadModel field below for the expected contents.
 %
+%  simoptions - options structure controlling the load model etc. which can
+%   contain the following fields:
+%
+%   LoadModel : charcter vector containing the load model type to be
+%    applied. Can be one of 'None', 'Simple RL Circuit', 'Resistive', 'Zero
+%    Power Factor' and 'Machine Side Power Converter'. Case is ignored, as
+%    is whitespace (so 'simplerlcircuit' works the same as 'Simple RL
+%    Circuit'). 
+%
+%    'Simple RL Circuit'
+%
+%    In this case the design structure must contain either the field
+%    'RlvRp' or 'LoadResistance', where LoadResistance would contain the
+%    per phase load resistance value in Ohms, and RlvRp would contain the
+%    resistance value expressed and a ratio of the machine phase
+%    resistance. The actual load resistance value in the RlvRp case is
+%    determined by multiplying RlvRp by design.PhaseResistance. The design
+%    structure may optionally also contain the fields LgvLc or
+%    LoadInductance. These work in the same way as RlvRp and LoadResistance
+%    except that if neither is present the load inductance is set to zero.
+%
+%    'Zero Power Factor'
+%
+%    This sets the machine to have a load resistance and also makes the
+%    machine inductance zero, as though inductance is being cancelled at
+%    all frequencies. The design structure must contain either the field
+%    'RlvRp' or 'LoadResistance', where LoadResistance would contain the
+%    per phase load resistance value in Ohms, and RlvRp would contain the
+%    resistance value expressed and a ratio of the machine phase
+%    resistance. The actual load resistance value in the RlvRp case is
+%    determined by multiplying RlvRp by design.PhaseResistance.
+%
+%    'Machine Side Power Converter'
+%
+%    The machine will be simulated attached to a voltage source active
+%    rectifier. In this case the design structure must contain the
+%    following fields:
+%
+%    MachineSidePowerConverter : this must be a structure containing the
+%     following fields:
+%
+%     Rds - The parasitic resistance of each switch in the converter
+%
+%     Vdc - The DC output voltage
+%
+%    By default when using the 'Machine Side Power Converter option,
+%    circuitprops_AM will attempt to determine PI paramters for control of
+%    the machine using field oriented control. Unless told otherwise,
+%    circuitprops_AM will attempt to determine these paramers automatically
+%    from the machine and converter specifications. However, if you wish to
+%    provide PI, or PID parameters, you may add a field 'FOControl' to the
+%    design structure
+%
+%    FOControl : this must be a structure containing the fields which
+%     determine the operation of the PID (or PI) controllers used to
+%     perform field oriented control using the active rectifier. The
+%     follwoing fields may be used:
+%
+%     DirectCurentKp : direct axis current PID proportional coefficient
+%
+%     DirectCurentKi : direct axis current PID integral coefficient
+%
+%     QuadratureCurentKp : quadrature axis current PID proportional 
+%      coefficient
+%
+%     QuadratureCurentKi : quadrature axis current PID integral coefficient
+%
+%     DirectCurentKd : optional field with the direct axis current PID
+%      derivative coefficient. If not present will be set to zero resulting
+%      in a PI controller.
+%
+%     QuadratureCurentKd : optional field with the quadrature axis current
+%      PID derivative coefficient. If not present will be set to zero
+%      resulting in a PI controller.
+%
+%   SkipFOControlSetup - this simoptions field is a true/false flag
+%     indicating whether to skip all field oreiented control setup. See the
+%     LoadModel option above in the 'Machine Side Power Converter' section
+%     for more information.
 %
 % Output
 %
 %  design - the input design structure modified with new fields depending
-%   on it's inital contents.
+%   on it's inital contents and the contents of simoptions.
 % 
-%   In all cases the field 'R' will be added which contains the phase
-%   resistance matrix, e.g., for a 3-phase system
+%   In all cases the field 'RDCPhase' will be added which contains the
+%   phase resistance matrix, e.g., for a 3-phase system
 % 
-%   R = [ Ra  0   0;
-%         0   Rb  0;
-%         0   0   Rc ];
+%   RDCPhase = [ Ra  0   0;
+%                0   Rb  0;
+%                0   0   Rc ];
 % 
 %   and Ra = Rb = Rc
 % 
@@ -84,9 +177,46 @@ function [design, simoptions] = circuitprops_AM(design, simoptions)
 %   If not present previously the fields 'Branches' and 'CoilsPerBranch' will
 %   be added.
 %
-%   If the field 'RlVRp' is present
+%   The other added fields depend on the contents of the
+%   simoptions.LoadModel field:
+%
+%   'Simple RL Circuit'
+%
+%   The RDCPhase and L fields described above are added
+%
+%   'Zero Power Factor'
+%
+%   The RDCPhase and L fields described above are added
+%
+%   'Machine Side Power Converter'
+%
+%   The RDCPhase and L fields described above are added. In addition, the
+%   structure in the MachineSidePowerConverter field will have the field
+%   REquivalent added which is the sum of the parasitic resistances and
+%   machine phase resistance. As well as this, the FOControl field will be
+%   added to the design structure if it is not already present. After
+%   running circuitprops_AM, the structure in FOControl will contain the
+%   following fields: DirectCurentKp, DirectCurentKi, QuadratureCurentKp,
+%   QuadratureCurentKi, DirectCurentKd and QuadratureCurentKd, PI_d, and
+%   PI_q. The PI_d and PI_q field will contain pidController objects for
+%   the q and d axis current with the PID coefficient value given in the
+%   fields DirectCurentKp, DirectCurentKi, QuadratureCurentKp,
+%   QuadratureCurentKi and DirectCurentKd.
+%
+%   FOControl may also contain the following fields if all the values in
+%   the previous fields were determined automatically by circuitprops_AM:
+%
+%   Rds
+%   Ls
+%   PoleNaturalFreqs
+%   Poles
+%   Damping
+%   MaxTimeStep
+%
+%
 %
 % See also: circuitode_linear
+%
 
     if all (isfield (design, {'CoilsPerBranch', 'Branches'}))
         
@@ -180,6 +310,10 @@ function [design, simoptions] = circuitprops_AM(design, simoptions)
         'simoptions.LoadModel must be a character vector');
 
     switch lower (regexprep (simoptions.LoadModel, '\s+', ''))
+        
+        case 'none'
+            
+            % do nothing
         
         case 'simplerlcircuit'
             
@@ -289,66 +423,88 @@ end
 function [design, simoptions] = focsetup (design, simoptions)
 
     have_control_tools = true;
-    if isoctave
+    if ~isoctave
         if ~license ('test', 'control_toolbox')
             have_control_tools = false;
         end
     end
     
-    design.FOControl.Rdc = design.MachineSidePowerConverter.REquivalent(1);
+    design = setfieldifabsent (design, 'FOControl', struct ());
     
-    if have_control_tools
-    
-%         design.FOControl.Laa = design.PhaseInductance(1);
-%         design.FOControl.M = design.PhaseInductance(2);
-% 
-%         design.FOControl.Lm = design.FOControl.M / -0.5;
-% 
-%         design.FOControl.Lsigma = design.FOControl.Laa - design.FOControl.Lm;
-
-        design.FOControl.Ls = cyclicinductance_AM (design);
-
-        % voltage-current transfer function
-        G = tf (1, [design.FOControl.Ls, design.FOControl.Rdc]);
-
-%         piopts = pidtuneOptions ('DesignFocus', 'reference-tracking');
-        [C_pi, info] = pidtune (G, 'pi');
-        
-%         if info.CrossoverFrequency > 600
-%             [C_pi, info] = pidtune (G, 'pi', 600);
-%         end
-
-        T_pi = feedback (G*C_pi, 1);
-
-        [Wn,zeta,P] = damp (T_pi);
-        
-        if any(info.CrossoverFrequency > 300)
-            warning ('Not all FOControl PI controller pole natural frequencies are less than 300 rad/s');
-        end
-        
-        if any ((1/sqrt(2)) - zeta > 0.5)
-            warning ('At least one FOControl PI controller damping is more than 0.5 from the reccomended value');
-        end
-        
-        design.FOControl.PoleNaturalFreqs = Wn;
-        design.FOControl.Poles = P;
-        design.FOControl.Damping = zeta;
-        
-        % set Vdc to be twice the natural rectification voltage
-        design.FOControl.PI_d = pidController ( C_pi.Kp, C_pi.Ki, 0, ...
-                                               'MaxOut', 0.5*design.MachineSidePowerConverter.Vdc, ...
-                                               'MinOut', -0.5*design.MachineSidePowerConverter.Vdc, ...
-                                               'AntiWindup', false );
-                                           
-        design.FOControl.PI_q = pidController ( C_pi.Kp, C_pi.Ki, 0, ...
-                                                'MaxOut', 0.5*design.MachineSidePowerConverter.Vdc, ...
-                                                'MinOut', -0.5*design.MachineSidePowerConverter.Vdc, ...
-                                                'AntiWindup', false );
-                                            
-        design.FOControl.MaxTimeStep = min (1./Wn);
+    if all (isfield (design.FOControl, { 'DirectCurentKp', ...
+                                         'DirectCurentKi', ...
+                                         'QuadratureCurentKp', ...
+                                         'QuadratureCurentKi' } ))
+                                     
+        design.FOControl = setfieldifabsent (design.FOControl, 'DirectCurentKd', 0);
+        design.FOControl = setfieldifabsent (design.FOControl, 'QuadratureCurentKd', 0);               
     
     else
-        warning ('You are using Matlab and the control toolbox is not installed, so I cannot tune the Field Oreinted control PI controller parameters. Octave has a free control toolbox which can do this.');
+        if have_control_tools
+            
+            design.FOControl.Rdc = design.MachineSidePowerConverter.REquivalent(1);
+    %         design.FOControl.Laa = design.PhaseInductance(1);
+    %         design.FOControl.M = design.PhaseInductance(2);
+    % 
+    %         design.FOControl.Lm = design.FOControl.M / -0.5;
+    % 
+    %         design.FOControl.Lsigma = design.FOControl.Laa - design.FOControl.Lm;
+
+            design.FOControl.Ls = cyclicinductance_AM (design);
+
+            % voltage-current transfer function
+            G = tf (1, [design.FOControl.Ls, design.FOControl.Rdc]);
+
+    %         piopts = pidtuneOptions ('DesignFocus', 'reference-tracking');
+            [C_pi, info] = pidtune (G, 'pi');
+
+    %         if info.CrossoverFrequency > 600
+    %             [C_pi, info] = pidtune (G, 'pi', 600);
+    %         end
+
+            T_pi = feedback (G*C_pi, 1);
+
+            [Wn,zeta,P] = damp (T_pi);
+
+            if any(info.CrossoverFrequency > 300)
+                warning ('Not all FOControl PI controller pole natural frequencies are less than 300 rad/s');
+            end
+
+            if any ((1/sqrt(2)) - zeta > 0.5)
+                warning ('At least one FOControl PI controller damping is more than 0.5 from the reccomended value');
+            end
+
+            design.FOControl.PoleNaturalFreqs = Wn;
+            design.FOControl.Poles = P;
+            design.FOControl.Damping = zeta;
+            design.FOControl.MaxTimeStep = min (1./Wn);
+            
+            design.FOControl.DirectCurentKp = C_pi.Kp;
+            design.FOControl.DirectCurentKi = C_pi.Ki;
+            design.FOControl.DirectCurentKd = 0;
+            design.FOControl.QuadratureCurentKp = C_pi.Kp;
+            design.FOControl.QuadratureCurentKi = C_pi.Ki;
+            design.FOControl.QuadratureCurentKd = 0;
+
+        else
+            warning ('You are using Matlab and the control toolbox is not installed, so I cannot tune the Field Oreinted control PI controller parameters. Octave has a free control toolbox which can do this.');
+        end
+    
     end
+    
+    % set Vdc to be twice the natural rectification voltage
+    design.FOControl.PI_d = pidController ( design.FOControl.DirectCurentKp, ...
+                                            design.FOControl.DirectCurentKi, ...
+                                            design.FOControl.DirectCurentKd, ...
+                                           'MaxOut', 0.5*design.MachineSidePowerConverter.Vdc, ...
+                                           'MinOut', -0.5*design.MachineSidePowerConverter.Vdc, ...
+                                           'AntiWindup', false );
+
+    design.FOControl.PI_q = pidController ( design.FOControl.QuadratureCurentKp, ...
+                                            design.FOControl.QuadratureCurentKi, ...
+                                            design.FOControl.QuadratureCurentKd, ...
+                                            'MaxOut', 0.5*design.MachineSidePowerConverter.Vdc, ...
+                                            'MinOut', -0.5*design.MachineSidePowerConverter.Vdc, ...
+                                            'AntiWindup', false );
 
 end
