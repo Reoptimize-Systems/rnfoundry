e8effc0e8ff5f1a40d9b1adcc5fbcd897aa3a733
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2018-11-09 16:36:10 +0000

    added +wsim/wecControllerFcnBased.m and impemented sampleDelayedLogVars in +wsim/wecController.m

--- wave/matlab-octave/wec-sim/+wsim/wecController.m
+++ wave/matlab-octave/wec-sim/+wsim/wecController.m
@@ -5,12 +5,73 @@ classdef wecController < handle
     properties
         
         wecSimObj; % the wsim.wecSim object running the simulation
-        logger; % wsim.logger object
+        
+    end
+    
+    properties (GetAccess=public, SetAccess=protected)
+        
+        usingSampleDelays;
+        sampleDelayInfo;
         
     end
     
     
     methods
+        
+        function self = wecController (varargin)
+            % ceorl.wecController constructor
+            %
+            % Syntax
+            %
+            % ctrlobj = wsim.wecController ()
+            % ctrlobj = wsim.wecController ('Parameter', Value)
+            %
+            % Description
+            %
+            % The wsim.wecController class is a base class for all
+            % WEC controllers
+            %
+            % Input
+            %
+            % Addtional arguments may be supplied as parameter-value pairs.
+            % The available options are:
+            %
+            %  'SampleDelays' - empty matrix or structure containing 
+            %    specification of sample delayed logged variables to be
+            %    used by the controller to generate the control outputs
+            %
+            % Output
+            %
+            %  ctrlobj - wsim.wecController object
+            %
+            %
+            %
+            % See Also: 
+            %
+            
+            options.SampleDelays = [];
+            
+            options = parse_pv_pairs (options, varargin);
+            
+            if isempty (options.SampleDelays)
+                
+                self.usingSampleDelays = false;
+                self.sampleDelayInfo = [];
+                
+            else
+                
+                check.structHasAllFields (options.SampleDelays, {'SampleDelays', 'InitialValues'}, true, 'SampleDelays');
+                
+                self.sampleDelayInfo = options.SampleDelays;
+                
+                if all (self.sampleDelayInfo.SampleDelays == 0)
+                    self.usingSampleDelays = false;
+                else
+                    self.usingSampleDelays = true;
+                end
+            end 
+            
+        end
         
         function value = ptoControlOutput (self, id, pto_internal_variables)
             
@@ -30,6 +91,50 @@ classdef wecController < handle
         
         function finish (self)
             
+            
+        end
+        
+    end
+    
+    methods (Access = protected)
+        
+%         function out = timeDelayedLogVars (self, varnames, timevarnames, delays, initialvals)
+%             
+%             % copy intial vals for output as this will be correct size and
+%             % hold the right default values which we will only overwrite
+%             % once the current sim time 
+%             out = initialvals;
+%             
+%             for varind = 1:numel (varnames)
+%                 
+%                 if self.logger.data.(timevarnames{ind})(self.logger.info.(timevarnames{ind}).LastLogIndex) - delays(varind) > self.wecSimObj.simInfo.TStart
+%                     
+%                 end
+%                 
+%             end
+%             
+%         end
+        
+        function out = sampleDelayedLogVars (self, varnames, sampledelays, initialvals)
+            
+            % copy initial vals for output as this will be correct size and
+            % hold the right default values which we will only overwrite
+            % once the current sim time is greater than the sample delay
+            out = initialvals;
+            
+            for varind = 1:numel (varnames)
+                
+                if self.wecSimObj.logger.info.(varnames{varind}).LastLogIndex >= sampledelays(varind)
+                    
+                    % get value at sampledelays - 1, as a value of 0 will
+                    % give the last logged value, which is from the
+                    % previous time step, i.e. a delay of one time step
+                    % from the current time
+                    out{varind} = getPrevLoggedVal(self.wecSimObj.logger, varnames{varind}, sampledelays(varind)-1);
+                    
+                end
+                
+            end
             
         end
         
