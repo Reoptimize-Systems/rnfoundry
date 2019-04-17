5084af10a697c76b732522e14e7a93c4f2108b71
Richard Crozier <a@b.c>
2019-04-17 15:34:55 +0100

    +mbdyn/+pre/singleTplDriveCaller.m: added helpfor constructor

--- common/multibody/MBDyn/+mbdyn/+pre/singleTplDriveCaller.m
+++ common/multibody/MBDyn/+mbdyn/+pre/singleTplDriveCaller.m
@@ -13,7 +13,47 @@ classdef singleTplDriveCaller < mbdyn.pre.driveCaller
         function self = singleTplDriveCaller (entity, drivecaller, varargin)
             % mbdyn.pre.singleTplDriveCaller constructor
             %
-
+            %
+            % Syntax
+            %
+            % obj = mbdyn.pre.singleTplDriveCaller (entity, drivecaller)
+            % obj = mbdyn.pre.singleTplDriveCaller (..., 'Parameter', Value)
+            %
+            % Description
+            %
+            % singleTplDriveCaller is used where a scalar value is to be 
+            %
+            % Input
+            %
+            %  entity - numeric 3x1 vector, 6x1 vector, 3x3 matrix or 6x6
+            %    matrix
+            %
+            %  drivecaller - scalar drive caller (mbdyn.pre.drive object), 
+            %    the output of which is multiplied by the entity in each
+            %    step. 
+            %
+            % Addtional arguments may be supplied as parameter-value pairs.
+            % The available options are:
+            %
+            %  'Reference' - In some contexts (e.g. when specifying the
+            %    direction of a force), in which the entity is a 3 element
+            %    vector, a reference frame in which entity is
+            %    expressed can be supplied using the option.
+            %
+            % Output
+            %
+            %  obj - mbdyn.pre.singleTplDriveCaller object
+            %
+            % Examples
+            %
+            % Example 1
+            %
+            % % to apply a constant 100N force in the upward direction
+            % force_val = mbdyn.pre.singleTplDriveCaller ([0;0;1], mbdyn.pre.const (100));
+            %
+            %
+            % See Also: 
+            %
             
             options.Reference = '';
             
