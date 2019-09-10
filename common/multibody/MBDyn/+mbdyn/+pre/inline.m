45d6d74225f75dc9ddbfd89d40560ed4344a7fc9
Richard Crozier <3159692+crobarcro@users.noreply.github.com>
2019-09-10 16:32:31 +0100

    +mbdyn/+pre/inline.m: expect orientation to be an mbdyn.pre.orientmat object

--- common/multibody/MBDyn/+mbdyn/+pre/inline.m
+++ common/multibody/MBDyn/+mbdyn/+pre/inline.m
@@ -350,14 +350,14 @@ classdef inline < mbdyn.pre.twoNodeJoint
               
             % set the transform object which controls the line
             % location and orientation
-            M = [ lineorientm, linepos; ...
+            M = [ lineorientm.orientationMatrix, linepos; ...
                   0, 0, 0, 1 ];
                   
             set ( self.lineTransformObj, 'Matrix', M );
             
             % set the transform object which controls the point cylinder
             % location and orientation
-            M = [ lineorientm, pointpos; ...
+            M = [ lineorientm.orientationMatrix, pointpos; ...
                   0, 0, 0, 1 ];
                   
             set ( self.transformObject, 'Matrix', M );
