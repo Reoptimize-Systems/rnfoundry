16d44dac2cd545353d1eef480dbfa7ee6bd4d062
Richard Crozier <a@b.c>
2019-09-10 16:26:46 +0100

    +stl/read.m: improved help, code formatting

--- common/matlab-octave/+stl/read.m
+++ common/matlab-octave/+stl/read.m
@@ -1,13 +1,37 @@
-function [v, f, n, name] = read(fileName)
-%STLREAD reads any STL file not depending on its format
-%V are the vertices
-%F are the faces
-%N are the normals
-%NAME is the name of the STL object (NOT the name of the STL file)
+function [v, f, n, name] = read (fileName)
+% stl.read reads an STL file (binary or ASCII)
+%
+% Syntax
+%
+% [v, f, n, name] = stl.read (fileName)
+%
+% Input
+%
+%  fileName - path of stl file to be read
+% 
+% Output
+% 
+%  v - (n x 3) matrix with the x, y and z coordinates of the mesh vertices
+%
+%  f - (m x 3) matrix of connectivities representing the faces of the mesh
+%
+%  n - (m x 3) matrix of face normals
+%
+%  name - the name of the STL object stored in the file (NOT the name of
+%   the STL file)
+%
+%
 
-format = stl.getFormat(fileName);
-if strcmp(format,'ascii')
-  [v,f,n,name] = stl.readAscii(fileName);
-elseif strcmp(format,'binary')
-  [v,f,n,name] = stl.readBinary(fileName);
+    format = stl.getFormat(fileName);
+    
+    if strcmp(format,'ascii')
+        
+        [v,f,n,name] = stl.readAscii(fileName);
+        
+    elseif strcmp(format,'binary')
+        
+        [v,f,n,name] = stl.readBinary(fileName);
+        
+    end
+
 end
