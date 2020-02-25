681424533e804ef2d394d5f0fe5d9c325acc8774
Richard Crozier <addressforspam2003@yahoo.co.uk>
2020-02-25 17:32:18 +0000

    +wsim/exportBodyHydroDataToCSV.m: export correct data

--- wave/matlab-octave/wec-sim/+wsim/exportBodyHydroDataToCSV.m
+++ wave/matlab-octave/wec-sim/+wsim/exportBodyHydroDataToCSV.m
@@ -1,12 +1,12 @@
-function exportBodyHydroDataToCSV (body, filename)
+function exportBodyHydroDataToCSV (body, waves, filename)
 
     T = body.hydroData.simulation_parameters.T';
     
     % get the real valued excitation forces
-    F_ex_real = squeeze (permute (body.hydroData.hydro_coeffs.excitation.re(1:3,:,:), [3, 1, 2]));
+%     F_ex_real = squeeze (permute (body.hydroForce.fExt.re(1:3,:,:), [3, 1, 2]));
     
-    cellstr2txtfile (filename, {'Wave Period (s), F_ex_real_x (N), F_ex_real_y (N), F_ex_real_z (N)'});
+    cellstr2txtfile (filename, {'Wave Period (s), Wave Frequency (rad/s), F_ex_real_x (N), F_ex_real_y (N), F_ex_real_z (N), Tau_ex_real_x (N), Tau_ex_real_y (N), Tau_ex_real_z (N)'});
     
-    dlmwrite (filename, [T, F_ex_real], '-append');
+    dlmwrite (filename, [1./(waves.w(:)/(tau)), waves.w(:), body.hydroForce.fExt.re], '-append');
 
 end
