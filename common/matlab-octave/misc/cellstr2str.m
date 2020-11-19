acd74e438f8aea7f144d0a170095e2830a088465
Richard Crozier <addressforspam2003@yahoo.co.uk>
2020-11-19 23:21:59 +0000

    cellstr2str.m: add option to set split character

--- common/matlab-octave/misc/cellstr2str.m
+++ common/matlab-octave/misc/cellstr2str.m
@@ -1,4 +1,4 @@
-function s = cellstr2str(C)
+function s = cellstr2str(C, splitchar)
 % converts a cell array of strings to a single string with newlines
 %
 % Syntax
@@ -13,6 +13,9 @@ function s = cellstr2str(C)
 %
 %  C - cell string array
 %
+%  splitchar - optional character or string to insert between each
+%   character vector in the cell array. Devault is '\n' if not supplied. 
+%
 % Output
 %
 %  s - string composed of the strings in each cell of the input C cell
@@ -21,11 +24,19 @@ function s = cellstr2str(C)
 %
 % See Also: cellstr2txtfile, printcellstr
 %
+    if nargin < 2
+        splitchar = '\n';
+        numsplitchar = 1;
+    end
+    
+    numsplitchar = numel(sprintf (splitchar));
 
     if ~iscellstr (C)
         error ('C must a cell array of strings');
     end
     
-    s = sprintf ('%s\n', C{:});
+    s = sprintf (['%s', splitchar], C{:});
+    
+    s((end-numsplitchar+1):end) = [];
 
 end
