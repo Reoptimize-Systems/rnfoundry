7d92307e387a80bd2a8a93184f3f58bac9dd8c26
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2022-12-02 23:40:52 +0000

    ountslaveIDfiles.m: add default directory and  help text

--- common/matlab-octave/multicore/+mcore/countslaveIDfiles.m
+++ common/matlab-octave/multicore/+mcore/countslaveIDfiles.m
@@ -1,4 +1,30 @@
 function nslavefiles = countslaveIDfiles (dirpath)
+% count the number of slave ID fles in a directory
+%
+% Syntax
+%
+% nslavefiles = mcore.countslaveIDfiles ()
+% nslavefiles = mcore.countslaveIDfiles (dirpath)
+%
+% Input
+%
+%  dirpath - directory in which to search for slave ID files. If not
+%   specified the directory returned by mcore.defaultmulticoredir will be
+%   used.
+%
+% Output
+%
+%  nslavefiles - the number of slave ID files found in the specified
+%  directory
+%
+%
+%
+% See Also: 
+%
+
+    if nargin < 1
+        dirpath = mcore.defaultmulticoredir ();
+    end
 
     nslavefiles = mcore.countmatfilesstartingwith ('slaveID_', dirpath);
 
