726e880bf2718da2fa4442d731d8c2d8e7a34aad
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2023-04-05 08:55:43 +0100

    countparameterfiles.m: ad help and use default dir if not specified

--- common/matlab-octave/multicore/+mcore/countparameterfiles.m
+++ common/matlab-octave/multicore/+mcore/countparameterfiles.m
@@ -1,4 +1,29 @@
 function nparamfiles = countparameterfiles (dirpath)
+% count the number of multicore job parameter files
+%
+% Syntax
+%
+% nparamfiles = mcore.countparameterfiles (dirpath)
+%
+%
+% Input
+%
+%  dirpath - directory in which to search for parameter files. If not
+%   specified the directory returned by mcore.defaultmulticoredir will be
+%   used.
+%
+% Output
+%
+%  nparamfiles - number of parameter files
+%
+%
+%
+% See Also: 
+%
+
+    if nargin < 1
+        dirpath = mcore.defaultmulticoredir ();
+    end
 
     nparamfiles = mcore.countmatfilesstartingwith ('parameters_', dirpath);
     
