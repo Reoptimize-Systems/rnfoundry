edb68be3eb26db7f0281a5741510107685abd65d
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2023-05-04 16:50:24 +0100

    +mcore/errormail.m: make more generic, use settings for mail address

--- common/matlab-octave/multicore/+mcore/errormail.m
+++ common/matlab-octave/multicore/+mcore/errormail.m
@@ -1,5 +1,20 @@
 function settings = errormail(settings, errorstruct, i, parameterCell, evalfcn)
 
+% depends on the sendmail command. Before this can be used, need to run the
+% something like the following
+%
+% setpref('Internet','SMTP_Server','smtp.example.com');
+% setpref('Internet','E_mail','your_email@example.com'');
+% setpref('Internet','SMTP_Username','your_email@example.com');
+% setpref('Internet','SMTP_Password','<replace me>'); 
+% props = java.lang.System.getProperties;
+% props.setProperty('mail.smtp.auth','true');
+% props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+% props.setProperty('mail.smtp.socketFactory.port','465');
+%
+% With whatever settings are apropriate for your email provider.
+%
+
     persistent errorid
     
     if isempty(errorid) || ~strcmp(char(errorid), errorstruct.identifier)
@@ -10,6 +25,8 @@ function settings = errormail(settings, errorstruct, i, parameterCell, evalfcn)
         emailstr = sprintf(['This is an automated message from mcore\n\n', ...
             'The objective function has encountered the following error:\n\n%s\n\nWhile evaluating ind %d'], errorstruct.message, i);
 
+        % convert the paramter evaluation furntion to a string so it can be
+        % mentioned in the email report
         if isa(evalfcn, 'function_handle')
             evalfcn = func2str(evalfcn);
         else
@@ -19,12 +36,17 @@ function settings = errormail(settings, errorstruct, i, parameterCell, evalfcn)
             end
         end
 
-        try
-            sendmail('r.crozier@ed.ac.uk', ...
-                     sprintf('mcore: error in objective function %s', evalfcn), ...
-                     emailstr);
-        catch
-            warning('mcore objective fcn error email send failure.')
+        if isempty (settings.ErrorFcnSettings.MailToAddress)
+            warning('mcore objective fcn error email address in ErrorUserData.MailToAddress was empty. Email not sent, body was:\n\n%sn\n', ...
+                emailstr);
+        else
+            try
+                sendmail(settings.ErrorFcnSettings.MailToAddress, ...
+                         sprintf('mcore: error in objective function %s', evalfcn), ...
+                         emailstr);
+            catch
+                warning('mcore objective fcn error email send failure.');
+            end
         end
     
     end
