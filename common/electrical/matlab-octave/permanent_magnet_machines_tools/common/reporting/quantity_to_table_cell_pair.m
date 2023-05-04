84e453adb2023e7abe2a2e901ed2eff5e91ed915
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2023-05-04 17:04:31 +0100

    quantity_to_table_cell_pair.m: add help and pass through idx arg

--- common/electrical/matlab-octave/permanent_magnet_machines_tools/common/reporting/quantity_to_table_cell_pair.m
+++ common/electrical/matlab-octave/permanent_magnet_machines_tools/common/reporting/quantity_to_table_cell_pair.m
@@ -1,13 +1,56 @@
-function tcells = quantity_to_table_cell_pair (name, unit, S, fieldname, scalefac)
+function tcells = quantity_to_table_cell_pair (name, unit, S, fieldname, scalefac, idx)
+% creates well formatted name-value table enrtries from structure fields
+%
+% Syntax
+%
+% tcells = quantity_to_table_cell_pair (name, unit, S, fieldname, scalefac)
+%
+% Description
+%
 % created a two element cell array with a name and value with an
-% appropriate SI prefix
-
+% appropriate SI prefix, extracting the value from the field of a
+% structure. Typically for use in conjuction with 
+%
+% Input
+%
+%  name - name of the quantity being entered in the table
+%
+%  unit - SI unit of the quantity
+%
+%  S - structure contining the quantity as a field.
+%
+%  fieldname - field name of the quantity in the structure, S
+%
+%  scalefac - (optional) scale factor to apply to the number before doing
+%    the conversion. If not supplied, default is 1 (no scaling).
+%
+%  idx - (optional) the structure field may be a vector or matrix. By
+%    default quantity_to_table_cell_pair always converts only the number
+%    at index 1 in the numeric field. The idx option can be used to convert
+%    a number at a different index. Default is 1.
+%
+%
+% Output
+%
+%  tcells - a (1 x 2) cell array. The fisrt cell will contain the name of
+%   the quantity (simply copied from the input), the second will contain an
+%   SI prefixed string as produced by the function
+%   "scalar_field_num_and_si_prefix".
+%
+%
+%
+% See Also: scalar_field_num_and_si_prefix.m
+%
 
     if nargin < 5
         scalefac = 1;
     end
 
-    [num, prefix] = scalar_field_num_and_si_prefix (S, fieldname, scalefac, 1);
+    if nargin < 6
+        idx = 1;
+    end
+
+    [num, prefix] = scalar_field_num_and_si_prefix (S, fieldname, scalefac, idx);
 
     switch prefix
 
